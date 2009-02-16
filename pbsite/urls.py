293074510a2be88a982684e00f5b57346b2fd07e
ctmiller <chris@podiobooks.com>
2009-02-15 20:06:37 -0800

    Added base template, index page, and index view. Updated admin section.

--- pbsite/urls.py
+++ pbsite/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
-
+import os
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from pbsite import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^site/', include('site.foo.urls')),
+    (r'^$', 'pbsite.main.views.index'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -16,4 +16,8 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
 
 	
+)
+
+urlpatterns += patterns('',
+    (r'^content/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'content')}),
 )
