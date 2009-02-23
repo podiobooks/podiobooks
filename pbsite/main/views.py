ad8c0f327c37e10f7f58a56f692a3b8df6cb085e
Jason Penney <jpenney@mini.local>
2009-02-22 20:54:30 -0500

    beginnings of title page
    Former-commit-id: 7e3f9c482f01aa76462dec02af0191e5d51c93cc
    Former-commit-id: 8a309e7c98d26e69307544844f31f0c74b3e3588

--- pbsite/main/views.py
+++ pbsite/main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.http import HttpResponse,  HttpResponsePermanentRedirect
+from pbsite.main.models import *
 
 def index(request):
     return render_to_response('main/index.html', {}, context_instance=RequestContext(request))
+
