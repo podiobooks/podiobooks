ab08ea5a16565c5693da1462b2536d02be210910
Thomas Gideon <cmdln@thecommandline.net>
2009-08-04 15:59:23 -0400

    Migrating bookcategory to main_category.

--- dbscripts/pb_dump.sh
+++ dbscripts/pb_dump.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo -n "Enter MySQL password for root: "
+read pwd
+
 # base command to pull from MySQL
-dump_base="mysqldump -u root -p"
+dump_base="mysqldump -u $1 -p$pwd"
 
 # these are in dependency order
 tables="bookcategory \
@@ -16,4 +19,13 @@ downloads \
 subscription"
 
 $dump_base --no-create-db --no-data podiobooks $tables > pb_schema.sql
-$dump_base --no-create-info podiobooks $tables > pb_data.sql
+perl mysql2pgsql.perl --char2varchar pb_schema.sql pb_schema_pg.sql
+
+for table in $tables
+do
+    echo "Dumping $table."
+    $dump_base --no-create-info podiobooks $table > pb_data_$table.sql
+	perl mysql2pgsql.perl --char2varchar pb_data_$table.sql pb_data_${table}_pg.sql
+done
+
+
--- pbsite/main/management/commands/migrate.py
+++ pbsite/main/management/commands/migrate.py
@@ -11,6 +11,7 @@ import datetime
 
 from django.core.management.base import BaseCommand
 from django.db import connection, transaction
+from django.db import models
 
 class Command(BaseCommand):
     def __init__(self):
@@ -21,17 +22,43 @@ class Command(BaseCommand):
         sql_dir.append("sql")
         sql_dir = os.path.join("/", *sql_dir)
         self.sql_dir = sql_dir
-        
+
     def handle(self, *args, **options):
         start = datetime.datetime.now()
-        
+
         cursor = connection.cursor()
 
-        # TODO work!
+        self.__migrate_table(cursor, 'bookcategory', 'main_category',
+                             [
+                                DropColumn('display'),
+                                DropColumn('parentcatid'),
+                                DropColumn('itunesxml'),
+                                AddColumn(models.SlugField(name='slug',
+                                                           default="lower(regexp_replace(name, ' ', '-'))")),
+                                AddColumn(models.BooleanField(name='deleted', default='false')),
+                                AddColumn(models.DateTimeField(name='date_created', default='now()')),
+                                AddColumn(models.DateTimeField(name='date_updated', default='now()')),
+                                'category.sql'
+                             ])
 
         transaction.commit_unless_managed()
-        
-        print 'Took %s to complete calculations.' % elapsed
+
+        elapsed = datetime.datetime.now() - start
+
+        print 'Took %s to complete migration.' % elapsed
+
+
+    def __migrate_table(self, cursor, name, new_name, operations):
+        print 'Migrating %s' % name
+        cursor.execute('alter table %s rename to %s' % (name, new_name))
+        cursor.execute('alter sequence %s_id_seq rename to %s_id_seq' % (name, new_name))
+        for operation in operations:
+            if operation.__class__ == str:
+                op_sql = self.__load_sql(operation)
+                cursor.execute(op_sql)
+            else:
+                operation.table_name = new_name
+                operation.migrate(cursor)
 
     def __load_sql(self, file_name):
         sql_file = open(os.path.join(self.sql_dir, file_name))
@@ -41,7 +68,33 @@ class Command(BaseCommand):
                 sql += line
         finally:
             sql_file.close()
-        
+
         if not sql:
             raise Exception('Could not load sql file, %s, from directory, %s.' % (file_name, self.sql_dir))
         return sql
+
+
+class DropColumn:
+    def __init__(self, name):
+        self.name = name
+        self.table_name = None
+
+
+    def migrate(self, cursor):
+        sql = 'alter table %s drop column %s' % (self.table_name, self.name)
+        cursor.execute(sql)
+
+
+class AddColumn:
+    def __init__(self, field):
+        self.field = field
+        self.table_name = None
+
+
+    def migrate(self, cursor):
+        sql = 'alter table %s add column %s %s;' % (self.table_name, self.field.name, self.field.db_type())
+        cursor.execute(sql)
+        sql = 'update %s set %s = %s;' % (self.table_name, self.field.name, self.field.default)
+        cursor.execute(sql)
+        sql = 'alter table %s alter column %s set not null' % (self.table_name, self.field.name)
+        cursor.execute(sql)
