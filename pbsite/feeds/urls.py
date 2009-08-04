1d957cbbafe7a04b8eee7657af7de597e9487f37
unknown <tlwhit2@.AD.QINTRA.COM>
2009-08-04 17:01:03 -0600

    First cut at basic iTunes-extended RSS feed functionality.
    Former-commit-id: fce71c187ee1722defa2f20e86e4aa8857380d3d
    Former-commit-id: 19f6d41454a2c3c5994666478cc3d9958aaac492
    Former-commit-id: 0b8ec97b972fc2ed8296682c5d9b56b750f7364e

---
+++ pbsite/feeds/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from pbsite.feeds.feeds import TitleFeed, EpisodeFeed
+
+feeds = {
+    'titles': TitleFeed,
+    'episodes': EpisodeFeed,
+}
+
+urlpatterns = patterns('',
+    # feeds
+    url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),     
+)
