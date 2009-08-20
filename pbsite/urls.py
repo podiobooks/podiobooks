7b10b78f5f7ea3ddf0c503aad4b3a08f2aa71b05
unknown <tlwhit2@.AD.QINTRA.COM>
2009-08-20 16:21:27 -0600

    Added comments, cleaned up imports.
    Former-commit-id: f20ca663d89a47829095eecf18f1a28b57a22aa0
    Former-commit-id: 2c04f28221fc1bd5bdeb42b5a5a3d99c7949e102
    Former-commit-id: 4a71b43f78b7a666bd60d2e451569ddcd5fe1aef

--- pbsite/urls.py
+++ pbsite/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls.defaults import *
+"""
+    Master URL Pattern List for the application.  Most of the patterns here should be top-level
+    pass-offs to sub-modules, who will have their own urls.py defining actions within.
+"""
+
+from django.conf.urls.defaults import patterns, include
 import os
 from django.contrib import admin
 from pbsite import settings
-from pbsite.main.models import *
 
 admin.autodiscover()
 
@@ -27,5 +31,8 @@ urlpatterns = patterns('',
     
     # Feeds:
     (r'^rss/', include('pbsite.feeds.urls')),
+    
+    # Author Interface:
+    (r'^author/', include('pbsite.author.urls')),
     
 )
