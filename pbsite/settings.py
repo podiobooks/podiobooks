ffc1e6801c79e24452bccb59be7afe437702e816
cyface <tim@cyface.com>
2009-09-07 16:51:42 -0600

    moved MEDIA_URLS to local_settings in case a local install needs different settings.

--- pbsite/settings.py
+++ pbsite/settings.py
@@ -1,30 +1,14 @@
 import os
 # Django settings for pbsite project.
 
+# Set the root path of the project so it's not hard coded
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
 # NOTE
 # see local_settings_template.py for instructions on making your local settings file
 from local_settings import *
 
 MANAGERS = ADMINS
-
-# Set the root path of the project so it's not hard coded
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = PROJECT_PATH + '/media/'
-MEDIA_COVERS = MEDIA_ROOT + "/covers/"
-MEDIA_AWARDS = MEDIA_ROOT + "/awards/"
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
