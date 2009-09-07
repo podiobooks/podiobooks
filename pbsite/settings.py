af977ed6c3c5d3b0588db9ff182522d33ebb0c80
cyface <tim@cyface.com>
2009-09-07 16:39:19 -0600

    Changed MEDIA_URL to have trailing slash.

--- pbsite/settings.py
+++ pbsite/settings.py
@@ -12,9 +12,9 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = PROJECT_PATH + '/media'
-MEDIA_COVERS = MEDIA_ROOT + "/covers"
-MEDIA_AWARDS = MEDIA_ROOT + "/awards"
+MEDIA_ROOT = PROJECT_PATH + '/media/'
+MEDIA_COVERS = MEDIA_ROOT + "/covers/"
+MEDIA_AWARDS = MEDIA_ROOT + "/awards/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
