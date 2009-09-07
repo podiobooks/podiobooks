843e8d662417155972012c48381b35a0b15e0612
cyface <tim@cyface.com>
2009-09-07 17:29:07 -0600

    Trying to get global vars to pass into generic views.
    Former-commit-id: 69e0e323ab3a2684a00b52aaeb0282a643550662
    Former-commit-id: cbccf840de9aea180e6d298b42856d395f23fe6b
    Former-commit-id: 194e4e18c3507605a68e0d5502bdf869673a313e

--- pbsite/main/urls.py
+++ pbsite/main/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
 from pbsite.main.models import *
+from pbsite.settings import *
+
+extra_context = dict({'MEDIA_URL':MEDIA_URL,})
 
 urlpatterns = patterns('',
     
     # title
-    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all() }, 'title_list'),
-    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all() }, 'title_detail'),
+    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all(), 'extra_context': extra_context }, 'title_list'),
+    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'extra_context': extra_context}, 'title_detail'),
 
     # category
     url(r'^category/$','django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all() }, 'category_list'),
