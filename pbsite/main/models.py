b799e82283ec22b0e94bf7ab8879f9aefc9fc5f3
cyface <tim@cyface.com>
2009-09-07 23:15:29 -0600

    Added reverse ManyToMany for Category->Titles.
    Former-commit-id: 058592524b721835be504b819798bda77cc09edf
    Former-commit-id: 7bc6a32d59d1cd981b6bf0a1d6d0cd88ed6dfb2f
    Former-commit-id: 8de2cc200d2d7a8e728fa66aa0e301a059411648
    Former-commit-id: a3379682811ddb986c2c4c47d4d050659b7c6bc4

--- pbsite/main/models.py
+++ pbsite/main/models.py
@@ -43,6 +43,7 @@ class Category(models.Model):
 	"""Categories describe titles for easy of browsing and for recommendations."""
 	name = models.CharField(max_length=255)
 	slug = models.SlugField()
+	titles = models.ManyToManyField('Title', db_table="main_title_categories")
 	deleted = models.BooleanField(default=False)
 	date_created = models.DateTimeField(blank=False, default=datetime.datetime.now())
 	date_updated = models.DateTimeField(blank=False, default=datetime.datetime.now())
@@ -239,7 +240,7 @@ class Title(models.Model):
 	deleted = models.BooleanField(default=False)
 	old_id = models.IntegerField(blank=True, null=True)
 	contributors = models.ManyToManyField('Contributor', through='TitleContributors')
-	categories = models.ManyToManyField('Category')
+	categories = models.ManyToManyField('Category', db_table="main_title_categories")
 	awards = models.ManyToManyField('Award', blank=True)
 	date_created = models.DateTimeField(blank=False, default=datetime.datetime.now())
 	date_updated = models.DateTimeField(blank=False, default=datetime.datetime.now())
