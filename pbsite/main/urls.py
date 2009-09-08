bdc65fb8e68edaed2e0259b376ecd33bf863a178
cyface <tim@cyface.com>
2009-09-07 23:13:12 -0600

    Updated to order titles and categories.
    Former-commit-id: 7839276c5a6c9dc050e4f5217687ebc3bb716f19
    Former-commit-id: 1979aa32d36ae385b3dd0f6d30b1a1852ad9f383
    Former-commit-id: 27b8954759a97100cdcc5afa10520003c85d40c6
    Former-commit-id: 5722109503e023aaaf188d8bd0058d4e86988d1b

--- pbsite/main/urls.py
+++ pbsite/main/urls.py
@@ -5,11 +5,11 @@ from pbsite.settings import *
 urlpatterns = patterns('',
     
     # title
-    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all(), 'template_object_name': 'titles', 'template_name': 'main/title/list.html'}),
-    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail.html'}),
+    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all().order_by('name'), 'template_object_name': 'title', 'template_name': 'main/title/list.html'}, 'title_list'),
+    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail.html'}, 'title_detail'),
 
     # category
-    url(r'^category/$','django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all() }, 'category_list'),
-    url(r'^category/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all() }, 'category_detail'),     
+    url(r'^category/$','django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all().order_by('name'), 'template_object_name': 'category', 'template_name': 'main/category/list.html'}, 'category_list'),
+    url(r'^category/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all(), 'template_object_name': 'category', 'template_name': 'main/category/detail.html'}, 'category_detail'),     
                        
 )
