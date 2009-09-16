d7e7409a96466bcf121c67715607bbf68c35a343
Tim White <tim@cyface.com>
2009-09-16 13:48:24 -0600

    It's back, but with local updates.

---
+++ pbsite/contrib/site_info_context_processor.py
@@ -0,0 +1,10 @@
+from django.contrib.sites.models import Site, RequestSite
+
+def site(request):
+    site_info = {'protocol': request.is_secure() and 'https' or 'http'}
+    if Site._meta.installed:
+        site_info['domain'] = Site.objects.get_current().domain
+        site_info['site_name'] = Site.objects.get_current().name
+    else:
+        site_info['domain'] = RequestSite(request).domain
+    return site_info
