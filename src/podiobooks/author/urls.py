84582fe2c41efb66d83ccfb192d119b93c68bb1c
Tim White <cyface@ubuntu.ubuntu-domain>
2009-10-25 22:11:28 -0600

    Refactored entire dir structure to enable zc.buildout.

---
+++ src/podiobooks/author/urls.py
@@ -0,0 +1,11 @@
+"""
+    URL Pattern List for Author module
+"""
+
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('',
+    
+    # upload form
+    url(r'^upload[/]$', 'podiobooks.author.views.upload', name='author_upload'),            
+)
