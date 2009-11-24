c92f65d9d24731afbcdd8eaa4c0cae28dae53b1e
Tim White <tim@cyface.com>
2009-11-24 14:06:40 -0700

    Added live pull of the blog feed's recent items onto the homepage.
    Former-commit-id: c268966851fa21f1048adca6433bacd47dad11fc
    Former-commit-id: 4a07449e9d36ed33180166d95bee9e0361cbd736
    Former-commit-id: 648f548c24fc2c9f807e12c5755bfbadad0cc37d
    Former-commit-id: 32612fd6e682b49eaf6800e2076ae9bde70572e1
    Former-commit-id: 613b9b8993e269ad39c71ec8259e1527b48e738c

--- src/podiobooks/main/urls.py
+++ src/podiobooks/main/urls.py
@@ -1,5 +1,14 @@
 from django.conf.urls.defaults import patterns, url
 from models import Category, Title
+from contrib.django_restapi.model_resource import Collection
+from contrib.django_restapi.responder import JSONResponder
+
+# Create a handler for RESTful access to the Title Object
+title_resource = Collection(
+    queryset = Title.objects.all(),
+    permitted_methods = ('GET',),
+    responder = JSONResponder()
+)
 
 urlpatterns = patterns('',
     
@@ -8,6 +17,8 @@ urlpatterns = patterns('',
     url(r'^title/search/$','podiobooks.main.views.title_search', name='title_search_form'),
     url(r'^title/search/(?P<keywords>[^/]+)/$','podiobooks.main.views.title_search', name='title_search'),
     url(r'^title/search/redirect$','podiobooks.main.views.title_search', name='title_search_redirect'),
+    url(r'^title/json/(.*?)/?$', title_resource),
+    url(r'^title/ajaxtest/', 'django.views.generic.simple.direct_to_template', {'template': 'main/title/list2.html',}),
     url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail.html'}, name='title_detail'),
     
     # category
