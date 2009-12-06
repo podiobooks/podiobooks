24a11e919abab696defcb9a5269e8bc7068daa95
Tim White <tim@cyface.com>
2009-12-05 22:06:43 -0700

    Fixed bugs in user data, set up filtering to account for initial_data() fixture.

--- src/podiobooks/main/tests.py
+++ src/podiobooks/main/tests.py
@@ -3,6 +3,8 @@ from podiobooks.main.models import *
 from django.template.defaultfilters import slugify
 
 class TitleTestCase(unittest.TestCase):
+    fixtures = []
+    
     def setUp(self):
         # This method sets up a whole series of model objects, and then tries to connect them together.
         
@@ -15,12 +17,12 @@ class TitleTestCase(unittest.TestCase):
         self.user1.last_name = "user1"
         self.user1.save
         
-        self.user1.first_name = "test2"
-        self.user1.last_name = "user2"
+        self.user2.first_name = "test2"
+        self.user2.last_name = "user2"
         self.user2.save
         
-        self.user1.first_name = "test3"
-        self.user1.last_name = "user2"
+        self.user3.first_name = "test3"
+        self.user3.last_name = "user3"
         self.user3.save
         
         # Create some UserProfiles for those test users
@@ -28,42 +30,38 @@ class TitleTestCase(unittest.TestCase):
             user = self.user1,
             slug = slugify(self.user1.get_full_name())
             )
-        self.user1profile.save
         
         self.user2profile = UserProfile.objects.create (
             user = self.user2,
             slug = slugify(self.user2.get_full_name())
             )
-        self.user2profile.save
         
         self.user3profile = UserProfile.objects.create (
             user = self.user3,
             slug = slugify(self.user3.get_full_name())
             )
-        self.user3profile.save
         
         # Create Some Series
         self.series1 = Series.objects.create (
-            name = "PodioBook Series #1",
+            name = "Podiobooks Series #1",
             description = "A wonderful sample series that contains many fine books",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             deleted = False
             )
         self.series1.slug = slugify(self.series1.name)
-        self.series1.save
         
         self.series2 = Series.objects.create (
-            name = "PodioBook Series #2",
+            name = "Podiobooks Series #2",
             description = "Another wonderful sample series that contains many fine books",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             deleted = False
             )
         self.series2.slug = slugify(self.series2.name)
-        self.series2.save
         
         # Create Some Titles
         self.title1 = Title.objects.create (
-                name = "PodioBook Title #1",
+                name = "Podiobooks Title #1",
+                slug = slugify("Podiobooks Title #1"),
                 series = self.series1,
                 description = "A fantastic sample book of surpassing quality and scope, that truly redefines what it means to be a PodioBook.",
                 cover = "PodioBook Cover",
@@ -78,11 +76,10 @@ class TitleTestCase(unittest.TestCase):
                 avg_overall = 3.75,
                 deleted = False
                 )
-        self.title1.slug = slugify(self.title1.name)
-        self.title1.save
         
         self.title2 = Title.objects.create (
-                name = "PodioBook Title #2",
+                name = "Podiobooks Title #2",
+                slug = slugify("Podiobooks Title #2"),
                 series = self.series1,
                 description = "A second fantastic sample book of surpassing quality and scope, that truly redefines what it means to be a PodioBook.",
                 cover = "PodioBook Cover 2",
@@ -97,11 +94,10 @@ class TitleTestCase(unittest.TestCase):
                 avg_overall = 6.75,
                 deleted = False
                 )
-        self.title2.slug = slugify(self.title2.name)
-        self.title2.save
         
         self.title3 = Title.objects.create (
-                name = "PodioBook Title #3",
+                name = "Podiobooks Title #3",
+                slug = slugify("Podiobooks Title #3"),
                 series = self.series2,
                 description = "A third fantastic sample book of surpassing quality and scope, that truly redefines what it means to be a PodioBook.",
                 cover = "PodioBook Cover 3",
@@ -116,66 +112,59 @@ class TitleTestCase(unittest.TestCase):
                 avg_overall = 4.75,
                 deleted = False
                 )
-        self.title3.slug = slugify(self.title2.name)
-        self.title3.save
         
         #Create a Partner Object
         self.partner1 = Partner.objects.create (
-            name = "PodioBooks Partner #1",
-            url = "http://podiobooks.com",
-            logo = "http://podiobooks.com",
+            name = "Podiobooks Partner #1",
+            url = "http://Podiobooks.com",
+            logo = "http://Podiobooks.com",
             deleted = False
             )
-        self.partner1.save
         
         #Create some Episodes
         self.episode1 = Episode.objects.create (
             title = self.title1,
-            name = "PodioBooks Title 1 Episode #1",
+            name = "Podiobooks Title #1 Episode #1",
             sequence = 1,
             description = "This is the first wonderful episode of the test title1!",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             filesize = 328886,
             status = 1,
             deleted = False,
             )
-        self.episode1.save
         
         self.episode2 = Episode.objects.create (
             title = self.title2,
-            name = "PodioBooks Title 2 Episode #1",
+            name = "Podiobooks Title #2 Episode #1",
             sequence = 1,
             description = "This is the first wonderful episode of the test title2!",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             filesize = 32886,
             status = 1,
             deleted = False,
             )
-        self.episode2.save
         
         self.episode3 = Episode.objects.create (
             title = self.title2,
-            name = "PodioBooks Title 2 Episode #2",
+            name = "Podiobooks Title #2 Episode #2",
             sequence = 1,
             description = "This is the second wonderful episode of the test title2!",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             filesize = 32886,
             status = 1,
             deleted = False,
             )
-        self.episode3.save
         
         self.episode4 = Episode.objects.create (
             title = self.title3,
-            name = "PodioBooks Title 3 Episode #1",
+            name = "Podiobooks Title #3 Episode #1",
             sequence = 1,
             description = "This is the first wonderful episode of the test title3!",
-            url = "http://www.podiobooks.com",
+            url = "http://www.Podiobooks.com",
             filesize = 32886,
             status = 1,
             deleted = False,
             )
-        self.episode4.save
         
         # Create Some Subscriptions
         
@@ -189,7 +178,6 @@ class TitleTestCase(unittest.TestCase):
                 finished = False,
                 deleted = False
                 )
-        self.subscription1.save()
         self.subscription1.titles.add(self.title1)
         self.subscription1.series.add(self.series2)
         
@@ -203,7 +191,6 @@ class TitleTestCase(unittest.TestCase):
                 finished = False,
                 deleted = False
                 )
-        self.subscription2.save()
         self.subscription2.titles.add(self.title1)
         self.subscription2.titles.add(self.title2)
         
@@ -217,18 +204,24 @@ class TitleTestCase(unittest.TestCase):
                 finished = False,
                 deleted = False
                 )
-        self.subscription3.save()
         self.subscription3.series.add(self.series1)
-        self.subscription3.series.add(self.series2)
-        
+        self.subscription3.series.add(self.series2)     
 
     def testAll(self):
+        # USERS
         print 'Users:'
-        for currentUser in User.objects.all() :
-            print '\tUsername: ' + currentUser.username + '\tSlug: ' + currentUser.get_profile().slug
+        testUsers = User.objects.all().filter(username__startswith='test')
         
+        for currentUser in testUsers:
+            print '\tUsername: ' + currentUser.username + '\tSlug: ' + currentUser.get_profile().slug
+            
+        # User Assertions
+        self.assertEquals(len(testUsers), 3)
+        
+
+        # SERIES
         print '\nSeries: '
-        for currentSeries in Series.objects.all() :
+        for currentSeries in Series.objects.all().filter(name__startswith='Podiobooks Series') :
             print '\n\tName: ' + currentSeries.name
             print '\tSlug: ' + currentSeries.slug
             print '\tTitles:'
@@ -240,17 +233,18 @@ class TitleTestCase(unittest.TestCase):
                 print '\t\tUserName: ' + currentSubscription.user.username
             
             # Series Assertions
-            if currentSeries.name == "PodioBook Series #1" :
+            if currentSeries.name == "Podiobooks Series #1" :
                 self.assertEquals(len(currentSeries.subscription_set.all()), 1)
                 self.assertEquals(len(currentSeries.title_set.all()), 2)
-            elif currentSeries.name == "PodioBook Series #2" :
+            elif currentSeries.name == "Podiobooks Series #2" :
                 self.assertEquals(len(currentSeries.subscription_set.all()), 2)
                 self.assertEquals(len(currentSeries.title_set.all()), 1)
             else :
                 self.fail('Non-matching Series!' + currentSeries.name)
                 
+        # TITLES 
         print '\nTitles: '
-        for currentTitle in Title.objects.all() :
+        for currentTitle in Title.objects.all().filter(name__startswith='Podiobooks Title') :
             print '\n\tName: ' + currentTitle.name
             print '\tSlug: ' + currentTitle.slug
             print '\tSeries: ' + currentTitle.series.name
@@ -262,21 +256,22 @@ class TitleTestCase(unittest.TestCase):
                 print '\t\tUserName: ' + currentSubscription.user.username
             
             # Title Assertions
-            if currentTitle.name == "PodioBook Title #1" :
+            if currentTitle.name == "Podiobooks Title #1" :
                 self.assertEquals(len(currentTitle.episode_set.all()), 1)
                 self.assertEquals(len(currentTitle.subscription_set.all()), 2)
-            elif currentTitle.name == "PodioBook Title #2" :
+            elif currentTitle.name == "Podiobooks Title #2" :
                 self.assertEquals(len(currentTitle.episode_set.all()), 2)
                 self.assertEquals(len(currentTitle.subscription_set.all()), 1)
-            elif currentTitle.name == "PodioBook Title #3" :
+            elif currentTitle.name == "Podiobooks Title #3" :
                 self.assertEquals(len(currentTitle.episode_set.all()), 1)
                 self.assertEquals(len(currentTitle.subscription_set.all()), 0)
             else :
                 self.fail('Non-matching Title!' + currentTitle.name)
-        
+                
+        # SUBSCRIPTIONS  
         print '\nSubscriptions'
         for currentSubscription in Subscription.objects.all() :
-            print '\n\tUser: ' + currentSubscription.user.username
+            print '\n\tUser: ' + currentSubscription.user.username + ": " + currentSubscription.user.get_profile().slug
             print '\tPartner: ' + currentSubscription.partner.name
             print '\tLast Episode Downloaded: ' + currentSubscription.last_downloaded_episode.name
             print '\n\tTitles: '
@@ -290,15 +285,15 @@ class TitleTestCase(unittest.TestCase):
             if currentSubscription.user.username == "testuser1" :
                 self.assertEquals(len(currentSubscription.titles.all()), 1)
                 self.assertEquals(len(currentSubscription.series.all()), 1)
+                self.assertEquals("test1-user1", currentSubscription.user.get_profile().slug)
             elif currentSubscription.user.username == "testuser2" :
                 self.assertEquals(len(currentSubscription.titles.all()), 2)
                 self.assertEquals(len(currentSubscription.series.all()), 0)
+                self.assertEquals("test2-user2", currentSubscription.user.get_profile().slug)
             elif currentSubscription.user.username == "testuser3" :
                 self.assertEquals(len(currentSubscription.titles.all()), 0)
                 self.assertEquals(len(currentSubscription.series.all()), 2)
+                self.assertEquals("test3-user3", currentSubscription.user.get_profile().slug)
             else :
                 self.fail('Non-matching Subscription!' + currentSubscription.user.username)
-        
-        
-        
         
