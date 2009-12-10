2226bcfa8a338b208cda5780755cafe8c179a015
Tim White <tim@cyface.com>
2009-12-09 21:47:18 -0700

    Added partner import script.
    Former-commit-id: 05c4a7e6890658ace1b72e73f71255fb01c87144
    Former-commit-id: dca7a016c11ee97528ab0e6b64f0bbfee9733ef4
    Former-commit-id: b99855ea924c69310e1e6240eb441c923f654de4
    Former-commit-id: e63b784809e24c5fe72eda9a614c5dadf6163a55
    Former-commit-id: fe39c22ce02b63cc40ec9a5b66e8035de879cbc5
    Former-commit-id: 8c30a456f57d4f4bc4140961c538f063411fb6c7

---
+++ dbscripts/migrate_partnercsv_to_partner.py
@@ -0,0 +1,68 @@
+"""
+This script reads in an "CSV for Excel" export from phpMyAdmin into a the new Podiobooks Schema
+
+##############################################
+#######--------Partner Importer-------########
+##############################################
+"""
+
+import csv # first we need import necessary lib:csv
+from podiobooks.main.models import *
+from django.template.defaultfilters import slugify
+
+def importPartners():
+    # Now, begin reading in the CSV and using the Django model objects to populate the DB
+    
+    #Open Partner File for Import
+    partnerCSVFile=open("podiobooks_legacy_partner_table.csv")
+    
+    #Parse the Partner File CSV into a dictionary based on the first row values
+    partnerCSVReader=csv.DictReader(partnerCSVFile,dialect='excel')
+    
+    #PRE CLEANOUT
+    Partner.objects.all().delete()
+    
+    # Loop through the rest of the rows in the CSV
+    for row in partnerCSVReader:
+        print row
+        
+        # Create an object in the database based on the current row
+        partner = Partner.objects.create (
+            id = row['ID'],
+            old_id = row['ID'],
+            name = row['Name'].replace('\\',''),
+            url = row['URL'],
+            logo = row['Logo'],
+            deleted = False,
+            date_created = row['datecreated'],
+        )
+        print "Partner: %s" % (partner.name)
+    
+    partnerCSVFile.close()
+
+##### MAIN FUNCITON TO RUN IF THIS SCRIPT IS CALLED ALONE ###
+if __name__ == "__main__":
+    importPartners()
+    
+    
+# HANDY MAPPING REFERENCE
+# TITLE MODEL FIELDS
+#name = models.CharField(blank=False, max_length=255)
+#url = models.URLField(blank=False, verify_exists=True)
+#logo = models.ImageField(upload_to="/dir/path")
+#deleted = models.BooleanField(default=False)
+#old_id = models.IntegerField(blank=True, null=True)
+#date_created = models.DateTimeField(blank=False, default=datetime.datetime.now())
+#date_updated = models.DateTimeField(blank=False, default=datetime.datetime.now())
+
+# BOOK CSV FIELDS
+#"ID",
+#"Name",
+#"URL",
+#"Logo",
+#"CSS",
+#"HasLibrary",
+#"enabled",
+#"datecreated",
+#"HeaderHTML",
+#"FooterHTML"
