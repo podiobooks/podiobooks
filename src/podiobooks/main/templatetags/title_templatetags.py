60ed056dfcadcd68645fb9d249d6fbf7df836075
Tim White <tim@cyface.com>
2009-12-15 13:57:23 -0700

    Moved title cover generation into custom tag.
    Former-commit-id: 22775094aeb52c8acf1dbbda6b1c1c663ad54c88
    Former-commit-id: 1b2eb260b4a4f1fec298aac40e7aa0ee2fd5b54f
    Former-commit-id: a50355eef02b18071b4e9833fdf7a6fbdc752a49
    Former-commit-id: a6378532caf5280dbb0f81431e2a732254852ea5
    Former-commit-id: a6f99606efa839976b49b63f7491e1571e553d92
    Former-commit-id: c709ea134f32c2438cabf371719dec8faa163e3d

--- src/podiobooks/main/templatetags/title_templatetags.py
+++ src/podiobooks/main/templatetags/title_templatetags.py
@@ -11,3 +11,9 @@ def show_categories(title):
 def show_contributors(title):
     contributors = title.contributors.all()
     return { 'contributors' : contributors }
+
+# takes_context makes MEDIA_URL available inside the tag
+@register.inclusion_tag('main/title/tags/show_titlecover.html', takes_context=True)
+def show_titlecover(title):
+    title = title
+    return { 'title' : title}
