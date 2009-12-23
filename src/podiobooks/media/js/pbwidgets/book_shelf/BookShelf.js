bff08ae4e4a36bfcdffd9d1b9758c6445727e8e5
Tim White <tim@cyface.com>
2009-12-23 13:28:20 -0700

    Simplified fades; still can't get slides to work.

--- src/podiobooks/media/js/pbwidgets/book_shelf/BookShelf.js
+++ src/podiobooks/media/js/pbwidgets/book_shelf/BookShelf.js
@@ -5,30 +5,15 @@ dojo.require("dijit.layout.ContentPane");
 dojo.declare( "pbwidgets.book_shelf.BookShelf",
 	[dijit.layout.StackContainer],
 {
-	_setupChild: function(/*dijit._Widget*/ child){
-		this.inherited(arguments);
-
-		// Start the nodes with 0 opacity so the fade in works the first time
-		dojo.style(child.domNode,"opacity",0);
-	},
-	
 	_showChild: function(/*dijit._Widget*/ page){
 		
+		this.inherited(arguments);
+		
+		dojo.style(page.domNode,"opacity",0);
+		
 		dojo.fadeIn({ 
-        	node: page.domNode,
-			duration:500
+	        node: page.domNode,
+			duration:500,
 		}).play();
-		
-		this.inherited(arguments);
-	},
-
-	_hideChild: function(/*dijit._Widget*/ page){
-		
-		dojo.fadeOut({ 
-        	node: page.domNode,
-			duration:500
-		}).play();
-		
-		this.inherited(arguments);
 	},
 } );
