c4bd9c22a64a8f5379a813b19ca3775b24159f23
Tim White <tim@cyface.com>
2009-12-25 11:29:41 -0700

    Moved shelf out into a tag.  Added TypeKit to base.html to pull in specialty font.  CSS Cleanup.  Added TEMPLATE_MEDIA_URL to settings.
    Former-commit-id: f19ece6c0089a029dde8188001c2eecfa5f4beca
    Former-commit-id: 95d2e2719e5c2c017d9c7aa8ee7b755ba819c6ca
    Former-commit-id: 91d1c91e65d0040d9194de173623b22d2faa710b
    Former-commit-id: b58b3db1008a5af6e641dc0f020111018e6702a6
    Former-commit-id: 15bf031c8bc51491da1f4e9d33e9979a7690c540

--- src/podiobooks/main/templatetags/main_templatetags.py
+++ src/podiobooks/main/templatetags/main_templatetags.py
@@ -1,7 +1,7 @@
 from django import template
 from podiobooks.main.models import *
 from podiobooks.main.views import *
-from podiobooks.settings import MEDIA_URL
+from podiobooks.settings import MEDIA_URL, THEME_MEDIA_URL
 register = template.Library()
 
 @register.inclusion_tag('main/tags/show_heading.html')
@@ -11,4 +11,17 @@ def show_heading(text):
 @register.inclusion_tag('main/tags/show_searchbox.html')
 def show_searchbox():
     return { 'categoryChoiceForm':CategoryChoiceForm(), 'titleQuickSearchForm': TitleQuickSearchForm(), 'MEDIA_URL': MEDIA_URL}
+
+@register.inclusion_tag('main/tags/show_shelf.html')
+def show_shelf(shelf_id, shelf_title, title_list, dropdown_values, dropdown_url, base_css_class, shelf_title_width):
+    return {'shelf_id': shelf_id,
+            'shelf_title': shelf_title,
+            'title_list': title_list,
+            'dropdown_values': dropdown_values,
+            'dropdown_url': dropdown_url,
+            'base_css_class': base_css_class,
+            'shelf_title_width': shelf_title_width,
+            'MAIN_TEMPLATE_THEME': THEME_MEDIA_URL,
+            'MEDIA_URL': MEDIA_URL
+            }
 
