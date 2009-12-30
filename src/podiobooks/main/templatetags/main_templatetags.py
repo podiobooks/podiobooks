d0bec5aaf4ddaf817928fd5acfde626289ad25fe
Tim White <tim@cyface.com>
2009-12-30 15:11:28 -0700

    Added ability to cycle through a list of hostnames for the covers.

--- src/podiobooks/main/templatetags/main_templatetags.py
+++ src/podiobooks/main/templatetags/main_templatetags.py
@@ -1,8 +1,19 @@
 from django import template
 from podiobooks.main.models import *
 from podiobooks.main.views import *
-from podiobooks.settings import MEDIA_URL, THEME_MEDIA_URL, COVER_MEDIA_URL
+from podiobooks.settings import MEDIA_URL, THEME_MEDIA_URL, COVER_MEDIA_URLS
+import itertools
+
 register = template.Library()
+
+@register.simple_tag
+def cover_media_url():
+    """
+    Cycles through the values of COVER_MEDIA_URLS to enable increased parallel download speed.
+    """
+    if not hasattr(cover_media_url, 'state'):
+        cover_media_url.state = itertools.cycle(COVER_MEDIA_URLS)
+    return cover_media_url.state.next()
 
 @register.inclusion_tag('main/tags/show_heading.html')
 def show_heading(text):
@@ -21,7 +32,6 @@ def show_shelf(shelf_id, shelf_title, title_list, dropdown_values, dropdown_url,
             'dropdown_url': dropdown_url,
             'base_css_class': base_css_class,
             'shelf_title_width': shelf_title_width,
-            'COVER_MEDIA_URL': COVER_MEDIA_URL,
             'THEME_MEDIA_URL': THEME_MEDIA_URL,
             'MEDIA_URL': MEDIA_URL
             }
