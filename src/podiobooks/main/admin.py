0b285b2d661188d9afd984b834990939ac9986c1
Tim White <tim@cyface.com>
2010-01-12 18:47:50 -0700

    After rebuilding server with 100% python2.5, instead of the 2.5/2.6 mix, going to try and re-add the social stuff...
    Former-commit-id: 057ecd20ed6fe2a218aa610384de82055d44b8a7
    Former-commit-id: 02110d4c4e31bc5191b2308f0257f9ff36aabc5d
    Former-commit-id: 163e7df35936eb0f9798d3cd66ed046c62b44e80
    Former-commit-id: 4b4351b988c95c3935681b5b7a7a58a09d896e33
    Former-commit-id: 09768949e49acbcfbd4ccc287e03daa80042e2b9
    Former-commit-id: 35d128b7bf99549adc6a2028d02bd75aaccc0053

--- src/podiobooks/main/admin.py
+++ src/podiobooks/main/admin.py
@@ -69,6 +69,19 @@ class TitleAdmin(admin.ModelAdmin):
     save_on_tap = True
     search_fields = ["name"]
 
+class TinyMCEFlatPageAdmin(FlatPageAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'content':
+            return forms.CharField(widget=TinyMCE(
+                attrs={'cols': 80, 'rows': 30},
+                mce_attrs={'external_link_list_url': reverse('tinymce.views.flatpages_link_list'),
+                           'theme' : 'advanced'},
+            ))
+        return super(TinyMCEFlatPageAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, TinyMCEFlatPageAdmin)
+
 admin.site.register(Award)
 admin.site.register(Advisory)
 admin.site.register(Category,CategoryAdmin)
