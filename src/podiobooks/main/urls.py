c8740827e2cb7267e8c2a5c86509f38b78c1fb63
Tim White <tim@cyface.com>
2010-01-15 20:28:13 -0700

    Split the JSON stuff into it's own urls-json.py.

--- src/podiobooks/main/urls.py
+++ src/podiobooks/main/urls.py
@@ -1,16 +1,5 @@
 from django.conf.urls.defaults import * #@UnusedWildImport
 from models import Category, Contributor, Episode, Title
-from contrib.django_restapi.model_resource import Collection
-from contrib.django_restapi.responder import JSONResponder
-
-# Create a handler for RESTful access to the Title Object
-# Info At: http://code.google.com/p/django-rest-interface/wiki/RestifyDjango
-# @TODO: Consider Piston Framework instead for API layer
-title_resource = Collection(
-    queryset = Title.objects.all(),
-    permitted_methods = ('GET',),
-    responder = JSONResponder()
-)
 
 urlpatterns = patterns('',
     
@@ -21,8 +10,6 @@ urlpatterns = patterns('',
     url(r'^title/search/redirect$','podiobooks.main.views.title_search', name='title_search_redirect'),
     url(r'^title/summary/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail_summary.html'}, name='title_detail_summary'),
     url(r'^title/snippet/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail_snippet.html'}, name='title_detail_snippet'),
-    url(r'^title/json/(.*?)/?$', title_resource, name='json_title_detail'),
-    url(r'^title/json/$', title_resource, name='json_title_list'),
     url(r'^title/ajaxtest/', 'django.views.generic.simple.direct_to_template', {'template': 'main/title/ajax_test.html',}),
     url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail.html'}, name='title_detail'),
     
@@ -38,4 +25,7 @@ urlpatterns = patterns('',
     
     # episode
     url(r'^episode/(?P<id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Episode.objects.all(), 'template_object_name': 'episode', 'template_name': 'main/episode/detail.html'}, name='episode_detail'),
+    
+    # JSON API
+    (r'^json/', include('podiobooks.main.urls-json')),
 )
