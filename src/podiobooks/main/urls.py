7eae5af413d50d55f9b11cea429995be4799f4fa
Tim White <tim@cyface.com>
2010-01-22 20:34:38 -0700

    Massive rename to prepend model name to template names.  Added new browse by...drop down.
    Former-commit-id: 77d8518aff5422f35eab7e66ef2f8e845a5a2d33
    Former-commit-id: f9e4b9c94290170e6831ef1e2a1899b12ea89024
    Former-commit-id: 558cefad2fc7cc4d187ad31a61bb935b5a656056
    Former-commit-id: c69aae5eb43ca30451c90df97ff0dc8f14d0fc82
    Former-commit-id: 792ac8f9f9643cd9441a7543928343ba058a390c
    Former-commit-id: aa232b2fa1799c7287219ac43fb404a59499d91f

--- src/podiobooks/main/urls.py
+++ src/podiobooks/main/urls.py
@@ -4,27 +4,27 @@ from models import Category, Contributor, Episode, Title
 urlpatterns = patterns('',
     
     # title
-    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all().order_by('name'), 'template_object_name': 'title', 'template_name': 'main/title/list.html'}, name='title_list'),
+    url(r'^title/$','django.views.generic.list_detail.object_list', { 'queryset': Title.objects.all().order_by('name'), 'template_object_name': 'title', 'template_name': 'main/title/title_list.html'}, name='title_list'),
     url(r'^title/search/$','podiobooks.main.views.title_search', name='title_search'),
     url(r'^title/search/(?P<keywords>[^/]+)/$','podiobooks.main.views.title_search', name='title_search_keywords'),
-    url(r'^title/summary/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail_summary.html'}, name='title_detail_summary'),
-    url(r'^title/snippet/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail_snippet.html'}, name='title_detail_snippet'),
+    url(r'^title/summary/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/title_detail_summary.html'}, name='title_detail_summary'),
+    url(r'^title/snippet/(?P<object_id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/title_detail_snippet.html'}, name='title_detail_snippet'),
     url(r'^title/ajaxtest/', 'django.views.generic.simple.direct_to_template', {'template': 'main/title/ajax_test.html',}),
-    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/detail.html'}, name='title_detail'),
+    url(r'^title/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Title.objects.all(), 'template_object_name': 'title', 'template_name': 'main/title/title_detail.html'}, name='title_detail'),
     
     # category
-    url(r'^category/$','django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all().order_by('name'), 'template_object_name': 'category', 'template_name': 'main/category/list.html'}, name='category_list'),
+    url(r'^category/$','django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all().order_by('name'), 'template_object_name': 'category', 'template_name': 'main/category/category_list.html'}, name='category_list'),
     url(r'^category/redirect/$', 'podiobooks.main.views.category_redirect', name='category_redirect'),
-    url(r'^category/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all(), 'template_object_name': 'category', 'template_name': 'main/category/detail.html'}, name='category_detail'),
-    url(r'^category/shelf/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all(), 'template_object_name': 'category', 'template_name': 'main/category/shelf.html'}, name='category_shelf'),
+    url(r'^category/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all(), 'template_object_name': 'category', 'template_name': 'main/category/category_detail.html'}, name='category_detail'),
+    url(r'^category/shelf/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Category.objects.all(), 'template_object_name': 'category', 'template_name': 'main/category/category_shelf.html'}, name='category_shelf'),
       
     # contributor
-    url(r'^contributor/$','django.views.generic.list_detail.object_list', { 'queryset': Contributor.objects.all().order_by('last_name'), 'template_object_name': 'contributor', 'template_name': 'main/contributor/list.html'}, name='contributor_list'),
-    url(r'^contributor/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Contributor.objects.all(), 'template_object_name': 'contributor', 'template_name': 'main/contributor/detail.html'}, name='contributor_detail'),
-    url(r'^contributor/shelf/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Contributor.objects.all(), 'template_object_name': 'contributor', 'template_name': 'main/contributor/shelf.html'}, name='contributor_shelf'),
+    url(r'^contributor/$','django.views.generic.list_detail.object_list', { 'queryset': Contributor.objects.all().order_by('last_name'), 'template_object_name': 'contributor', 'template_name': 'main/contributor/contributor_list.html'}, name='contributor_list'),
+    url(r'^contributor/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Contributor.objects.all(), 'template_object_name': 'contributor', 'template_name': 'main/contributor/contributor_detail.html'}, name='contributor_detail'),
+    url(r'^contributor/shelf/(?P<slug>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Contributor.objects.all(), 'template_object_name': 'contributor', 'template_name': 'main/contributor/contributor_shelf.html'}, name='contributor_shelf'),
     
     # episode
-    url(r'^episode/(?P<id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Episode.objects.all(), 'template_object_name': 'episode', 'template_name': 'main/episode/detail.html'}, name='episode_detail'),
+    url(r'^episode/(?P<id>[^/]+)/$','django.views.generic.list_detail.object_detail', {'queryset': Episode.objects.all(), 'template_object_name': 'episode', 'template_name': 'main/episode/episode_detail.html'}, name='episode_detail'),
     
     # JSON API
     (r'^json/', include('podiobooks.main.urls-json')),
