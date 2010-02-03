d007f5b36cc883e1e57d37dd6c641617d98ab8b9
Tim White <tim@cyface.com>
2010-02-03 16:35:58 -0700

    Added comment import-from-blog script.
    Former-commit-id: 6709dbbf72f5bef10e379df9c0fe4805b1b00db4
    Former-commit-id: 6be983b60a9a9c9e7141f460497d3572bd017bea
    Former-commit-id: 3d77e9e56c4bf85f3321d5241597631a29fa30e9
    Former-commit-id: 079e27a69c8841792a847e34afb6bf9a83c7f81b
    Former-commit-id: b0059f6839095de83629149b022cbed8e61cfcbf
    Former-commit-id: 503c6d88f36a124a7c6c1cd7b9f7cfadb3bb7f40

--- dbscripts/migrate_comments_from_blog.py
+++ dbscripts/migrate_comments_from_blog.py
@@ -8,16 +8,17 @@ import feedparser
 import podiobooks.settings
 from django.contrib import comments
 from django.contrib.contenttypes.models import ContentType
+from podiobooks.main.models import Title
 
 LOCKFILE = podiobooks.settings.PROJECT_PATH + "update_feeds.lock"
 
-feed_list = ['http://www.podiobooks.com/blog/2007/02/18/quarter-share-by-nathan-lowell/feed/',]
+feed_list = Title.objects.all().filter(podiobooker_blog_url__isnull = False).values('id', 'podiobooker_blog_url')
 
 def update_feeds(verbose=False):
     for feed in feed_list:
         if verbose:
             print feed
-        parsed_feed = feedparser.parse(feed)
+        parsed_feed = feedparser.parse(feed['podiobooker_blog_url'] + 'feed/')
         for entry in parsed_feed.entries:
             title = entry.title.encode(parsed_feed.encoding, "xmlcharrefreplace")
             guid = entry.get("id", entry.link).encode(parsed_feed.encoding, "xmlcharrefreplace")
@@ -30,7 +31,7 @@ def update_feeds(verbose=False):
             title_content_type = ContentType.objects.get(app_label='main', name='title')
             
             new_comment = comments.Comment.objects.get_or_create(comment__iexact=content,
-                 defaults={'comment': content, 'user_name':entry.author, 'submit_date': date_modified, 'content_type': title_content_type, 'object_pk': 130, 'site_id': 1 })
+                 defaults={'comment': content, 'user_name':entry.author[:50], 'submit_date': date_modified, 'content_type': title_content_type, 'object_pk': feed['id'], 'site_id': 1 })
             print new_comment
 
 def main(argv):
