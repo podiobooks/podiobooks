e652bc748e94fb279b8023cca33807dfbff9b8ea
Tim White <tim@cyface.com>
2010-02-10 21:33:06 -0700

    Minimal file.
    Former-commit-id: 1736ccf5273cf79fbedfe82330cec3f0e8c32d0d
    Former-commit-id: 1510f1fd3c04400886764cd768f25d46ba70f182
    Former-commit-id: 01eac7cc4d7fe9cbaf0b73cc0299179c779fc0e6
    Former-commit-id: e352b60ae85af262945e8be3fb0c3c36d8fbe095
    Former-commit-id: deafb7a5f61d106dc383c2bc8a30720c2a0f56e8
    Former-commit-id: 93caf873761c415b4cc25bd464c3c2861ed48b24

--- src/podiobooks/settings_test.py
+++ src/podiobooks/settings_test.py
@@ -1,7 +1,3 @@
-# Django settings for podiobooks project.
-
-# NOTE
-# see local_settings_template.py for instructions on making your local settings file
 from settings import * #@UnusedWildImport
 from settings_local_template import * #@UnusedWildImport
 import tempfile
@@ -12,4 +8,3 @@ DATABASE_NAME = ''
 
 # Test Cache Settings
 CACHE_BACKEND = "file://" + tempfile.gettempdir()
-CACHE_MIDDLEWARE_ANONYMOUS_ONLY = False
