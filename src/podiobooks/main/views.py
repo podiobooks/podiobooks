2131eabe221572ad4907edadb1c7d88b14e228ac
Tim White <tim@cyface.com>
2010-02-10 20:37:32 -0700

    fixed incorrect settings import.

--- src/podiobooks/main/views.py
+++ src/podiobooks/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from podiobooks.main.models import Title
 from podiobooks.main.forms import CategoryChoiceForm, ContributorChoiceForm, TitleSearchForm
-from podiobooks import settings
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.db.models import Q
