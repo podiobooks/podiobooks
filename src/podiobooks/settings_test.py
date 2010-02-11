31175189f5d64efbea5a16b16b7bac3275a83ccc
Tim White <tim@cyface.com>
2010-02-10 20:37:17 -0700

    Redid the way that tests work..trying to avoid having to keep duplicate settings files.
    Former-commit-id: 36c6a9edca0780fcd4e687c2e351ac6518452454
    Former-commit-id: 6639c1e255a59d4c8400827dbb7f604a153c635b
    Former-commit-id: 307702dc6dbae5d388dfeb6d58e702d57bc39659
    Former-commit-id: 6961eeff25e260b83c3ca04e135431c8237f439f
    Former-commit-id: 8dfa6da639c14401a0eb7c9454a4f2ec5ac767e7
    Former-commit-id: 9fe66ff90f0ec02967a0d6778d9451a54fba08ef

--- src/podiobooks/settings_test.py
+++ src/podiobooks/settings_test.py
@@ -2,93 +2,14 @@
 
 # NOTE
 # see local_settings_template.py for instructions on making your local settings file
-from settings_local_test import * #@UnusedWildImport
+from settings import * #@UnusedWildImport
+from settings_local_template import * #@UnusedWildImport
+import tempfile
 
-MANAGERS = ADMINS
+# Test DB settings. (SQLLite)
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = ''
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
-
-#List of callables that add their data to each template
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.auth',
-    'django.core.context_processors.media',
-    'podiobooks.main.context_processors.theme_media',
-    'django.core.context_processors.request',
-    'django_authopenid.context_processors.authopenid',
-    'contrib.site_info_context_processor.site',
-)
-
-if LOCAL_TEMPLATE_CONTEXT_PROCESSORS:
-    TEMPLATE_CONTEXT_PROCESSORS += LOCAL_TEMPLATE_CONTEXT_PROCESSORS
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.gzip.GZipMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_authopenid.middleware.OpenIDMiddleware',
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'django.middleware.http.ConditionalGetMiddleware',
-)
-
-if LOCAL_MIDDLEWARE_CLASSES:
-    MIDDLEWARE_CLASSES += LOCAL_MIDDLEWARE_CLASSES
-
-ROOT_URLCONF = 'podiobooks.urls'
-
-# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-# Always use forward slashes, even on Windows.
-# Don't forget to use absolute paths, not relative paths.
-TEMPLATE_ROOT = PROJECT_PATH + '/templates'
-TEMPLATE_DIRS = (TEMPLATE_ROOT, TEMPLATE_ROOT + '/base')
-MAIN_TEMPLATE_THEME = 'base'
-if TEMPLATE_THEMES:
-    for theme in reversed(TEMPLATE_THEMES):
-        TEMPLATE_DIRS = (TEMPLATE_ROOT + '/themes/' + theme,) + TEMPLATE_DIRS
-    MAIN_TEMPLATE_THEME = TEMPLATE_THEMES[0]
-THEME_MEDIA_URL = MEDIA_URL +'themes/' + MAIN_TEMPLATE_THEME + '/'
-
-AUTH_PROFILE_MODULE = 'main.UserProfile'
-
-#authopenid
-ugettext = lambda s: s
-LOGIN_URL = '/%s%s' % (ugettext('account/'), ugettext('signin/'))
-LOGIN_REDIRECT_URL = '/'
-OPENID_SREG = {
-    "required": ['fullname', 'country']
-}
-
-#django_registration
-ACCOUNT_ACTIVATION_DAYS = 14
-
-INSTALLED_APPS = (
-    'contrib.django_restapi',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.auth',
-    'django.contrib.comments',
-    'django.contrib.contenttypes',
-    'django.contrib.databrowse',
-    'django.contrib.flatpages',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django_authopenid',
-    'faq',
-    'haystack',
-    'tagging',
-    'memcache_status',
-    'podiobooks.author',
-    'podiobooks.feeds',
-    'podiobooks.main',
-    'podiobooks.social',
-    'registration',
-    'tinymce',
-)
+# Test Cache Settings
+CACHE_BACKEND = "file://" + tempfile.gettempdir()
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = False
