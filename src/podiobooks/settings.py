92fe3aff9d15769f27bfa8bbdbe5cdbeaf25db16
Tim White <tim@cyface.com>
2010-02-27 13:00:39 -0700

    Added NameError type.
    Former-commit-id: c19ee44bf5062bef6819914c0b810987c48a4ed0
    Former-commit-id: c3e5a48fedb5cc133c0e66ae533c00de3b4a3f34
    Former-commit-id: 9fb650ff30546cae660eeb2672691d9f8001226c
    Former-commit-id: db55c6ec3573d5b238ecd1caa6a2e68d167deed3
    Former-commit-id: e501e05ac79b796abbf32ea7905a3489f3afcd71
    Former-commit-id: 6701ef41a34311fc4f877fb19f3d9978cf6e177d

--- src/podiobooks/settings.py
+++ src/podiobooks/settings.py
@@ -11,6 +11,6 @@ from podiobooks.settings_main import * #@UnusedWildImport
 
 try:
     from settings_local import * #@UnusedWildImport
-except:
+except NameError:
     from settings_local_template import * #@UnusedWildImport
     print("WARNING!  Using default Settings")
