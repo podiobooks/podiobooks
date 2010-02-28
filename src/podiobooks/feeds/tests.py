e7036493ba018a17b7a8d04cb2cfd6432147ab11
Tim White <tim@cyface.com>
2010-02-27 23:36:33 -0700

    Added tests for feeds.
    Former-commit-id: cd2e848c1177e391aa838fe06e1493bea0dccc33
    Former-commit-id: eb59d687055cf96b69e2a8c97f93080925e4d4e3
    Former-commit-id: dcac2f330eeefb8d0dfb6bf7d341c0461466dcdb
    Former-commit-id: e9587dccf8c4e78ca111a1b9cf87639c9fb5595c
    Former-commit-id: 09176a9882d5b09a65b8b184f336ee6f51030e7b
    Former-commit-id: 5ab69a7d9f70f529f1c82a9fd0cb8038bfddb926

---
+++ src/podiobooks/feeds/tests.py
@@ -0,0 +1,20 @@
+"""Automated Tests of the Podiobooks Feed URLs"""
+
+# pylint: disable-msg=C0103,C0111,R0904
+
+from django.test import TestCase
+from django.test.client import Client
+
+class FeedUrlTestCase(TestCase):
+    fixtures = ['test_data.json', ]
+    
+    def setUp(self):
+        self.c = Client()
+    
+    def testEpisodeFeed(self):
+        response = self.c.get('/rss/feeds/episodes/double-share/')
+        self.assertEquals(200, response.status_code)
+        
+    def testTitlesFeed(self):
+        response = self.c.get('/rss/feeds/titles/')
+        self.assertEquals(200, response.status_code)
