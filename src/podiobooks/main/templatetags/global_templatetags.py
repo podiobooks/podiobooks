cb921aa7121fd5e89398cfc2d6a3c072bb09eeba
Tim White <tim@cyface.com>
2010-03-29 18:31:28 -0600

    jQuery Branch.
    Former-commit-id: 8daff91c80a987687d169c25ab44ee9080c8f330
    Former-commit-id: 5fc5041363f360122ebceb2e0dac68121215456a
    Former-commit-id: c2960de965cd7ffa612be8eaef1ef2db5070151a
    Former-commit-id: 14fe52b2b50279e3a25743eaf9a8d9e2d45ac91e
    Former-commit-id: 16c8242e38a4ea11c08fb13fec3056db1453636b
    Former-commit-id: edcd01029e678ac20c48521a5b1c80fe78eb0192

--- src/podiobooks/main/templatetags/global_templatetags.py
+++ src/podiobooks/main/templatetags/global_templatetags.py
@@ -46,3 +46,36 @@ def show_variable(variable): # pragma: nocover
         result[var] = getattr(variable, var)
         
     return { 'result': result, 'MEDIA_URL': settings.MEDIA_URL }
+
+@register.filter
+def truncatewords_afterchar(value, limit=80):
+    """
+    Truncates a string after a given number of chars keeping whole words.
+    
+    Usage:
+        {{ string|truncatesmart }}
+        {{ string|truncatesmart:50 }}
+    """
+    
+    try:
+        limit = int(limit)
+    # invalid literal for int()
+    except ValueError:
+        # Fail silently.
+        return value
+    
+    # Make sure it's unicode
+    value = unicode(value)
+    
+    # Return the string itself if length is smaller or equal to the limit
+    if len(value) <= limit:
+        return value
+    
+    # Cut the string
+    value = value[:limit]
+    
+    # Break into words and remove the last
+    words = value.split(' ')[:-1]
+    
+    # Join the words and return
+    return ' '.join(words) + '...'
