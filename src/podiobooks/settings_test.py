00f0b3d60e7cdcfd0c50f6f1243c24db704bff7c
Tim White <tim@cyface.com>
2010-06-20 23:08:37 -0600

    Changed back to old way of calling test runner, since coverage seems to have broken with new way.
    Former-commit-id: b62257db117be1c7de99ab9e18b0bee0a845e72f
    Former-commit-id: 5a34e69865ed18362626abc55434be95817488f6
    Former-commit-id: dbfa663ba1cb97ce84569d4f71c585256279f0dc
    Former-commit-id: ea9523d5a8499ccdd2f918374fccb8c7ae05995c
    Former-commit-id: 607998fb29b9b863c2ebaa04b08a41b7b62b1772
    Former-commit-id: 469043324dacc4fe51e93fb3fbc8ca936438f4e0

--- src/podiobooks/settings_test.py
+++ src/podiobooks/settings_test.py
@@ -20,4 +20,5 @@ INSTALLED_APPS += (
     'django_nose',
 )
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner' # Should work with Django 1.2.1, but coverage is broken
+TEST_RUNNER = 'django_nose.run_tests'
