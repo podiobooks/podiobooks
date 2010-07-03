1a8ec13fc52a9707201c21bf74c3c57f4ba0fbd6
Tim White <tim@cyface.com>
2010-07-02 19:45:46 -0600

    Fixed calculation of boolean values on import.
    Former-commit-id: 90ab321305e32bf3d09cf47af77d89dc81fddb65
    Former-commit-id: 03008da977be3e1f55c69fcd15c01707fd21121c
    Former-commit-id: 0e68b4888bb61ae2e0d228af5b5caa34b183615e
    Former-commit-id: 0a4b8293817e7a376408cdd6c05c4dbc9e8f0e20
    Former-commit-id: b41011d17c47718b9fa769bb0b440a9ad3e5d7ce
    Former-commit-id: 80a6e0fbe09b054ff4ca08563e150cadc1fd24d0

--- dbscripts/migrate_bookcsv_to_title.py
+++ dbscripts/migrate_bookcsv_to_title.py
@@ -35,10 +35,10 @@ def booleanClean(data):
     if (data == None) or (data == ''):
         return 0
     else:
-        if int(data) == 1:
-            return 1
+        if int(data) >= 1:
+            return True
         else:
-            return 0
+            return False
     
 def getOrCreateAward(awardSlug):
     """Retrieves or creates an Award type based on the slug of the award"""
