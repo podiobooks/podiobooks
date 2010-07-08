bbf14397c5accb23b1bb940d7f7ce63671ffd521
Tim White <tim@cyface.com>
2010-07-07 22:23:27 -0600

    Added first pass at custom feed.

--- src/podiobooks/feeds/feeds.py
+++ src/podiobooks/feeds/feeds.py
@@ -7,7 +7,9 @@ from django.contrib.syndication.views import Feed
 
 from django.utils.html import strip_tags
 
-from podiobooks.main.models import Title, Episode
+from podiobooks.main.models import Title, Episode, TitleSubscription
+
+from django.contrib.auth.models import User
 
 from podiobooks.feeds.protocols.itunes import ITunesFeed
 from django.utils.feedgenerator import Rss201rev2Feed
@@ -15,6 +17,8 @@ from django.utils.feedgenerator import Rss201rev2Feed
 from podiobooks import settings
 
 from podiobooks.feeds import feed_tools
+
+from datetime import timedelta, datetime
 
 class TitleFeed(Feed):
     """A simple feed that lists recent Titles"""
@@ -142,3 +146,41 @@ class EpisodeFeed(Feed):
     
     def title(self, obj):
         return obj.name
+    
+class CustomTitleSubscriptionFeed(EpisodeFeed):
+    
+    def get_subscription(self):
+        return self.subscription
+    
+    def get_object(self, request, title_slug, username):
+        title = Title.objects.get(slug__exact=title_slug)
+        user = User.objects.get(username=username)
+        subscription = TitleSubscription.objects.get(title=title, user=user)
+        self.subscription = subscription
+        
+        return title
+
+    def items(self, obj):
+        date_diff = datetime.now() - self.subscription.date_created
+        episodes_to_show = date_diff.days / self.subscription.day_interval
+        
+        # Always show at least one episode rule
+        if episodes_to_show < 1:
+            episodes_to_show = 1
+        
+        # Make sure they haven't manually released episodes rule
+        if episodes_to_show < self.subscription.last_downloaded_episode.sequence:
+            episodes_to_show = self.subscription.last_downloaded_episode.sequence
+            
+        # Update last_downloaded_episode if we are releasing a new one rule
+        if episodes_to_show > self.subscription.last_downloaded_episode.sequence:
+            try:
+                next_episode = Episode.objects.get(title__id__exact=obj.id, sequence=self.subscription.last_downloaded_episode.sequence + 1)
+                self.subscription.last_downloaded_episode = next_episode
+            except:
+                pass # likely because we're at the end of the book
+                
+            self.subscription.last_downloaded_date = datetime.now()
+            self.subscription.save()
+            
+        return Episode.objects.filter(title__id__exact=obj.id, sequence__lte=episodes_to_show).order_by('-sequence')
