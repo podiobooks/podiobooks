0fb272450d61b15dad065532a462824483971e6f
Tim White <tim@cyface.com>
2010-07-07 22:23:27 -0600

    Added first pass at custom feed.
    Former-commit-id: bbf14397c5accb23b1bb940d7f7ce63671ffd521
    Former-commit-id: ed1170d9a757e903e7af971ad14b3223aac276b3
    Former-commit-id: 0763d3d3c758d66c8514adf459fe9373d6ea46c7
    Former-commit-id: 5a8ad9f23f89208cfb4b3838766060cd742bdd0e
    Former-commit-id: f6487c0c1ed63ea0539c05ff55c1571e8baba1de
    Former-commit-id: 9288a6c1184d71212136d565b1cb762bd83a5d93

--- src/podiobooks/feeds/urls.py
+++ src/podiobooks/feeds/urls.py
@@ -1,7 +1,7 @@
 """URLs for the RSS Feeds module"""
 
 from django.conf.urls.defaults import patterns
-from podiobooks.feeds.feeds import TitleFeed, EpisodeFeed
+from podiobooks.feeds.feeds import TitleFeed, EpisodeFeed, CustomTitleSubscriptionFeed
 
 # pylint: disable=E0602,F0401
 
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # feeds
     (r'^feeds/titles/$', TitleFeed()),
     (r'^feeds/episodes/(?P<title_slug>[^/]+)/$', EpisodeFeed()),
+    (r'^feeds/episodes/(?P<title_slug>[^/]+)/(?P<username>[^/]+)/$', CustomTitleSubscriptionFeed()),
 )
