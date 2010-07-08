f693961a1cc74253c6a3d735189fdbb91c6cbc9f
Tim White <tim@cyface.com>
2010-07-07 22:22:30 -0600

    Added custom save method to user profile to generate slug automatically.

--- src/podiobooks/profile/models.py
+++ src/podiobooks/profile/models.py
@@ -4,15 +4,22 @@ from __future__ import division
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from django.template.defaultfilters import slugify
 
 
 class UserProfile(models.Model):
     """Information about the user which we need for preferences, social needs,
     etc."""
-    user = models.ForeignKey(User, related_name='userprofiles') #User is an OOTB Django Auth Model
+    user = models.ForeignKey(User, related_name='userprofile') #User is an OOTB Django Auth Model
     slug = models.SlugField()
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
         
     def __unicode__(self):
         return "UserProfile"
+    
+    def save(self):
+        if not self.id:
+            self.slug = slugify(self.user.username)
+
+        super(UserProfile, self).save()
