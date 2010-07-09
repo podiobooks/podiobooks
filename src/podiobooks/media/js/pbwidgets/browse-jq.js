5a61b20a930862cf26fb874ff2bdbdb24c677de0
Tim White <tim@cyface.com>
2010-07-09 08:19:22 -0600

    Moved jq css to css dir.

--- src/podiobooks/media/js/pbwidgets/browse-jq.js
+++
@@ -1,5 +0,0 @@
-function populateContributorBrowseLists() {
-	console.log('bob');
-	$('#pb-header-menubar').append("<div id='pb-browse-lists'></div>");
-	$('#pb-browse-lists').load('/contributor');
-}
