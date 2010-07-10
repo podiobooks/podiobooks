e19609cc186f42cf711eecdcdf8a2c62c124a091
Tim White <tim@cyface.com>
2010-07-10 10:49:27 -0600

    Reordered FlatpageFallbackMiddleware to hopefully get it to cache.
    Former-commit-id: 2c70a78255848c86c9d3eb3ff742babb9ee3d6db
    Former-commit-id: d1f6b3b30f7c66f04c77c641f07e61dc29e08940
    Former-commit-id: bc716138dfc4fb5ced6d273585ab1ea13fc12b7d
    Former-commit-id: 8a7728fe724d0e357d282a2e42d984fa500e3a10
    Former-commit-id: 2174bfc7b89787deb43ae61fd423b63d4c23fbd5
    Former-commit-id: b7b3f5de89ba31f4b00d9126ceda287a94382769

--- src/podiobooks/settings_main.py
+++ src/podiobooks/settings_main.py
@@ -23,9 +23,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_authopenid.middleware.OpenIDMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
 )
