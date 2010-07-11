65a4fe122fb902f9d43116cbf694c48976d0979a
Tim White <tim@cyface.com>
2010-07-10 22:26:59 -0600

    Refactored all the subscription stuff over into it's own module, added a separate page to view/manage subscriptions.
    Former-commit-id: 75e200845def4852ea1ff2159c67eacb34202961
    Former-commit-id: 8c3969b61b667a360b92bad435429aab7891922b
    Former-commit-id: 04da27357672980c6b018e013decd70aff20935d
    Former-commit-id: 5d9d9539c719c484b2a04f24c34bc8aa3892c65c
    Former-commit-id: a01bd3ca0c91696fe8fdf4420e6e1b6127ef1e6b
    Former-commit-id: f3e82e82d582abe740dd465c1ffa60658342a838

--- src/podiobooks/feeds/urls.py
+++ src/podiobooks/feeds/urls.py
@@ -10,6 +10,4 @@ urlpatterns = patterns('',
     (r'^feeds/titles/$', TitleFeed()),
     (r'^feeds/episodes/(?P<title_slug>[^/]+)/$', EpisodeFeed()),
     (r'^feeds/episodes/(?P<title_slug>[^/]+)/(?P<username>[^/]+)/$', CustomTitleSubscriptionFeed()),
-    (r'^feeds/episodes/release/one/(?P<title_slug>[^/]+)/$', 'podiobooks.feeds.views.release_one_episode'),
-    (r'^feeds/episodes/release/all/(?P<title_slug>[^/]+)/$', 'podiobooks.feeds.views.release_all_episodes'),
-)
+ )
