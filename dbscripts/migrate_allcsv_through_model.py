257d910055f64c82e0263899431ca64cded33073
Tim White <tim@cyface.com>
2010-07-11 10:33:20 -0600

    trying to fix pathing issues.
    Former-commit-id: 20e1a6574b42cdf51abf7b9cd9d3c3745ab8d12e
    Former-commit-id: 68afc2d8e6174237812fa7b99b693fa032811405
    Former-commit-id: 038b44b8831785b1a921a6ab740116472353cda9
    Former-commit-id: aa6f3c38dda1b4227fb6030e20f8c96921893150
    Former-commit-id: a8599797a899c230c82945363fd19c4727b4c911
    Former-commit-id: 9b9d55e60ecd928b1d63383d55caae4a9c1a33d2

--- dbscripts/migrate_allcsv_through_model.py
+++ dbscripts/migrate_allcsv_through_model.py
@@ -1,6 +1,5 @@
 """This script reads in an "CSV for Excel" export from phpMyAdmin into a the new Podiobooks Schema"""
 
-from pb1_csv_migration import *
 from data_cleanup import *
 
 # IMPORT CATEGORIES
@@ -8,18 +7,18 @@ from pb1_csv_migration.migrate_bookcategorycsv_to_category import *
 importBookCategoriesFromCSV()
 
 # IMPORT PARTNERS
-from migrate_partnercsv_to_partner import *
+from pb1_csv_migration.migrate_partnercsv_to_partner import *
 importPartnersFromCSV()
 
 # IMPORT BOOKS
-from migrate_bookcsv_to_title import *
+from pb1_csv_migration.migrate_bookcsv_to_title import *
 importBooksFromCSV()
 
 # IMPORT CHAPTERS
-from migrate_chaptercsv_to_episode import *
+from pb1_csv_migration.migrate_chaptercsv_to_episode import *
 importChaptersFromCSV()
 
 # IMPORT RATINGS
-from migrate_bookratingcsv_to_title import *
+from pb1_csv_migration.migrate_bookratingcsv_to_title import *
 importRatingsFromCSV()
  
