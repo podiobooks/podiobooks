380ac78d0562cee7b73c9b0fcd8bb1e9e7bca356
Tim White <tim@cyface.com>
2011-02-06 18:38:50 -0700

    Updated contributor data cleanup stuff for latest books.
    Former-commit-id: 1c0b3630891a6a63ef321a2a3d7846c696c1f751
    Former-commit-id: ccedc6cf70e4f00896c11d3705b5d145a9caf4ad
    Former-commit-id: ef171a210c3b452956c2b3ccf06a9bbe1a8131c3
    Former-commit-id: 84d746f0ff96093cd74422c201df57a24f46b934
    Former-commit-id: 7be0aad4de38b49c4a4e902bcf9220d376d637f1
    Former-commit-id: 3658f569b102cc93aa1a84d43bb6b6bbaa1388d3

--- dbscripts/data_cleanup/contributor_translation.py
+++ dbscripts/data_cleanup/contributor_translation.py
@@ -7,6 +7,12 @@ contributor_translation = {
                       'Mur Lafferty (editor)': [{'name': 'Mur Lafferty', 'type': 'Editor'}, ],
                       'Jeff Kafer (Editor)': [{'name': 'Jeffrey Kafer', 'type': 'Editor'}, ],
                       'Jeffrey Kafer (Editor)': [{'name': 'Jeffrey Kafer', 'type': 'Editor'}, ],
+                      'Ian G Hulme': [{'name': 'Ian G. Hulme', 'type': 'Author'}, ],
+                      'I G Hulme': [{'name': 'Ian G. Hulme', 'type': 'Author'}, ],
+                      'Tom R Baynham': [{'name': 'Tom R. Baynham', 'type': 'Author'}, ],
+                      'Nathan P Butler': [{'name': 'Nathan P. Butler', 'type': 'Author'}, ],
+                      'Roy and Revel Hession': [{'name': 'Roy Hession', 'type': 'Author'}, {'name': 'Revel Hession', 'type': 'Author'}, ],
+                      'Antonio Libertino, Giuseppe Meligrana': [{'name': 'Antonio Libertino', 'type': 'Author'}, {'name': 'Giuseppe Meligrana', 'type': 'Author'}, ],
                       'Michelle M. Welch, editor': [{'name': 'Michelle M. Welch', 'type': 'Editor'}, ],
                       'Ramona Holliday, narrated by Jeffrey Kafer': [{'name': 'Ramona Holliday', 'type': 'Author'}, {'name': 'Jeffrey Kafer', 'type': 'Narrator'}, ],
                       'Stacey Cochran, narrated by Owen Daly': [{'name': 'Stacey Cochran', 'type': 'Author'}, {'name': 'Owen Daly', 'type': 'Narrator'}, ],
@@ -31,6 +37,14 @@ contributor_translation = {
                       'Jeremy Robinson': [{'name': 'Jeremy Robinson', 'type': 'Author'}, {'name': 'Jeffrey Kafer', 'type': 'Narrator'}, ],
                       'Mike Yachnik': [{'name': 'Mike Yachnik', 'type': 'Author'}, {'name': 'Jeffrey Kafer', 'type': 'Narrator'}, ],
                       'J. J. Hebert': [{'name': 'J.J. Hebert', 'type': 'Author'}, {'name': 'Jeffrey Kafer', 'type': 'Narrator'}, ],
+                      'Scott Sigler and Matt Wallace': [{'name': 'Scott Sigler', 'type': 'Author'}, {'name': 'Matt Wallace', 'type': 'Author'}, ],
+                      'Katharina Maimer and Mick Bordet': [{'name': 'Katharina Maimer', 'type': 'Author'}, {'name': 'Mick Bordet', 'type': 'Author'}, ],
+                      'Vic Zarley and Eva Zarley': [{'name': 'Vic Zarley', 'type': 'Author'}, {'name': 'Eva Zarley', 'type': 'Author'}, ],
+                      'H.E. Roulo and Brian Holtz': [{'name': 'H.E. Roulo', 'type': 'Author'}, {'name': 'Brian Holtz', 'type': 'Author'}, ],
+                      'Basil Sands and The Authors of The Killzone Blog': [{'name': 'Basil Sands', 'type': 'Author'}, {'name': 'The Authors of The Killzone Blog', 'type': 'Author'}, ],
+                      'Mike Bennett, narrator': [{'name': 'Mike Bennett', 'type': 'Narrator'}, ],
+                      'Collin Earl and Chris Snelgrove': [{'name': 'Collin Earl', 'type': 'Author'}, {'name': 'Chris Snelgrove', 'type': 'Author'}, ],
+                      'Mercedes Lackey, Dennis Lee, and Cody Martin, with Larry Dixon and Veronica Giguere': [{'name': 'Mercedes Lackey', 'type': 'Author'}, {'name': 'Dennis Lee', 'type': 'Author'}, {'name': 'Larry Dixon', 'type': 'Contributor'}, {'name': 'Veronica Giguere', 'type': 'Contributor'}, ],
                       }
 
 def translate_contributor(legacy_display_name):
