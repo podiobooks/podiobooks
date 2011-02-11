dc2d6e94c1a2d587f9988fdf21a363410448400e
Tim White <tim@cyface.com>
2011-02-11 13:40:19 -0700

    Updated CSV extract/load logic to create and use a CSV cache file of libsyn IDs, so that libsyn API isn't pounded every time we do a dataload.

---
+++ dbscripts/pb1_prod_db_extract/unicode_writer.py
@@ -0,0 +1,37 @@
+"""
+This script reads defines a csv writer that can export particular unicode encodings
+
+##############################################
+###-------Unicode CSV Writer Class-------#####
+##############################################
+"""
+import csv, cStringIO, codecs
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([unicode(s).encode('utf-8', 'ignore') for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8", 'ignore')
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
