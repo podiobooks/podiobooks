12b4321bc3096ca2ffc9dc1aa5fbfdc8477cbd20
Tim White <tim@cyface.com>
2011-02-13 13:49:09 -0700

    Moved JavaScript API Keys into Settings File, added context processor to pull them out.
    Former-commit-id: 35a1de10dd7b7d8b740ac1aa0236df5508f23fb3
    Former-commit-id: 3254a5db432c4e9b615da57c949a63f0c40c94ce
    Former-commit-id: 78ac7c03e3b865596fdbbedfe66a02064b2be86c
    Former-commit-id: ace93ca51f93928fc5a88cc8db8b44da56b3085d
    Former-commit-id: 24ba8b703459c343025b7c9862e5bb49fc62aa6d
    Former-commit-id: 737bb5eb86c3333b4f33f009fce12f6808f03b1d

--- src/podiobooks/main/context_processors.py
+++ src/podiobooks/main/context_processors.py
@@ -1,7 +1,5 @@
 """
 Special Context Processors (adds variables to template contexts) for Podiobooks
-
-Note: for Title Covers, use the {% cover_media_url %} tag in global_templatetags.
 """
 
 from django.conf import settings
@@ -9,6 +7,11 @@ from django.conf import settings
 def theme_media(request): # pylint: disable=W0613
     """
     Adds theme-media-related context variables to the context.
-
     """
     return {'THEME_MEDIA_URL': settings.THEME_MEDIA_URL}
+
+def js_api_keys(request): # pylint: disable=W0613
+    """
+    Adds JavaScript API Keys variables to the context.
+    """
+    return { 'GOOGLE_JS_API_KEY': settings.GOOGLE_JS_API_KEY, 'TYPEKIT_KIT_ID': settings.TYPEKIT_KIT_ID }
