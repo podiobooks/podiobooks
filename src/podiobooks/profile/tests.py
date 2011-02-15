dd2f62454ed028ac75109a5f78c7100e5e6874f7
Tim White <tim@cyface.com>
2011-02-15 12:31:10 -0700

    Updated Profile Tests for new URLs.
    Former-commit-id: 87701530908fe07fd4d65207e6ce36309ef44204
    Former-commit-id: c6185d13dd890d829439492ae32bb827650df791
    Former-commit-id: dc49be588ee9bdd5186f904b2f8a794143a212d6
    Former-commit-id: ec2f7ca0a7e85c67ccf0a47e4c8578f77b3acf6e
    Former-commit-id: 25ce1a58f0245d53b9b57c07505f8de1f322fb87
    Former-commit-id: b2f27ffc6478a7ac9a0f533a4e5076209d8b4246

--- src/podiobooks/profile/tests.py
+++ src/podiobooks/profile/tests.py
@@ -15,15 +15,23 @@ class ProfileTestCase(TestCase):
         self.c = Client()
         self.user1 = User.objects.create_user('testuser1', 'testuser1@test.com', 'testuser1password')
     
-    def testProfileRedirect(self):
+    def testProfileContributorRedirect(self):
         response = self.c.get('/profile/', follow=True)
-        print response
+        self.assertRedirects(response, "http://testserver/contributor/")
+    
+    def testProfileManageRedirect(self):
+        response = self.c.get('/profile/manage/', follow=True)
         self.assertRedirects(response, "http://testserver/account/signin/?next=/profile/")
         
-    def testProfilePage(self):
+    def testProfileManagePage(self):
         self.c.login(username='testuser1', password='testuser1password')
-        response = self.c.get('/profile/')
+        response = self.c.get('/profile/manage/')
         self.assertContains(response, 'testuser1@test.com')
+        
+    def testProfileDetailPage(self):
+        self.c.login(username='testuser1', password='testuser1password')
+        response = self.c.get('/profile/nathan-lowell/')
+        self.assertContains(response, 'Nathan Lowell')
     
     def testUserProfileObj(self):
         self.assertEqual(self.user1.get_profile().slug, 'testuser1')
