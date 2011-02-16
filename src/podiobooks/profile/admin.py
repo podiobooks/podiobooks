dc3541bf9e3cda190dcbb167925df76ce69de097
Tim White <tim@cyface.com>
2011-02-16 15:49:37 -0700

    Updated admin.py for profile to not care about get_full_name.  Not sure what admin testing strategy should be?  Can I just run general URL tests over there?
    Former-commit-id: 6f31c92fd7cacd35c4d2d4658b14a77a79c1e339
    Former-commit-id: b5bfb14bf6349bf15aa97fb8a4bb915e4186244c
    Former-commit-id: 45b6ed5a81e6306eeb8187463ca8f0d0b395a151
    Former-commit-id: c1c962bdd14dfb362af435d9d2942e7d86893c85
    Former-commit-id: 2f1d3294fb4ef07d61aae395226a0e98fc3945d6
    Former-commit-id: 1bd273c41c73e8f257f554ae0c658c2f5b761e41

--- src/podiobooks/profile/admin.py
+++ src/podiobooks/profile/admin.py
@@ -11,7 +11,7 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_links = ('get_user_full_name', )
     exclude = ('date_created', 'date_updated',)
     
-    def get_user_full_name(self, obj):
+    def get_user_full_name(self, obj): # pragma: no cover
         return '%s'%(obj.user.get_full_name())
     get_user_full_name.short_description = 'User Full Name'
 
