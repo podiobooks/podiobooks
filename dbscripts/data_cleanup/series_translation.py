078e8c946df6597e4751c31b8045472f7191dac5
Tim White <tim@cyface.com>
2011-02-16 16:07:17 -0700

    Added latest 7th son and nlowell books to series translation.
    Former-commit-id: 9e3c03b28af3c2eba7b432a9fa63729718b958e1
    Former-commit-id: da3494d20bbddb48fd3132f7d911a75c1aa4ee89
    Former-commit-id: 7e207faa234f1af7c377a5bf7b928e63edbf656e
    Former-commit-id: 9faf8ace648d5db7cd655c60eccb7066cc2abff8
    Former-commit-id: 180fe01ee1bd8e94e18e524970b79841e5074de1
    Former-commit-id: 76c7f5f4dbd77747bcfbcc78a9099572e650bce7

--- dbscripts/data_cleanup/series_translation.py
+++ dbscripts/data_cleanup/series_translation.py
@@ -6,6 +6,7 @@ series_translation = {
                       '53': '7th-son',
                       '103': '7th-son',
                       '192': '7th-son',
+                      '444': '7th-son',
                       '97': 'heaven',
                       '112': 'heaven',
                       '260': 'heaven',
@@ -13,7 +14,9 @@ series_translation = {
                       '130': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
                       '151': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
                       '184': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
-                      #'151': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',], #cpt share
+                      '293': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
+                      '436': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
+                      '582': 'a-traders-tale-from-the-golden-age-of-the-solar-clipper',
                       '237': 'a-shamans-tale-from-the-golden-age-of-the-solar-clipper',
                       }
 
