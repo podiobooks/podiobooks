15a78cd790780611e6dbf8d04a140c9350e34de9
Brant Steen <brant.steen@gmail.com>
2011-02-20 15:29:06 -0500

    see shelf.js for usage
    Former-commit-id: 384bb681a47c360190dab397798424d36c71aff2
    Former-commit-id: ebab039b8369cc1c129f19e6f99bd4ea574e7eee
    Former-commit-id: b7f6f9f28ab0e77184b817645b8aae3e8e8aa2f4
    Former-commit-id: 1058e212a95ada4b293a968650dfcd4205e101f7
    Former-commit-id: 59adcf9c9caadfb45ae4270a523b3826a609de87
    Former-commit-id: ce7d0b0d0d97782de708db6a61159ed5f2bff8c9

--- src/podiobooks/media/themes/pb2-jq/js/shelf.js
+++ src/podiobooks/media/themes/pb2-jq/js/shelf.js
@@ -1,56 +1,6 @@
 $(function(){
-	$(".shelf").each(function(){
-		var shelf = $(this);
-		shelf.height(shelf.height());
-		
-		shelf.addClass('fun-shelf');
-		
-		$.ajax({
-			method:"get",
-			url:"/lazy/featured/",
-			success:function(data){
-				
-				$(data).appendTo("#featured-shelf");
-				
-				$("#featured-shelf>div").wrapAll("<div class='whole-shelf'/>");
-				$("#featured-shelf>.whole-shelf").wrap("<div class='shelf-view'/>");
-				
-				var leftArrow = $("<a class='shelf-arrow shelf-arrow-left' href='#'>previous</a>").appendTo(shelf);
-				var rightArrow = $("<a class='shelf-arrow shelf-arrow-right' href='#'>next</a>").appendTo(shelf);
-		
-				rightArrow.click(function(e){
-					e.preventDefault();
-					if (where < 3){
-						where++;
-						
-						var targ = "-" + (($(".whole-shelf>div:first").width()+12) * where) + "px";
-						
-						$(".whole-shelf").animate({
-							left:targ
-						},600,"easeOutCirc");
-					}
-				});
-				
-				leftArrow.click(function(e){
-					e.preventDefault();
-					if (where > 0){
-						where--;
-						
-						var targ = "-" + (($(".whole-shelf>div:first").width()+12) * where) + "px";
-						
-						$(".whole-shelf").animate({
-							left:targ
-						},600,"easeOutCirc");
-					}
-				});
-				
-			}
-		});
-		
-		
-		
-	});
+	
+	$("#featured-shelf").pbShelf({"url" : "/lazy/featured/"});
+	$("#featured-shelf2").pbShelf({"url" : "/lazy/featured/"});
 	
 });
-// Todo: Localize as $.data
-var where = 0;
