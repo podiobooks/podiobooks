bc94fb89bcb780a9087a19b891f087505fd50e94
Brant Steen <brant.steen@gmail.com>
2011-02-21 10:47:14 -0500

    Added cookie setting for selecting category on featured shelf
    It will remember your last selected category for next time.
    
    
    Former-commit-id: 8e2e2e07bca37e9cdf99530da526cf98c5f0d765
    Former-commit-id: 8e7d3d5cebea12f2f90a21d9109ec71bbd2dc4d6
    Former-commit-id: 7bc72f8ecf7c7bf7692e0e6a7650e4c13926feac
    Former-commit-id: ba2f7e8356c6a449feffce82e90655e6e9e49530
    Former-commit-id: 971018dd2615efc13d1067c1fe8a63183ed26a39
    Former-commit-id: e52d7203d6f375a7bab52a5d2bed2acff3f21a5b

--- src/podiobooks/main/views/lazy.py
+++ src/podiobooks/main/views/lazy.py
@@ -4,17 +4,34 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from podiobooks.main.models import Title
 from django.conf import settings
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
-
-from podiobooks.main.views import INTIIAL_CATEGORY
+from podiobooks.main.views import get_initial_category
 
 def homepage_featured(request,cat=None):
+    """
+    Gets a requested set of featured titles
+    
+    for use with ajax
+    
+    also sets cookie for that selected category
+    """
     homepage_title_list = Title.objects.filter(display_on_homepage=True).order_by('-date_created').all()
     
+    
     if not cat:
-        cat = INTIIAL_CATEGORY
+        cat = get_initial_category(request)
+    
+    
     
     featured_title_list = homepage_title_list.filter(categories__slug=cat).order_by('-date_created', 'name')[:16]
     
-    return render_to_response("main/lazy/shelf_items.html", {"items":featured_title_list}, context_instance=RequestContext(request))
+    str = render_to_string("main/lazy/shelf_items.html", {"items":featured_title_list}, context_instance=RequestContext(request))
+    
+    
+    response = HttpResponse(str)
+    response.set_cookie("featured_cat",cat)
+    
+    return response
     
