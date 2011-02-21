a0375baf64feb1d21062ffc007353b1e7682c3db
Brant Steen <brant.steen@gmail.com>
2011-02-21 14:17:36 -0500

    .
    Former-commit-id: 70f27de732195571aae71a5191478d991ebc539f
    Former-commit-id: 0aff90cb496242b1dfa063e0c6cf65e3037ba062
    Former-commit-id: faa6fda31e05ad6e18bb061b0f75d9e524379762
    Former-commit-id: cc5000eb31e07a91f1672bba1e14571ad83bfc34
    Former-commit-id: 350a7b2b5261572f185c3430b42d233964664d61
    Former-commit-id: cfa444f7303d3e873d34ea5100e2ddd1efe039f2

--- src/podiobooks/media/themes/pb2-jq/js/jquery.touchSwipe-1.1.0.js
+++ src/podiobooks/media/themes/pb2-jq/js/jquery.touchSwipe-1.1.0.js
@@ -1,43 +1,3 @@
-/*
- * touchSwipe - jQuery Plugin
- * http://plugins.jquery.com/project/touchSwipe
- * http://labs.skinkers.com/touchSwipe/
- *
- * Copyright (c) 2010 Matt Bryson (www.skinkers.com)
- * Licensed under the GNU GPL license
- *
- * $version: 1.1.0 
- *
- * Changelog
- * $Date: 2010-12-12 (Wed, 12 Dec 2010) $
- * $version: 1.0.0 
- * $version: 1.0.1 - removed multibyte comments
- *
- * $Date: 2011-21-02 (Mon, 21 Feb 2011) $
- * $version: 1.1.0 	- added allowPageScroll property to allow swiping and scrolling of page
- *					- changed handler signatures so one handler can be used for multiple events
- *
- *
- * A jQuery plugin to capture left, right, up and down swipes on touch devices.
- * You can capture 2 finger or 1 finger swipes, set the threshold and define either a catch all handler, or individual direction handlers.
- * Options:
- * 		fingers 	int 		Default 1. 	The number of fingers to trigger the swipe, 1 or 2.
- * 		threshold 	int  		Default 75.	The number of pixels that the user must move their finger by before it is considered a swipe.
- * 		swipe 		Function 	A catch all handler that is triggered for all swipe directions. Handler is passed 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
- * 		swipeLeft	Function 	A handler that is triggered for "left" swipes. Handler is passed 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
- * 		swipeRight	Function 	A handler that is triggered for "right" swipes. Handler is passed 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
- * 		swipeUp		Function 	A handler that is triggered for "up" swipes. Handler is passed 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
- * 		swipeDown	Function 	A handler that is triggered for "down" swipes. Handler is passed 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
- *		swipeStatus Function 	A handler triggered for every phase of the swipe. Handler is passed 4 arguments: event : The original event object, phase:The current swipe face, either "start, "move, "end or "cancel. direction : The swipe direction, either "up, "down, "left " or "right.distance : The distance of the swipe.
- *		triggerOnTouchEnd Boolean Default true If true, the swipe events are triggered when the touch end event is received (user releases finger).  If false, it will be triggered on reaching the threshold, and then cancel the touch event automatically.
- *		allowPageScroll String Default "auto". How the browser handles page scrolls when the user is swiping on a touchSwipe object. 
- *										"auto" : all undefined swipes will cause the page to scroll in that direction.
- *										"none" : the page will not scroll when user swipes.
- *										"horizontal" : will force page to scroll on horizontal swipes.
- *										"vertical" : will force page to scroll on vertical swipes.
- *
- * This jQuery plugin will only run on devices running Mobile Webkit based browsers (iOS 2.0+, android 2.2+)
- */
 (function($) 
 {
 	$.fn.swipe = function(options) 
@@ -54,7 +14,7 @@
 			swipeRight		: null,		// Function - A handler that is triggered for "right" swipes. Accepts 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
 			swipeUp			: null,		// Function - A handler that is triggered for "up" swipes. Accepts 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
 			swipeDown		: null,		// Function - A handler that is triggered for "down" swipes. Accepts 3 arguments, the original event object, the direction of the swipe : "left", "right", "up", "down" and the distance of the swipe.
-			swipeStatus		: null,		// Function - A handler triggered for every phase of the swipe. Handler is passed 4 arguments: event : The original event object, phase:The current swipe face, either "start�, "move�, "end� or "cancel�. direction : The swipe direction, either "up�, "down�, "left " or "right�.distance : The distance of the swipe.
+			swipeStatus		: null,		// Function - A handler triggered for every phase of the swipe. Handler is passed 4 arguments: event : The original event object, phase:The current swipe face, either "start, "move, "end or "cancel. direction : The swipe direction, either "up, "down, "left " or "right.distance : The distance of the swipe.
 			
 			triggerOnTouchEnd : true,	// Boolean, if true, the swipe events are triggered when the touch end event is received (user releases finger).  If false, it will be triggered on reaching the threshold, and then cancel the touch event automatically.
 			allowPageScroll : "auto" 	/* How the browser handles page scrolls when the user is swiping on a touchSwipe object. 
