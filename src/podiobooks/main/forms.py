26f36df36245410cec7ae72e2862d97370e51801
Brant Steen <brant.steen@gmail.com>
2011-02-21 07:52:37 -0500

    Genres... WEEEEE
    Former-commit-id: c2a30aa9ec97ab73820e2b9533dd9a065f49a5b0
    Former-commit-id: 2ed37deec22ed1e8f74d94c6944cab951d39130e
    Former-commit-id: 919f120f05d6019bdcda8f25e83aa020ed7cbbfa
    Former-commit-id: a1bd110f8c7b05f3d6b3fb17850cc1037580c4a6
    Former-commit-id: e38170751548a6c07ce291fc3f7792fccb9b3b85
    Former-commit-id: 616ae3eebd0db1f323e92f391a6da42b8c0442a5

--- src/podiobooks/main/forms.py
+++ src/podiobooks/main/forms.py
@@ -18,7 +18,7 @@ class CategoryChoiceForm(forms.Form):
     if (not categories):
         categories = Category.objects.filter(title__display_on_homepage=True).annotate(title_count=Count('title')).filter(title_count__gt=2).order_by('name').values_list('slug','name')
         cache.set('category_dropdown_values', categories, 240)
-    category = forms.ChoiceField(choices=categories, widget=forms.Select(attrs={'class':'pb-category-choice', 'onchange':'shelfChange(this.form.name, this.form.action, this.value);'}))
+    category = forms.ChoiceField(choices=categories, widget=forms.Select(attrs={'class':'pb-category-choice'}))
 
 class ContributorChoiceForm(forms.Form):
     """ Form used to select contributors on the Author Spotlight shelf """
