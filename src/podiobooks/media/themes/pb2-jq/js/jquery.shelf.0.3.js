d8686da940bf22cb2f803cb192e007b1cda91f1e
Brant Steen <brant.steen@gmail.com>
2011-02-25 10:19:06 -0500

    jquery.shelf.0.3 plugin
    Former-commit-id: f16df179891321b09c7e54625aa7d5995c84ae01
    Former-commit-id: 7768e439bb35d99bb0215869fb88bba6306fe2be
    Former-commit-id: 7b8e5163b89d02242c2d1850467526f738380d4d
    Former-commit-id: 7fd87c01bf57c2172559a46ec6fb67ff3c868f9f
    Former-commit-id: 368958e23cb73afb10f08f8e2151ab2ce7916662
    Former-commit-id: 60911ef334b065c1da7578e8e777ed462de21f2e

--- src/podiobooks/media/themes/pb2-jq/js/jquery.shelf.0.3.js
+++ src/podiobooks/media/themes/pb2-jq/js/jquery.shelf.0.3.js
@@ -3,6 +3,8 @@
  * 
  * 0.1: Initial plugin creation
  * 0.2: Internalized checking for change form, adding binding for onChange
+ * 0.3: Added some more settings for custom classes
+ * 0.4: Shelf position indicator
  */
 (function( $ ){
 
@@ -11,7 +13,9 @@
 		var settings = {
 			'url' 			: 		'/',
 			"cookie"		: 		null,
-			"checkCookie"	: 		false
+			"checkCookie"	: 		false,
+			"shelfItem"		: 		".shelf-item",
+			"shelfItemCover": 		".shelf-cover"
 		};
 		
 		return this.each(function(){
@@ -20,14 +24,32 @@
 				$.extend(settings,options);
 			}
 			
-			var where = 0;
-			var cur = 0;
-			var maxWidth = 0;
-			var itemWidth = 0;
+			/*
+			 * Shelf status variables
+			 */
+			var where = 0;			// How many "titles in" the user is (integer, 0-# of books)
+			var cur = 0;			// current number of pixles have been shoved left
+			var maxWidth = 0;		// maximum number of pixels the shelf can move to the left
+			var itemWidth = 0;		// how wide each .shelf-item is
 						
+			/*
+			 * Store the shelf as local variable,
+			 * Set shelf height 
+			 * 
+			 * (otherwise it compresses when elements are removed)
+			 */
 			var shelf = $(this);
 			shelf.height(shelf.height());
 			
+			
+			/*
+			 * Remove all content from shelf,
+			 * except for the select box
+			 * 
+			 * This allows the plugin to call itself,
+			 * instead of the calling script needing to declare
+			 * an on-change event for the select box
+			 */
 			shelf.children().each(function(){
 				if (!($(this).is("form"))){
 					$(this).html("");
@@ -36,7 +58,10 @@
 			});
 			
 			
-			
+			/*
+			 * If the shelf has a select box,
+			 * bind the on-change event to reset the plugin
+			 */
 			if (shelf.has("form select")){
 				var sel = shelf.find("form select");
 				
@@ -56,6 +81,10 @@
 				});				
 			}
 	
+			/*
+			 * If we should be checking the cookie,
+			 * check it, then set the inital select box value
+			 */
 			if (settings.checkCookie){
 				if($.cookie(settings.cookie)){
 					if (shelf.has("form select")){
@@ -65,22 +94,38 @@
 				}
 			}
 			
+			
+			/*
+			 * If we have a cookie name to set, set it 
+			 * based on the currently selected form>select
+			 */
 			if(settings.cookie){
 				$.cookie(settings.cookie,shelf.find("form select").val(),{expires:7});
 			}		
 			
-			var progress = $("<p class='shelf-ajax-loader'><img src='" + siteVars("img") + "ajax-loader-bar.gif'/></p>");
 			
+			/*
+			 * Ajax Progress element image, 
+			 * appears in middle of shelf during onchange ajax call
+			 * 
+			 * Create it, append it to the body for caching, 
+			 * move it into the shelf and show
+			 */ 
+			var progress = $("<p class='shelf-ajax-loader'><img src='" + siteVars("img") + "ajax-loader-bar.gif'/></p>");
 			progress.appendTo($("body")).hide();
 			progress.appendTo(shelf).show();
 			
-			shelf.addClass('fun-shelf');
 			
+			/*
+			 * Right/left shelf arrows
+			 * create, added/removed to/from shelf later
+			 */
 			var leftArrow = $("<a class='shelf-arrow shelf-arrow-left' href='#'></a>");
 			var rightArrow = $("<a class='shelf-arrow shelf-arrow-right' href='#'></a>");
-			
-			
-			
+						
+			/*
+			 * Debugging function for logging the current shelf status
+			 */
 			var status = function(){
 				l("cur: " + cur);
 				l("where: " + where);
@@ -88,6 +133,10 @@
 				l("itemWidth: " + itemWidth);
 			};
 				
+			/*
+			 * Hiding/showing arrows 
+			 * based on shelf position
+			 */
 			var handleArrows = function(){
 				if (cur == 0){
 					leftArrow.hide();
@@ -103,61 +152,100 @@
 				}
 			};
 			
-			
+			/*
+			 * Big beefy ajax workhorse
+			 */
 			$.ajax({
 				method:"get",
 				url:settings.url,
 				success:function(data){
 					
+					/*
+					 * Ajax request should return HTML
+					 * 
+					 * Append all data to the shelf
+					 */
 					$(data).appendTo(shelf);
 					
-					if(shelf.find(".shelf-item").length){
+					/*
+					 * If the appended data has shelf items,
+					 * proceed with building shelf functionality
+					 */
+					if(shelf.find(settings.shelfItem).length){
 						
-						$(shelf).find(".shelf-cover img:not(.shelf-cover-loading)").each(function(){
+						/*
+						 * Find all shelf items that arent marked as covers being loaded
+						 */
+						$(shelf).find(settings.shelfItemCover).find("img:not(.shelf-cover-loading)").each(function(){
 							
 							var img = $(this);
 							
+							/*
+							 * Hide the image, replace it with a progress loader graphic
+							 */
 							img.hide();
-							var l = $("<img class='shelf-cover-loading' src='" + siteVars("img") + "loading.gif' />").appendTo(img.parents(".shelf-cover"));
+							var loader = $("<img class='shelf-cover-loading' src='" + siteVars("img") + "loading.gif' />").appendTo(img.parents(".shelf-cover"));
 							
+							/*
+							 * Once the real cover has loaded,
+							 * remove the loader graphic, fade in the cover
+							 */
 							img.load(function(){
-								l.remove();
+								loader.remove();
 								img.fadeIn();
 								
 							});
 						});
 						
+						/*
+						 * while the covers are loading, hide the progress bar
+						 */
 						progress.hide();
 						
-						var shelfItems = shelf.find(".shelf-item");
+						/*
+						 * Find all the shelf items,
+						 * add up their total widths
+						 */
+						var shelfItems = shelf.find(settings.shelfItem);
 						var w = 0;
-						
 						shelfItems.each(function(){
 							itemWidth = parseInt($(this).width()) + parseInt($(this).css("padding-left")) + parseInt($(this).css("padding-right")) + parseInt($(this).css("margin-left")) + parseInt($(this).css("margin-right"));
 							w += itemWidth;
 						});
-						
 						maxWidth = w;
 						
-						shelf.children("div").wrapAll("<div class='whole-shelf'/>");
+						/*
+						 * Wrap all the shelf items,
+						 * create a "field of vision"
+						 */
+						shelf.children(settings.shelfItem).wrapAll("<div class='whole-shelf'/>");
 						var wholeShelf = shelf.children(".whole-shelf");
 						wholeShelf.wrap("<div class='shelf-view'/>");
 						
-						
-						
+						/*
+						 * Append the right/left arrows,
+						 * let the arrow handler figure out
+						 * if they should be shown or hidden
+						 */
 						leftArrow.appendTo(shelf);
 						rightArrow.appendTo(shelf);
-						
 						handleArrows();
 					}
 					else{
+						/*
+						 * If there were no returned shelf items,
+						 * Just hide the progress bar
+						 */
 						progress.hide();
 					}
 					
+					
+					/*
+					 * Click events for right and left arrows
+					 */
 					rightArrow.click(function(e){
 						e.preventDefault();
-						if (cur < maxWidth - shelf.width()){
-							
+						if (cur < maxWidth - shelf.width()){							
 							where += shelf.width() / itemWidth;
 							if (where * itemWidth > maxWidth - shelf.width()){
 								where = (maxWidth - shelf.width()) / itemWidth;
@@ -176,15 +264,13 @@
 					leftArrow.click(function(e){
 						e.preventDefault();
 						
-						if (cur > 0){
-							
+						if (cur > 0){							
 							where -= shelf.width() / itemWidth;
 							if (where < 0){
 								where = 0;
 							}
 							var targ = "-" + (where * itemWidth) + "px";
-							cur = where * itemWidth;
-							
+							cur = where * itemWidth;							
 							wholeShelf.animate({
 								left:targ
 							},600,"easeOutCirc");
@@ -193,6 +279,11 @@
 						status();
 					});
 					
+					/*
+					 * Use the jquery.touchSwipe plugin
+					 * to have swipes trigger click events 
+					 * on the arrows
+					 */
 					wholeShelf.swipe({
 						swipeLeft:function(event){
 							rightArrow.trigger("click");	
@@ -200,23 +291,8 @@
 						swipeRight:function(event){
 							leftArrow.trigger("click");
 						},
-						swipeStatus:function(event, phase, direction, distance){
-							/*
-							if (direction == "left" && cur < maxWidth - shelf.width()){
-								wholeShelf.animate({"left": parseInt(wholeShelf.css("left").replace("px","")) - distance},1);
-								cur -= parseInt(wholeShelf.css("left").replace("px","")) - distance;
-								status();
-							}
-							if (direction == "right" && cur > 0){
-								wholeShelf.animate({"left":parseInt(wholeShelf.css("left").replace("px","")) + distance},1);
-								cur += parseInt(wholeShelf.css("left").replace("px","")) + distance;
-								status();								
-							} 
-							*/				
-						},
 						allowPageScroll:"vertical"						
-					});
-					
+					});					
 				}
 			});
 		});
