599451b6017f1653c920282be62d5a283a3d432c
Brant Steen <brant.steen@gmail.com>
2011-03-07 12:54:37 -0500

    Remove contributors that have no titles
    Former-commit-id: 1f4d932f851bdad54aa43a30347e3ebfde943b32
    Former-commit-id: 19afd30ee6231b27c3d87ac1d2bc5ca510eb9f23
    Former-commit-id: 143f955c3a83b33650cd67550b21e6e8cc251ffa
    Former-commit-id: f6fb4f1805a62fc16d59ba4c74e5c51a4fa06acc
    Former-commit-id: ba554a951595edfde3148ff356f41cb310d63cd3
    Former-commit-id: e7cc6cb3678004d35e592a47587f3e32f7e00526

--- src/podiobooks/main/forms.py
+++ src/podiobooks/main/forms.py
@@ -29,10 +29,11 @@ class ContributorChoiceForm(forms.Form):
     
     if (not contributors):
         top_contributors = Contributor.objects.annotate(title_count=Count('title')).filter(title__display_on_homepage=True).order_by('-title_count').values_list('slug', 'display_name', 'title_count')[:10]
-        
+         
         contributors = []
         for slug, name, titles in top_contributors:
-            contributors.append( (str(slug), str(name)), )  #strip off the count, which has to be in the values list because of the order_by
+            if titles > 0:
+                contributors.append( (str(slug), str(name)), )  #strip off the count, which has to be in the values list because of the order_by
           
         cache.set('contributor_dropdown_values', contributors, 240)
         
