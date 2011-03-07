f5fc7918824b6073335bfde03493e42baaca9824
Tim White <tim@cyface.com>
2011-03-07 13:58:48 -0700

    First cut at creating a title from a libsyn RSS feed.

---
+++ src/podiobooks/libsyn/tests_rss.py
@@ -0,0 +1,22 @@
+"""Automated Tests of the Podiobooks LibSyn RSS Feed Parser"""
+
+# pylint: disable=C0103,C0111,R0904
+
+from django.test import TestCase
+from podiobooks.libsyn import create_title_from_libsyn_rss
+
+class LibsynRSSTestCase(TestCase):
+    # fixtures = []
+    
+    def setUp(self):
+        pass
+    
+    def testParseRss(self):
+        titleData = create_title_from_libsyn_rss.parseLibsynRSS('http://infected.podiobooks.libsynpro.com/rss')
+        print titleData
+        self.assertEquals(titleData['Subtitle'], 'infected')
+        
+        titleData = create_title_from_libsyn_rss.parseLibsynRSS('http://shadowmagic2.podiobooks.libsynpro.com/rss')
+        print titleData
+        self.assertEquals(titleData['Subtitle'], 'shadowmagic2')
+        
