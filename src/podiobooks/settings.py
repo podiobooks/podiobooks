022dddb82eb92c1abb03a299517374985da8b366
Podiobooks 2.0 Development Team <podiobooksdev@gmail.com>
2011-03-14 09:45:07 -0700

    Changed comment to reflect settings naming convention.

--- src/podiobooks/settings.py
+++ src/podiobooks/settings.py
@@ -3,7 +3,7 @@
 # pylint: disable=E0611,F0401,W0401,W0614
 
 # NOTE
-# see local_settings_template.py for instructions on making your local settings file
+# see settings_local.template for instructions on making your local settings file
 
 from podiobooks.settings_main import * #@UnusedWildImport
 
