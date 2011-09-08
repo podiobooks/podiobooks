a0b2175889e6aa8663337c8831ab97b97e01f50a
Tim White <tim@cyface.com>
2011-09-08 13:14:40 -0700

    Moved mediabrute to eggs-local, fixed .gitignore to let me.
    Former-commit-id: 075ceb26ae4635f5db188ed91fc1608d7ec573e3
    Former-commit-id: 1c48fa45d270bda0e63ca7b092407d364ce9494d
    Former-commit-id: 30a5a1074ff4051a596853a75fae3a3d81e19afb
    Former-commit-id: eacfeb74a1b582c8a8c11f11618164cf7c331c5a
    Former-commit-id: a05e5a13e7f00c734177640fa570952427748411
    Former-commit-id: 4e60e292cdc62b9ff09206b4e5afd94715667339

---
+++ eggs-local/mediabrute.egg/mediabrute/util/defaults.py
@@ -0,0 +1,17 @@
+"""
+Default Settings
+
+See README for info
+"""
+CSS_DIR = "css"
+JS_DIR = "js"
+
+APP_CSS = "css"
+APP_JS = "js"
+
+MEDIA_CACHE_DIR = "cache"
+
+JS_SETTINGS_FILENAME = "mediabrute-settings.js"
+
+SEPARATE_CSS = []
+SEPARATE_JS = []
