b12b8f7fe332f7da5802291aedd5854342c53faf
Brant <brant.steen@gmail.com>
2011-09-08 16:00:23 -0700

    Webfonts, Poorly Organized
    There are some browser constraints that will require us to load fonts
    from the same url as the SITE (not necessarily the same as the static or
    media)  For the time being, they are in a place for development but
    later on this organization is going to have to move around… plan
    pending.

--- src/podiobooks/urls.py
+++ src/podiobooks/urls.py
@@ -70,7 +70,7 @@ urlpatterns += patterns('',
 
 #if settings.DEBUG:
 #    urlpatterns += patterns('',
-#        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-#            'document_root': settings.MEDIA_ROOT,
+#        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+#            'document_root': settings.THEME_MEDIA_ROOT,
 #        }),
 #   )
