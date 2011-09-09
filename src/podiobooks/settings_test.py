3eb59d279c84328cba1bf6763c6f3a58d163c40d
Brant <brant.steen@gmail.com>
2011-09-09 15:12:41 -0700

    Gut

--- src/podiobooks/settings_test.py
+++ src/podiobooks/settings_test.py
@@ -10,17 +10,6 @@ except:
     pass
 import tempfile
 
-# Test DB settings. (SQLLite)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': # DO NOT SPECIFY A NAME SO THAT TEST RUNNER WILL USE IN-MEMORY DB
-        'SUPPORTS_TRANSACTIONS': 'false',
-    }
-}
-
-# Test Cache Settings
-CACHE_BACKEND = "file://" + tempfile.gettempdir()
 
 INSTALLED_APPS += ('django_nose',)
 
