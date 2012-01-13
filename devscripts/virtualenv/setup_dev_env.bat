399bf2b168e3130871c24b56ec9d9f25c98049f6
cyface <tim@cyface.com>
2012-01-13 10:30:18 -0700

    Updated INSTALL and README to reflect virtualenv setup and PyCharm.

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -1,9 +1,10 @@
 REM You must have virtualenv installed, and the virualenv command in your path for this to work.
-REM Assuming you have python installed, you can install virtualenv using the command below.
-REM curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 REM This should be run from the project directory, not inside the socialprofile dir
-REM BOB
-virtualenv --no-site-packages podiobooks-env
+REM the pypm command below assumes you are using the ActiveState Community Python installer from here:
+REM http://www.activestate.com/activepython/downloads
+pypm install distribute
+pypm install virtualenv
+virtualenv --distribute podiobooks-env
 call podiobooks-env\Scripts\activate.bat
 REM Setup Path for Visual Studio Express so that PIL can compile.
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC;%PATH%
