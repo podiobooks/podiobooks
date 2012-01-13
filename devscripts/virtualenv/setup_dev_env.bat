14408eea0761be865d136044284f5b2c119fbb37
cyface <tim@cyface.com>
2012-01-12 22:32:57 -0700

    Fixed up windows virtualenv script to point at path to visual studio so it will compile PIL.
    Former-commit-id: c5694dac5ca018567ad309646f2c97818824a2e9
    Former-commit-id: ebf700957b069ad45a4e69dbb4df6320843e8f19
    Former-commit-id: 04a48ca7217ec5652b05c301b5bd73bae76c8c90
    Former-commit-id: 661af087665d61cd9878b4dec08498d340bf4022
    Former-commit-id: d2ad5f31429c465f56ba097efffa15c87b4f310f
    Former-commit-id: 468a511df52a37eb529091fa5b2e00e406b999ba

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -2,7 +2,9 @@ REM You must have virtualenv installed, and the virualenv command in your path f
 REM Assuming you have python installed, you can install virtualenv using the command below.
 REM curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 REM This should be run from the project directory, not inside the socialprofile dir
-
+REM BOB
 virtualenv --no-site-packages podiobooks-env
 call podiobooks-env\Scripts\activate.bat
+REM Setup Path for Visual Studio Express so that PIL can compile.
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC;%PATH%
 pip install -r podiobooks/requirements_dev.txt
