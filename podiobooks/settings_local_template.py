997bb3e87c2838037cce84349810cb22f26a93b0
Tim White <tim@cyface.com>
2012-01-14 16:11:53 -0700

    Updated settings template with fixed disqus key basics.

--- podiobooks/settings_local_template.py
+++ podiobooks/settings_local_template.py
@@ -165,8 +165,9 @@ LIBSYN_API_SERVER_URL = 'http://api.libsyn.com/xmlrpc'
 DATALOAD_DIR = PROJECT_ROOT + "/../../podiobooks-dataload/datafiles/"
 
 ### DISQUS
-DISQUS_API_KEY = 'FOOBARFOOBARFOOBARFOOBARFOOBARF'
 DISQUS_WEBSITE_SHORTNAME = 'podioadventures'
+DISQUS_API_PUBLIC_KEY = ''
+DISQUS_API_SECRET_KEY = ''
 
 ### MARKITUP
 MARKITUP_SET = 'markitup/sets/markdown'
