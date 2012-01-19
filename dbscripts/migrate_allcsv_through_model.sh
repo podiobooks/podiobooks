3bda29cd83de40fc0efd2995fbec2926f9777a3c
Tim White <tim@cyface.com>
2012-01-18 21:13:51 -0700

    Comment in migrate script.

--- dbscripts/migrate_allcsv_through_model.sh
+++ dbscripts/migrate_allcsv_through_model.sh
@@ -1,3 +1,4 @@
+#Run this script from the podiobooks dir, not the podiodbooks/podiobooks dir.
 export DJANGO_SETTINGS_MODULE=podiobooks.settings
 . podiobooks-env/bin/activate
 export PYTHONPATH=.:./podiobooks:$PYTHONPATH
