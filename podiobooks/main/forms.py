7469549901d4349fdf006338beff13eb5a083e3c
Tim White <tim@cyface.com>
2012-01-22 19:32:22 -0700

    pylint cleanup.
    Former-commit-id: 32abe9eb46780bbdcf411ed9433db53c2bcf33f1
    Former-commit-id: 46029a945fd89d02d3c5f1763c2c18ca0c2da92f
    Former-commit-id: cc06ea4eb62761bb91351857d58b99560a87c4e8
    Former-commit-id: 755f44c006a4be1338943dbc8387fffd9d7296ae
    Former-commit-id: 9ffc02d9d48e526fffa53db293ad543b529edc9e
    Former-commit-id: bcc2721eb313b7187d42807ad2b4bbd93fa0cddd

--- podiobooks/main/forms.py
+++ podiobooks/main/forms.py
@@ -16,7 +16,7 @@ class CategoryChoiceForm(forms.Form):
     submit_url = None
     form_name = 'category'
     if (not categories):
-        categories = Category.objects.filter(~Q(slug="erotica"),title__display_on_homepage=True).annotate(title_count=Count('title')).filter(title_count__gt=2).order_by('name').values_list('slug','name')
+        categories = Category.objects.filter(~Q(slug="erotica"), title__display_on_homepage=True).annotate(title_count=Count('title')).filter(title_count__gt=2).order_by('name').values_list('slug', 'name')
         cache.set('category_dropdown_values', categories, 240)
     category = forms.ChoiceField(choices=categories, widget=forms.Select(attrs={'class':'pb-category-choice'}))
 
