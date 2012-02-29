f28b5048c881ad5187c9291eb570356e723fe610
cyface <tim@cyface.com>
2012-02-29 13:06:32 -0700

    Renamed initial_data to minimal_data to prevent autoloading.

---
+++ devscripts/gondor/gondor_load_alldata.bat
@@ -0,0 +1,4 @@
+REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
+call ..\podiobooks-env\Scripts\activate.bat
+REM Creates Superuser in primary Gondor Instance
+gondor run primary loaddata alldata
