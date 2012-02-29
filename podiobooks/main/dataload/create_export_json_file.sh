5cea83b5b3f206209b1ac35cb58405d82fa36f7f
cyface <tim@cyface.com>
2012-02-29 15:22:26 -0700

    Updated alldata export to not include 'standard' stuff.
    Former-commit-id: 549dde079eae39134bd92936eeb07d47bdb9b2b0
    Former-commit-id: 349d0242d75f9558ba41d820d922bb72d2c5f6fa
    Former-commit-id: 7e7da0dff133965032035dba870b2466f0f7e8ff
    Former-commit-id: 7a5a370300465f106313441257c1e4f36c279d6f
    Former-commit-id: 01f714244cc416463fc6aab9658137459bb03225
    Former-commit-id: ac139145daf2c878537d223c93c01903b615c34d

--- podiobooks/main/dataload/create_export_json_file.sh
+++ podiobooks/main/dataload/create_export_json_file.sh
@@ -1,4 +1,4 @@
 #Run this script from the podiobooks dir, not the podiodbooks/podiobooks dir.
 export DJANGO_SETTINGS_MODULE=podiobooks.settings
 . podiobooks-env/bin/activate
-python manage.py dumpdata --all > main/fixtures/alldata.json
+python manage.py dumpdata main > main/fixtures/alldata.json
