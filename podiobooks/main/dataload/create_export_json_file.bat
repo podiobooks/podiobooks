f46801ebdd95b3e3dd9135a464550a2281d002bf
cyface <tim@cyface.com>
2012-02-29 15:22:26 -0700

    Updated alldata export to not include 'standard' stuff.

--- podiobooks/main/dataload/create_export_json_file.bat
+++ podiobooks/main/dataload/create_export_json_file.bat
@@ -1,4 +1,4 @@
 REM Run this script from the podiobooks\podiobooks dir (as ..\dbscripts\migrate_allcsv_through_model.bat).
 SET DJANGO_SETTINGS_MODULE=podiobooks.settings
 call ..\podiobooks-env\Scripts\activate.bat
-python manage.py dumpdata --all > main/fixtures/alldata.json
+python manage.py dumpdata main > main/fixtures/alldata.json
