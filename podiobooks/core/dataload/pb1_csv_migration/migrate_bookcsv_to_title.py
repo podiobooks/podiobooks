b348bd082f1b03e684ade59721b95b435f53f628
cyface <tim@cyface.com>
2012-03-06 07:38:47 -0700

    Pylint cleanup. Last ROUND (I hope).

--- podiobooks/core/dataload/pb1_csv_migration/migrate_bookcsv_to_title.py
+++ podiobooks/core/dataload/pb1_csv_migration/migrate_bookcsv_to_title.py
@@ -28,7 +28,7 @@ def determine_license(license_slug):
             found_license = License.objects.create(slug=license_slug)
         else:
             found_license = License.objects.get(slug='by-nc-nd')
-    
+
     return found_license
 
 ###### Define general utility functions ##########
@@ -41,17 +41,21 @@ def boolean_clean(data):
             return True
         else:
             return False
-    
+
+
 def get_or_create_award(award_slug):
     """Retrieves or creates an Award type based on the slug of the award"""
 
     award, created = Award.objects.get_or_create(slug__iexact=award_slug,
-              defaults={ 'slug': award_slug, 'name': award_slug, 'image': 'unknown', 'url': 'unknown' })
+        defaults={'slug': award_slug, 'name': award_slug, 'image': 'unknown', 'url': 'unknown'})
     return award
+
 
 def get_or_create_contributor(contributor_name):
     """Retrieves or creates a Contributor object based on the name of the Contributor"""
-    contributor_name = contributor_name.strip().replace('  ', ' ').replace('\\', '').replace('&apos;', '\'').replace('Theater', 'Theatre').replace('J. C.', 'J.C.').replace('J. A.', 'J.A.').replace('J. J.', 'J.J.').replace('J. T.', 'J.T.').replace('J. P.', 'J.P.').replace('JP', 'J.P.').replace('I G', 'I.G.')
+    contributor_name = contributor_name.strip().replace('  ', ' ').replace('\\', '').replace('&apos;', '\'').replace(
+        'Theater', 'Theatre').replace('J. C.', 'J.C.').replace('J. A.', 'J.A.').replace('J. J.', 'J.J.').replace('J. T.'
+        , 'J.T.').replace('J. P.', 'J.P.').replace('JP', 'J.P.').replace('I G', 'I.G.')
     try:
         contributor_name_to_split = contributor_name.replace(' III', '')
         contributor_name_tokens = contributor_name_to_split.split(" ", 2)
@@ -64,28 +68,32 @@ def get_or_create_contributor(contributor_name):
         first_name_guess = ""
         middle_name_guess = ""
         last_name_guess = contributor_name
-        
+
     contributor, created = Contributor.objects.get_or_create(display_name__iexact=contributor_name,
-                  defaults={ 'display_name':contributor_name, 'slug': slugify(contributor_name), 'first_name': first_name_guess, 'middle_name': middle_name_guess.replace(',', ''), 'last_name': last_name_guess.replace(',', '').replace(' Ph.D.', '') })
+        defaults={'display_name': contributor_name, 'slug': slugify(contributor_name), 'first_name': first_name_guess,
+                  'middle_name': middle_name_guess.replace(',', ''),
+                  'last_name': last_name_guess.replace(',', '').replace(' Ph.D.', '')})
     return contributor
+
 
 def get_or_create_contributor_type(contributor_type):
     """Retrieves or creates a Contributor type based on the name of the type"""
     contributorTypeSlug = slugify(contributor_type)
     contributorTypeToBylineMapping = {
-      'author': " and ",
-      'editor':" edited by ",
-      'narrator': " narrated by ",
-      'penname': " writing as ",
-      'ghostwriter': " as told to ",
-      'contributor': " with ",
-    }
+        'author': " and ",
+        'editor': " edited by ",
+        'narrator': " narrated by ",
+        'penname': " writing as ",
+        'ghostwriter': " as told to ",
+        'contributor': " with ",
+        }
     byline_text = contributorTypeToBylineMapping[contributorTypeSlug]
     if not byline_text:
         byline_text = ""
     contributorTypeObject, created = ContributorType.objects.get_or_create(slug=contributorTypeSlug,
-                  defaults={'name': contributor_type, 'byline_text': byline_text })
+        defaults={'name': contributor_type, 'byline_text': byline_text})
     return contributorTypeObject
+
 
 def get_category(category_id):
     """Retrieves a category by id or none if not found"""
@@ -93,8 +101,9 @@ def get_category(category_id):
         category = Category.objects.get(id=category_id)
     except:
         category = None
-    
+
     return category
+
 
 def get_partner(partner_id):
     """Retrieves a partner by id or none if not found"""
@@ -102,76 +111,80 @@ def get_partner(partner_id):
         partner = Partner.objects.get(id=partner_id)
     except:
         partner = None
-    
+
     return partner
+
 
 def get_or_create_series(series_slug):
     """Retrieves or creates an Series based on the slug of the series"""
 
     series, created = Series.objects.get_or_create(slug__iexact=series_slug,
-              defaults={ 'slug': series_slug, 'name': series_slug, })
+        defaults={'slug': series_slug, 'name': series_slug, })
     return series
+
 
 def get_libsyn_id_cache():
     """Pull up the file containing the cache of libsyn IDs and load it into an object for use"""
     libsynIDCache = {}
-    
+
     #Open Cache File for Import
     cacheCSVFile = open(settings.DATALOAD_DIR + "podiobooks_libsyn_id_cache.csv")
-    
+
     #Parse the Cache File CSV into a dictionary based on the first row values
     cacheCSVReader = csv.DictReader(cacheCSVFile, dialect='excel')
-    
+
     for title in cacheCSVReader:
-        libsynIDCache[ title['ID'] ] = title['LibsynShowId']
-        
+        libsynIDCache[title['ID']] = title['LibsynShowId']
+
     return libsynIDCache
+
 
 def get_itunes_id_cache():
     """Pull up the file containing the cache of itunes IDs and load it into an object for use"""
     iTunesIDCache = {}
-    
+
     #Open Cache File for Import
     cacheCSVFile = open(settings.DATALOAD_DIR + "podiobooks_itunes_id_cache.csv")
-    
+
     #Parse the Cache File CSV into a dictionary based on the first row values
     cacheCSVReader = csv.DictReader(cacheCSVFile, dialect='excel')
-    
+
     for title in cacheCSVReader:
         print title
         if title['ID']:
-            iTunesIDCache[ title['ID'] ] = title['iTunesID']
-        
+            iTunesIDCache[title['ID']] = title['iTunesID']
+
         if title['Slug']:
-            iTunesIDCache[ title['Slug'] ] = title['iTunesID']
-            
+            iTunesIDCache[title['Slug']] = title['iTunesID']
+
     return iTunesIDCache
+
 
 def import_books_from_csv():
     """Reads in the CSV and using the Django model objects to populate the DB"""
-    
+
     #Open Book File for Import
     bookCSVFile = open(settings.DATALOAD_DIR + "podiobooks_legacy_book_table.csv")
-    
+
     #Parse the Book File CSV into a dictionary based on the first row values
     bookCSVReader = csv.DictReader(bookCSVFile, dialect='excel')
-    
+
     #PRE CLEANOUT
     Title.objects.all().delete()
     TitleContributor.objects.all().delete()
     Contributor.objects.all().delete()
-    
+
     create_titles_from_book_rows(bookCSVReader)
- 
+
+
 def create_titles_from_book_rows(title_list):
     """Takes a list of title rows from a database query or a CSV file read and creates Title objects"""
-    
+
     libsyn_id_cache = get_libsyn_id_cache()  #Load the Libsyn ID Cache from its CSV
     itunes_id_cache = get_itunes_id_cache()  #Load the iTunes ID Cache from its CSV
-    
+
     # Loop through the rest of the rows in the CSV
     for row in title_list:
-        
         # Look up itunes_id in Cache
         itunes_id = None
         try:
@@ -181,10 +194,10 @@ def create_titles_from_book_rows(title_list):
                 itunes_id = itunes_id_cache[row['Subtitle']]
             except KeyError:
                 pass
-        
+
         if (int(row['Enabled']) == 1 and int(row['Standby']) == 0):
             # Create a title object in the database based on the current book row
-            title = Title.objects.create (
+            title = Title.objects.create(
                 id=row['ID'],
                 name=row['Title'].replace('\\', ''),
                 slug=slugify(row['Title']),
@@ -202,59 +215,61 @@ def create_titles_from_book_rows(title_list):
                 avg_overall=row['AvgOverall'],
                 deleted=False,
                 podiobooker_blog_url=row['DiscussURL'],
-                libsyn_show_id=libsyn_id_cache.get(row['ID'],""),
+                libsyn_show_id=libsyn_id_cache.get(row['ID'], ""),
                 itunes_adam_id=itunes_id,
                 date_created=row['DateCreated']
             )
-            
+
             # Contributors
-            contributor_list = contributor_translation.translate_contributor(row['Authors']) #Manual Contributor Lookup Translation
+            contributor_list = contributor_translation.translate_contributor(
+                row['Authors']) #Manual Contributor Lookup Translation
             for contributor in contributor_list:
                 contributorObject = get_or_create_contributor(contributor['name'])
                 contributorType = get_or_create_contributor_type(contributor['type'])
-                TitleContributor.objects.create(title=title, contributor=contributorObject, contributor_type=contributorType)
+                TitleContributor.objects.create(title=title, contributor=contributorObject,
+                    contributor_type=contributorType)
             print "Title: %s\n\t\tContributors: %s" % (title.name, title.contributors.values('display_name'))
-            
-           # Awards
+
+            # Awards
             award_list = award_translation.translate_award(row['ID']) #Manual Award Lookup Translation
             if award_list:
                 for awardSlug in award_list:
                     awardObject = get_or_create_award(awardSlug)
                     title.awards.add(awardObject)
                 print "\t\tAwards: %s" % (title.awards.values('name'))
-            
+
             # Series
             series_slug = series_translation.translate_series(row['ID']) #Manual Series Lookup Translation
             if series_slug:
                 seriesObject = get_or_create_series(series_slug)
                 title.series = seriesObject
                 print "\t\tSeries: %s" % (title.series.name)
-            
+
             # Category
             category = get_category(row['CategoryID'])
             if category:
-                TitleCategory.objects.create(title=title,category=category)
+                TitleCategory.objects.create(title=title, category=category)
                 print "\t\tCategories: %s" % (title.categories.values('name'))
                 if category.slug == 'erotica':
                     title.is_adult = True
-            
+
             # Partner
             partner = get_partner(row['PartnerID'])
             if partner:
                 title.partner = partner
                 print "\t\tPartner: %s" % (title.partner.name)
-                
+
             title.save()
-            
-        # @TODO Need to double check what we want to do with URLs to iTunes, etc.
-        
-        # @TODO Create Media Objects for the URL Fields from the Book Row
+
+            # @TODO Need to double check what we want to do with URLs to iTunes, etc.
+
+            # @TODO Create Media Objects for the URL Fields from the Book Row
 
 ##### MAIN FUNCTION TO RUN IF THIS SCRIPT IS CALLED ALONE ###
 if __name__ == "__main__":
     import_books_from_csv()
-    
-    
+
+
 # HANDY MAPPING REFERENCE
 # TITLE MODEL FIELDS
 #name = models.CharField(max_length=255)
