ae725949a56ecb33738d51be3dd49abea7212b96
cyface <tim@cyface.com>
2012-06-14 11:44:19 -0600

    Updated settings to work with django 1.4.
    Former-commit-id: f09e58568272755c86f1eb51aaba4bba19be018c
    Former-commit-id: 989614c47de7f7d9f737dd3e4499f13ff8badd29
    Former-commit-id: 16f09aa34332822788cc94277c56599760b29082
    Former-commit-id: d41a2dc6484a1c79401946007e48cb77ce11fccb
    Former-commit-id: a4147027461fb32b8c50946f1d497ed3e7478233
    Former-commit-id: 186865f4b533a5d3e3b347e4d689950c3b14f510

--- podiobooks/settings_main.py
+++ podiobooks/settings_main.py
@@ -2,14 +2,14 @@
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 #List of callables that add their data to each template
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.media',
     'podiobooks.core.context_processors.js_api_keys',
     'django.core.context_processors.request',
