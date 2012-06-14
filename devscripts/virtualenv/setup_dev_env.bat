5ec21b5564851ff565f17f77cd34f79852f52ef2
cyface <tim@cyface.com>
2012-06-14 08:52:38 -0600

    Added --non-interactive prompt to pypm calls.

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -2,10 +2,10 @@ REM You must have virtualenv installed, and the virualenv command in your path f
 REM This should be run from the project directory, not inside the socialprofile dir
 REM the pypm command below assumes you are using the ActiveState Community Python installer from here:
 REM http://www.activestate.com/activepython/downloads
-pypm install distribute
-pypm install pip
-pypm install virtualenv
-pypm install pil
+pypm --non-interactive distribute
+pypm --non-interactive install pip
+pypm --non-interactive install virtualenv
+pypm --non-interactive install pil
 virtualenv --system-site-packages --distribute podiobooks-env
 call podiobooks-env\Scripts\activate.bat
 pip install -r podiobooks\requirements_dev.txt
