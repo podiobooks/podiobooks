221d41ffff09591b51cc483395c31383b1f5c1ef
cyface <tim@cyface.com>
2012-06-25 17:41:06 -0600

    Updated tests to work with Django 1.4.

--- podiobooks/core/tests/tests_urls.py
+++ podiobooks/core/tests/tests_urls.py
@@ -11,65 +11,65 @@ class UrlTestCase(TestCase):
     def setUp(self):
         self.c = Client()
     
-    def testHome(self):
+    def test_home(self):
         response = self.c.get('/')
         self.assertEquals(200, response.status_code)
     
-    def testTitleList(self):
+    def test_title_list(self):
         response = self.c.get('/title/')
         self.assertEquals(200, response.status_code)
         
-    def testTitleSearchPage(self):
+    def test_title_search_page(self):
         response = self.c.get('/title/search/')
         self.assertEquals(200, response.status_code)
         
-    def testTitleSearchKeywords(self):
+    def test_title_search_keywords(self):
         response = self.c.get('/title/search/science%20fiction/')
         self.assertEquals(200, response.status_code)
         
-    def testTitleSearchKeywordsPost(self):
+    def test_title_search_keywords_post(self):
         response = self.c.post('/title/search/', {"keywords": "science%20fiction"})
         self.assertEquals(200, response.status_code)
         
         response = self.c.post('/title/search/', {"keywords": "dollie", "include_adult": 1, "completed_only": 1})
         self.assertEquals(200, response.status_code)
         
-    def testTitleSummary(self):
+    def test_title_summary(self):
         response = self.c.get('/title/summary/293/') # Double Share
         self.assertEquals(200, response.status_code)
         
-    def testTitleSnippet(self):
+    def test_title_snippet(self):
         response = self.c.get('/title/snippet/293/') # Double Share
         self.assertEquals(200, response.status_code)
         
-    def testTitleDetail(self):
+    def test_title_detail(self):
         response = self.c.get('/title/trader-tales-4-double-share/')
         self.assertEquals(200, response.status_code)
         
-    def testCategoryList(self):
+    def test_category_list(self):
         response = self.c.get('/category/')
         self.assertEquals(200, response.status_code)
         
-    def testCategoryDetail(self):
+    def test_category_detail(self):
         response = self.c.get('/category/science-fiction/')
         self.assertEquals(200, response.status_code)
     
-    def testContributorList(self):
+    def test_contributor_list(self):
         response = self.c.get('/contributor/')
         self.assertEquals(200, response.status_code)
         
-    def testContributorDetail(self):
+    def test_contributor_detail(self):
         response = self.c.get('/contributor/nathan-lowell/')
         self.assertEquals(200, response.status_code)
         
-    def testEpisodeDetail(self):
+    def test_episode_detail(self):
         response = self.c.get('/episode/68250/') # Double Share, Episode 1
         self.assertEquals(200, response.status_code)
 
-    def testSeriesList(self):
+    def test_series_list(self):
         response = self.c.get('/series/')
         self.assertEquals(200, response.status_code)
         
-    def testSeriesDetail(self):
+    def test_series_detail(self):
         response = self.c.get('/series/a-traders-tale-from-the-golden-age-of-the-solar-clipper/')
         self.assertEquals(200, response.status_code)
