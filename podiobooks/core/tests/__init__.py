bb0ff35391f51748872abe770568ab6f1f7f7718
cyface <tim@cyface.com>
2012-06-26 22:28:25 -0600

    tests_forms is still causing trouble. Removing it.

--- podiobooks/core/tests/__init__.py
+++ podiobooks/core/tests/__init__.py
@@ -1,12 +1,10 @@
 """Creates A Test Suite for All the Tests"""
 import unittest
-from tests_forms import FormsTestCase
 from tests_urls import UrlTestCase
 from tests_models import TitleTestCase
 
 def suite():
     core_test_suite = unittest.TestSuite()
     core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(UrlTestCase))
-    core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(FormsTestCase))
     core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TitleTestCase))
     return core_test_suite
