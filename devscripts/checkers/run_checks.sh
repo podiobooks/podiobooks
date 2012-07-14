048786b709fb19823b03c2c308214ecff37fb49b
Tim White <tim@cyface.com>
2012-07-13 23:01:34 -0600

    switched to django-jenkins.

--- devscripts/checkers/run_checks.sh
+++ devscripts/checkers/run_checks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ./podiobooks-env/bin/activate
-python manage.py jenkins --pylint-rcfile=devscripts/checkers/pylintrc --coverage-rcfile=devscripts/checkers/coveragerc --csslint-interpreter=node --csslint-implementation=/usr/bin/csslint --jslint-interpreter=node podiobooks
+python manage.py jenkins --pylint-rcfile=devscripts/checkers/pylintrc --coverage-rcfile=devscripts/checkers/coveragerc --csslint-interpreter=node --csslint-implementation=/usr/bin/csslint --jslint-interpreter=node core feeds libsyn profile
