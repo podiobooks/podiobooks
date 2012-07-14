73e50325d052e3b1fa5a2712d6976f6e9db6dcd9
Tim White <tim@cyface.com>
2012-07-13 22:46:16 -0600

    switched to django-jenkins.

--- devscripts/virtualenv/setup_dev_env.sh
+++ devscripts/virtualenv/setup_dev_env.sh
@@ -1,8 +1,8 @@
-# You must have virtualenv installed, and the virualenv command in your path for this to work.
+# You must have virtualenv installed, and the virtualenv command in your path for this to work.
 # Assuming you have python installed, you can install virtualenv using the command below.
 # curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
-# This should be run from the project directory, not inside the socialprofile dir
+# This should be run from the project directory, not inside the project dir
 
-virtualenv --distribute --system-site-packages podiobooks-env
-. podiobooks-env/bin/activate
-pip install -r podiobooks/requirements_dev.txt
+virtualenv --system-site-packages podiobooks-env
+. ./podiobooks-env/bin/activate
+pip install -r ./podiobooks/requirements_dev.txt
--- podiobooks/settings_test.py
+++ podiobooks/settings_test.py
@@ -14,14 +14,24 @@ except ImportError:
 import tempfile
 
 # Test DB settings. (SQLLite)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'test_sqlite.db'),
-        'TEST_NAME': os.path.join(PROJECT_ROOT, 'test_sqlite.db'),
-        }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(PROJECT_ROOT, 'test_sqlite.db'),
+#        'TEST_NAME': os.path.join(PROJECT_ROOT, 'test_sqlite.db'),
+#        }
+#}
 
-INSTALLED_APPS += ('django_nose',)
+INSTALLED_APPS += ('django_jenkins',)
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner' # Should work with Django 1.2.1
+
+# JENKINS REPORTS
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount',
+)
