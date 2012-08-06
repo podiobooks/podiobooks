b348920412fa7ac51ae81f7d96ce168e6b776df7
Tim White <tim@cyface.com>
2012-08-05 21:50:50 -0600

    Got jPlayer working on Detail Page.  Missing items on detail page are author homepage and alternate renditions.  Need to load that data in dataload process before I can adjust page to use it.

--- podiobooks/core/context_processors.py
+++ podiobooks/core/context_processors.py
@@ -8,5 +8,8 @@ def js_api_keys(request): # pylint: disable=W0613
     """
     Adds JavaScript API Keys variables to the context.
     """
-    return { 'GOOGLE_JS_API_KEY': settings.GOOGLE_JS_API_KEY,
-             'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY,}
+    return {
+        'GOOGLE_JS_API_KEY': settings.GOOGLE_JS_API_KEY,
+        'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY,
+        'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID
+    }
