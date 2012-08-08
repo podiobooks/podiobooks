5e185afbd3c91d11ba42b7e2921940a2f1076a41
cyface <tim@cyface.com>
2012-08-08 13:11:53 -0600

    tweaked schema migration.
    Former-commit-id: 340eefeeaa15b20e745b98bba920f56282e2e977
    Former-commit-id: d2a722fa6a4d66623c939dc5a35e4bf5c714d214
    Former-commit-id: e7833764d300b8216bea7637c604b993eaa1d927
    Former-commit-id: e3c2dc505aa377e47b926e8b8b9286a7cd71258a
    Former-commit-id: 80178beb5cf63c1d32df15ccf3f5d5265096738e
    Former-commit-id: f253697d184ca686f4b7420e44308c35a5accc4d

--- podiobooks/core/models.py
+++ podiobooks/core/models.py
@@ -22,13 +22,14 @@ class Advisory(models.Model):
     date_updated = models.DateTimeField(default=datetime.datetime.now())
     # rather than storing a hex-color, would it make more sense to
     # add a css class 'Advisory_{slug}' for flexibility??
-        
+
     class Meta:
         verbose_name_plural = "advisories"
         ordering = ['name']
 
     def __unicode__(self):
         return self.name
+
 
 class Award(models.Model):
     """Awards are just that: awards for a title, like winning a Parsec, etc."""
@@ -39,12 +40,13 @@ class Award(models.Model):
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         ordering = ['name']
-    
+
     def __unicode__(self):
         return self.name
+
 
 class Category(models.Model):
     """Categories describe titles for easy of browsing and for recommendations."""
@@ -55,7 +57,7 @@ class Category(models.Model):
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-        
+
     class Meta:
         verbose_name_plural = "categories"
         ordering = ['name']
@@ -66,6 +68,7 @@ class Category(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('category_detail', [self.slug])
+
 
 class Contributor(models.Model):
     """A contributor is one who had done work on a title. For a book, it's an
@@ -87,10 +90,11 @@ class Contributor(models.Model):
 
     def __unicode__(self):
         return self.display_name
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('contributor_detail', [self.slug])
+
 
 class ContributorType(models.Model):
     """Types of contributors: author, key grid, best boy, director, etc."""
@@ -98,12 +102,13 @@ class ContributorType(models.Model):
     name = models.CharField(max_length=255)
     byline_text = models.CharField(max_length=255)
     # Note: TitleContributor Objects (intermediate table) are available as titlecontributors.all()
-    
+
     class Meta:
         verbose_name_plural = "Contributor Types"
 
     def __unicode__(self):
         return self.name
+
 
 class Episode(models.Model):
     """Titles are composed of Episodes. For a book, these are chapters or
@@ -121,20 +126,22 @@ class Episode(models.Model):
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         ordering = ['title__name', 'sequence']
-    
+
     def __unicode__(self):
         return self.title.name + ': Episode #' + str(self.sequence) + ': ' + self.name
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('episode_detail', [self.id]) # pylint: disable=E1101
-    
+
     def _get_filesize_mb(self):
         return round(self.filesize / 1024.0 / 1024.0, 2)
+
     filesize_mb = property(_get_filesize_mb)
+
 
 class EpisodeContributor(models.Model):
     """Join table to associate contributors to titles."""
@@ -142,9 +149,10 @@ class EpisodeContributor(models.Model):
     contributor = models.ForeignKey('Contributor', related_name='episodecontributors')
     contributor_type = models.ForeignKey('ContributorType', related_name='episodecontributors')
     date_created = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         verbose_name_plural = "Episode Contributors"
+
 
 class License(models.Model):
     """A collection of defined licenses for works. Creative Commons, All Rights
@@ -156,30 +164,32 @@ class License(models.Model):
     code = models.TextField(blank=True)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         ordering = ['slug']
-    
+
     def __unicode__(self):
         return self.slug
+
 
 class Media(models.Model):
     """Media are links to other forms of the title. In the case of books, these
     would be dead tree editions, epub, etc."""
     title = models.ForeignKey('Title', related_name='media')
-    name = models.CharField(max_length=255)
-    identifier = models.CharField(max_length=255, help_text="ISBN or Product ID")
+    name = models.CharField(max_length=255, default="Book Version")
+    identifier = models.CharField(max_length=255, blank=True, help_text="ISBN or Product ID")
     url = models.CharField(max_length=255, blank=True)
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-        
+
     class Meta:
         verbose_name_plural = "media"
         ordering = ['name']
 
     def __unicode__(self):
         return self.name
+
 
 class Partner(models.Model):
     """Partners are sites or organizations which contribute works to the system
@@ -191,12 +201,13 @@ class Partner(models.Model):
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         ordering = ['name']
-    
+
     def __unicode__(self):
         return "Modelname"
+
 
 class Promo(models.Model):
     """Promotional materials for a title. Built to allow many types of
@@ -208,23 +219,25 @@ class Promo(models.Model):
     display_order = models.IntegerField(null=False, default=1)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         ordering = ['name']
-    
+
     def __unicode__(self):
         return self.name
-    
+
+
 class Rating(models.Model):
     """The last rating that was loaded from the pb1 site"""
     last_rating_id = models.IntegerField(default=0, db_index=True)
     date_created = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         get_latest_by = 'date_created'
-    
+
     def __unicode__(self):
         return str(self.last_rating_id)
+
 
 class Series(models.Model):
     """Titles can belong to a series, which allows for higher level grouping."""
@@ -235,7 +248,7 @@ class Series(models.Model):
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-        
+
     class Meta:
         verbose_name_plural = "series"
         ordering = ['name']
@@ -289,26 +302,27 @@ class Title(models.Model):
     # Note: promos are available as promos.all()
     date_created = models.DateTimeField(default=datetime.datetime.now(), db_index=True)
     date_updated = models.DateTimeField(default=datetime.datetime.now(), db_index=True)
-    
+
     class Meta:
         ordering = ['name']
-        
+
     def __unicode__(self):
         return self.slug
 
     @models.permalink
     def get_absolute_url(self):
         return ('title_detail', [self.slug])
-    
+
     def net_promoter_score(self):
         total_count = self.promoter_count + self.detractor_count
         if total_count:
             return int((self.promoter_count / total_count) * 100)
         else:
             return 0
-    
+
     def description_br(self):
         return self.description.replace('\n', '\n<br/>') # pylint: disable=E1101
+
 
 class TitleCategory(models.Model):
     """
@@ -317,7 +331,7 @@ class TitleCategory(models.Model):
     """
     title = models.ForeignKey('Title', related_name='titlecategories')
     category = models.ForeignKey('Category', related_name='titlecategories')
-    
+
     class Meta:
         verbose_name_plural = "Title Categories"
 
@@ -325,11 +339,11 @@ class TitleCategory(models.Model):
 def update_category_list(sender, instance, **kwargs):
     """ Update category list cache on titles when a new title category is added...hooked to pre_save trigger for titlecategory below """
     categories = instance.title.categories.all()
-    category_list = render_to_string('core/title/tags/show_categories.html', {'categories': categories,})
-    
+    category_list = render_to_string('core/title/tags/show_categories.html', {'categories': categories, })
+
     instance.title.category_list = category_list
     instance.title.save()
-    
+
 post_save.connect(update_category_list, sender=TitleCategory)
 
 class TitleContributor(models.Model):
@@ -339,7 +353,7 @@ class TitleContributor(models.Model):
     contributor_type = models.ForeignKey('ContributorType', related_name='titlecontributors')
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-    
+
     class Meta:
         verbose_name_plural = "Title Contributors"
         ordering = ['contributor_type__slug', 'date_created']
@@ -351,11 +365,11 @@ class TitleContributor(models.Model):
 def update_byline(sender, instance, **kwargs):
     """ Update byline cache on titles when a new title contributor is added...hooked to pre_save trigger for titlecontributor below """
     titlecontributors = instance.title.titlecontributors.all().order_by('contributor_type__slug', 'date_created')
-    byline = render_to_string('core/title/tags/show_contributors.html', {'titlecontributors': titlecontributors,})
-    
+    byline = render_to_string('core/title/tags/show_contributors.html', {'titlecontributors': titlecontributors, })
+
     instance.title.byline = byline.strip()
     instance.title.save()
-    
+
 post_save.connect(update_byline, sender=TitleContributor) # Fires update_byline when a TitleContributor is saved
 
 class TitleUrl(models.Model):
@@ -366,7 +380,8 @@ class TitleUrl(models.Model):
     displayorder = models.IntegerField(null=False, default=1)
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_updated = models.DateTimeField(default=datetime.datetime.now())
-        
+
     def __unicode__(self):
         return "TitleUrls"
-# 
+
+    #
