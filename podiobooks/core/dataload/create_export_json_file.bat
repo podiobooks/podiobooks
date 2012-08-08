51b9edc01e586ecbbf16934fd27b45fcfec6c1ad
cyface <tim@cyface.com>
2012-08-08 08:10:17 -0600

    Added import of Media Items to CSV migration script. Updated DB Dump Scripts and alldata fixture with new media data.

--- podiobooks/core/dataload/create_export_json_file.bat
+++ podiobooks/core/dataload/create_export_json_file.bat
@@ -1,4 +1,3 @@
-REM Run this script from the podiobooks\podiobooks dir (as ..\dbscripts\migrate_allcsv_through_model.bat).
-SET DJANGO_SETTINGS_MODULE=podiobooks.settings
-call ..\podiobooks-env\Scripts\activate.bat
-python manage.py dumpdata main > main/fixtures/alldata.json
+REM Run this script from the podiobooks dir (as ..\podiobooks\core\dataload\migrate_allcsv_through_model.bat).
+call .\podiobooks-env\Scripts\activate.bat
+python manage.py dumpdata core > podiobooks\core\fixtures\alldata.json
