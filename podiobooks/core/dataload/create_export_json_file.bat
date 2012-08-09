bd8e35b41ff63ac330ec15ea9c1af41a79e49564
cyface <tim@cyface.com>
2012-08-09 08:09:11 -0600

    Moved dataload scripts out of project.
    Former-commit-id: be6d196eb90e6a2168b5757f80019c8d41b1de3c
    Former-commit-id: 711a599e1bcdd012e4266a75f2bae6740609dc5d
    Former-commit-id: 84a2182eb29ee7c797fca5b1a91a8834d6f8c48a
    Former-commit-id: b593cde6fd36c692e9ce683cb5841839716bd241
    Former-commit-id: cdf4a6a0612a568bb7ab21ba4f79f37792ba932f
    Former-commit-id: cd7ea19470069de1b075c96894f074a1bbb98a09

--- podiobooks/core/dataload/create_export_json_file.bat
+++
@@ -1,3 +0,0 @@
-REM Run this script from the podiobooks dir (as ..\podiobooks\core\dataload\migrate_allcsv_through_model.bat).
-call .\podiobooks-env\Scripts\activate.bat
-python manage.py dumpdata core > podiobooks\core\fixtures\alldata.json
