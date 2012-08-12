7b47b24d6083d576016a47ec30266ca63aa63c52
Tim White <tim@cyface.com>
2012-08-12 12:34:08 -0600

    Added context processor for site name. Changed all templates to use SITE.name instead of site_name.
    Former-commit-id: d0ac5510a562dee017a70dd1d4e815b4e9210358
    Former-commit-id: 61b840e5dd6509940fe8b7a6db613723805e7425
    Former-commit-id: 61113df1298bb5ac66a2e446838b6b3337cc54df
    Former-commit-id: 4854f9c5190918459482acdc099a1bf6222dce9c
    Former-commit-id: d8e8d8705b9f82dace8d83544794627bd95ae5a3
    Former-commit-id: b42c30bc6029d99adfc36e517898f73d93fe501c

--- podiobooks/core/context_processors.py
+++ podiobooks/core/context_processors.py
@@ -2,9 +2,12 @@
 Special Context Processors (adds variables to template contexts) for Podiobooks
 """
 
-from django.conf import settings
+# pylint: disable=W0613
 
-def js_api_keys(request): # pylint: disable=W0613
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+def js_api_keys(request):
     """
     Adds JavaScript API Keys variables to the context.
     """
@@ -13,3 +16,7 @@ def js_api_keys(request): # pylint: disable=W0613
         'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY,
         'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID
     }
+
+def current_site(request):
+    """Adds current site object to context"""
+    return {'SITE': Site.objects.get_current()}
