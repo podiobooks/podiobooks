71ca2410bd90e236223d88e564e7e2d470e804df
cyface <tim@cyface.com>
2012-09-14 12:21:03 -0600

    Rollback
    Former-commit-id: b9952b7be804f7531b566e71c6eebf53eb84e1a1
    Former-commit-id: 55fa14c43413f8f5c44de35817894ffb983cd1cb
    Former-commit-id: 708375c8b8a5a440aa561d980fb9c7a35b6c2cd4
    Former-commit-id: c49413301e84e08b4043adc4c17aba6eb6eeae92
    Former-commit-id: d91609d096d96d504299ce5ab268af3e2ab3dcbe
    Former-commit-id: fddb87a10af2b5c8210db67ded77ca524779d744

--- devscripts/gondor/gondor_deploy_primary_master.bat
+++
@@ -1,4 +0,0 @@
-REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
-call ..\podiobooks-env\Scripts\activate.bat
-REM This deploys the currently checked-in GIT Master Branch to the primary Gondor Instance
-gondor deploy primary master
