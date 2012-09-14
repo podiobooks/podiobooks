6420ac195e7967d50559b4d8da6e1498ed5224c0
cyface <tim@cyface.com>
2012-09-14 12:45:06 -0600

    Updated gondor scripts.
    Former-commit-id: 0fc019c0f05be1a5c5ccf8ec0e0cf00caadda860
    Former-commit-id: c6ba8cb0b2ff6d93840a8ef781d57bd2450f7567
    Former-commit-id: 812a48b15b5785f0b9b607da12318ea74a6c16c9
    Former-commit-id: 9df84d563e8ec14f5d0f2f80252f70a7cea068d1
    Former-commit-id: d3b69b12a3d631d9792240785b9ee8eb6056c498
    Former-commit-id: 5a64322f2487e22184c6916f01924bc6677bcfc3

--- devscripts/gondor/gondor_configure_settings_env.bat
+++ devscripts/gondor/gondor_configure_settings_env.bat
@@ -1,4 +1,4 @@
 REM This needs to be run from the directory where the gondor.yml file lives
-call podiobooks-env\Scripts\activate.bat
+call .\podiobooks-env\Scripts\activate.bat
 REM This sets the gondor environment to point at the correct settings file.
 gondor env:set primary DJANGO_SETTINGS_MODULE=podiobooks.settings_gondor
