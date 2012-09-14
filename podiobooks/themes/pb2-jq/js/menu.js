97285f95935086593c826b30baa1b4ea59359790
Brant Steen <brant.steen@gmail.com>
2012-09-14 09:48:53 -0400

    fleshing out nav bar for small screen
    Former-commit-id: cff1ee9150711c6b1ee7899c04912b235f04d352
    Former-commit-id: 8a8e2c332916017a1ef61867da25a4410a22af1f
    Former-commit-id: bb7dc92b6d6e83c92312709625d241dc1150ee77
    Former-commit-id: 8b48100b4d5830552438ecf8186792776e1874b7
    Former-commit-id: 5dd8b769c18b475c8479c972bf29b4e2da207073
    Former-commit-id: 97209181336fb3ee855e17522c06ebc7c34405c4

--- podiobooks/themes/pb2-jq/js/menu.js
+++ podiobooks/themes/pb2-jq/js/menu.js
@@ -5,7 +5,7 @@ $(function(){
 		$(this).clone().appendTo(menu);
 	});
 	$("<ul><li><a href='" + siteVars("home") + "'>Home</a></li></ul>").prependTo(menu);
-	var triggers = $("<ul class='menu-triggers' />").appendTo(".nav-bar");
+	var triggers = $("<ul class='menu-triggers' />").prependTo(".nav-bar");
 	var menuTrigger = $("<li><a href='#' class='menu-trigger' id='site-menu-trigger'><img src='" + siteVars("img") + "pb-logo-icon.png' /></a></li>").appendTo(triggers);
 	
 	$("#site-menu-trigger").triggeredMenu({
