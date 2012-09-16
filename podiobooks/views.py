d4329243fec87c022594acff7aad0f37a721d078
Tim White <tim@cyface.com>
2012-09-16 10:24:23 -0600

    Robots.txt, humans.txt, crossdomain.xml updates. Fixes #109.

---
+++ podiobooks/views.py
@@ -0,0 +1,23 @@
+""" Django Views for the Overall Site"""
+
+from django.views.generic import TemplateView
+from django.contrib.sites.models import Site
+
+class TextTemplateView(TemplateView):
+    """Utility View to Render text/plain MIME Type"""
+    def render_to_response(self, context, **response_kwargs):
+        """Returns a Template as text/plain"""
+        response_kwargs['mimetype'] = 'text/plain'
+        return super(TemplateView, self).render_to_response(context, **response_kwargs)
+
+class RobotsView(TextTemplateView):
+    """
+    Routes robots to the robots.txt file depending on the site name.
+    """
+
+    def get_template_names(self, **kwargs):
+        current_site = Site.objects.get_current()
+        if current_site.domain == 'podiobooks.com' or current_site.domain == 'www.podiobooks.com':
+            return ['robots_prod.txt']
+        else:
+            return ['robots.txt']
