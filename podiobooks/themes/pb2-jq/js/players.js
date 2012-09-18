8955d2bba566e55082c1cbbafb961cacb30c1a39
Brant Steen <brant.steen@gmail.com>
2012-09-18 12:12:02 -0400

    Fixing up an issue with browsers that are HTML5 audio capable but that don't know how to play MP3's (e.g. FireFox)

--- podiobooks/themes/pb2-jq/js/players.js
+++ podiobooks/themes/pb2-jq/js/players.js
@@ -1,57 +1,69 @@
 $(function(){
 	
+	/* href is location of audio file, ele is where to put this thing */
+	var createHTML5Player = function(ele, href){
+		
+		$("<h2>Play First Chapter</h2><audio controls><source src='" + href + "' type='audio/mp3' codecs='mp3'></audio>").appendTo(ele);
+	};
+	
+	/* href is location of audio file, ele is where to put this thing */
+	var createFlashPlayer = function(ele, href){
+		var replaceMe = $("<span id='replaceMeYo' />").appendTo(ele);
+		AudioPlayer.embed(replaceMe.attr("id"), {
+			autostart:"no",
+			loop:"no",
+			animation:"yes",
+			remaining:"no",
+			noinfo:"yes",
+			initialvolume:100,
+			buffer:5,
+			width:290,
+			download:"yes",
+			transparentpagebg:"yes",
+			bg:"d9e4f0",
+			leftbg:"a9d4f9",
+			lefticon:"06539e",
+			voltrack:"06539e",
+			volslider:"fefefe",
+			rightbg:"06539e",
+			rightbghover:"a9d4f9",
+			righticon:"fefefe",
+			righticonhover:"fefefe",
+			loader:"06539e",
+			track:"fefefe",
+			tracker:"06539e",
+			border:"06539e",
+			skip:"666666",
+			text:"6b6b6b", 
+			soundFile:href
+		});		
+	};
+	
 	var episodes = $(".episode-audio-link:first");
 	var placement = $(".title-description p:first");
 	var htmlAudio = $("html").hasClass("audio");
+	
 	// check for HTML5 audio capabilities first
 	if (episodes.length > 0){
+		
 		// check for MP3 capabilities
-		
-		
-		
 		episodes.each(function(){
 			
 			var link = $(this);
 			var href = link.attr("href");
 			var audio = $("<aside id='title-first-chapter-player' class='first-chapter'></aside>").insertBefore(placement);
+			
 			if (htmlAudio){
 				var browserAudio = new Audio();
-				if(browserAudio && browserAudio.canPlayType("audio/mpeg")){
-					$("<h2>Play First Chapter</h2><audio controls><source src='" + href + "' type='audio/mp3' codecs='mp3'></audio>").appendTo(audio);
-					
+				if(browserAudio && browserAudio.canPlayType("audio/mpeg")){					
+					createHTML5Player(audio, href);
+				}
+				else{
+					createFlashPlayer(audio, href);
 				}
 			}
 			else{
-				var replaceMe = $("<span id='replaceMeYo' />").appendTo(audio);
-				AudioPlayer.embed(replaceMe.attr("id"), {
-					autostart:"no",
-					loop:"no",
-					animation:"yes",
-					remaining:"no",
-					noinfo:"yes",
-					initialvolume:100,
-					buffer:5,
-					width:290,
-					download:"yes",
-					transparentpagebg:"yes",
-					bg:"d9e4f0",
-					leftbg:"a9d4f9",
-					lefticon:"06539e",
-					voltrack:"06539e",
-					volslider:"fefefe",
-					rightbg:"06539e",
-					rightbghover:"a9d4f9",
-					righticon:"fefefe",
-					righticonhover:"fefefe",
-					loader:"06539e",
-					track:"fefefe",
-					tracker:"06539e",
-					border:"06539e",
-					skip:"666666",
-					text:"6b6b6b", 
-					soundFile:href
-				});
-				
+				createFlashPlayer(audio, href);
 				$("<h2>Play First Chapter</h2>").prependTo(audio);
 			}
 			$(".title-details-infobar").insertAfter(audio);
