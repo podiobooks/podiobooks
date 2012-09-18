47a9756b6a40a59490e29b64dea5cdc04944c140
Tim White <tim@cyface.com>
2012-09-17 22:03:32 -0600

    Changed /rss/feeds/titles to dump all titles, added /rss/feeds/titles/recent to dump 30 most recent titles.

--- podiobooks/feeds/urls.py
+++ podiobooks/feeds/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import patterns, url
 
-from podiobooks.feeds.feeds import TitleFeed, EpisodeFeed
+from podiobooks.feeds.feeds import EpisodeFeed, RecentTitleFeed, TitleFeed
 
 urlpatterns = patterns('',
     # feeds
     url(r'^feeds/titles/$', TitleFeed(), name="all_titles_feed"),
+    url(r'^feeds/titles/recent/$', RecentTitleFeed(), name="recent_titles_feed"),
     url(r'^feeds/episodes/(?P<title_slug>[^/]+)/$', EpisodeFeed(), name="title_episodes_feed"),
  )
