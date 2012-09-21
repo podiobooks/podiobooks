b071918926f7a6514de0a68030a8b2d860fc879a
Tim White <tim@cyface.com>
2012-09-20 20:43:49 -0600

    Pylint cleanup.
    Former-commit-id: 92cd81ee2305a53770beb3971c23e9cfbd6e0420
    Former-commit-id: feab75b4ecf2cc2a5241c86bd41ad2cef637e5e8
    Former-commit-id: aa31abc082263bb82dd780cf71bfa5bc9e007c69
    Former-commit-id: 0377d9d54f8ae5cdd4e4a68d551464efc4d51e4e
    Former-commit-id: 965addc1ca1152b3dda5cb41219b4ed05e7502d3
    Former-commit-id: 5543e62ae1d041da7c993b1be0cf07a28d0b73dc

--- podiobooks/feeds/feeds.py
+++ podiobooks/feeds/feeds.py
@@ -1,6 +1,6 @@
 """RSS Feed Definitions for Podiobooks, uses the Django Syndication Framework"""
 
-# pylint: disable=R0201, C0111, R0904, R0801
+# pylint: disable=R0201, C0111, R0904, R0801, F0401
 
 from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Rss201rev2Feed
