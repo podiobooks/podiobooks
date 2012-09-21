eba1a3bace0b15abf785dfc6bdcbd9e58e2adc9e
Tim White <tim@cyface.com>
2012-09-20 19:58:47 -0600

    Added gondor script to update alldata fixture.

---
+++ devscripts/gondor/gondor_update_alldata.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Extracts prod data to update alldata fixture.
+rm podiobooks/core/fixtures/alldata.json.zip
+gondor run primary manage.py dumpdata core > podiobooks/core/fixtures/alldata.json
+ditto -ck --rsrc --sequesterRsrc podiobooks/core/fixtures/alldata.json alldata.json.zip
