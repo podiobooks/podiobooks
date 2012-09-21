d37212d448e8aa8e59c4e85618015a690e34ee5c
cyface <tim@cyface.com>
2012-09-21 12:14:24 -0600

    Moved Libsyn tests around to make more sense.
    Former-commit-id: 0b5c59c81dcd5e86f7370bfff21f60914a98ab2d
    Former-commit-id: fa42a2f5ae526ca60d8bb0a6d91b1ad78d717e68
    Former-commit-id: 12df275f8550e9fd1af56c60a7a70a8cce1bb1e7
    Former-commit-id: 7e8cd3448ccceab57bb962c726146bc650baf0ce
    Former-commit-id: 3821414861521ce51fd1ab9d78c32a8790eb5c43
    Former-commit-id: 350ad46d285cd8c47bfffbf32076d52b7be15e9c

---
+++ podiobooks/libsyn/tests_api.py
@@ -0,0 +1,34 @@
+"""Automated Tests of the Podiobooks LibSyn Interface"""
+
+# pylint: disable=C0103,C0111,R0904
+
+from django.test import TestCase
+from podiobooks.libsyn.libsyn_utils import get_show_info
+from podiobooks.libsyn import libsyn_method_help
+from podiobooks.libsyn import libsyn_system_methods
+import pprint
+
+class LibsynAPITestCase(TestCase):
+    # fixtures = []
+
+    def setUp(self):
+        self.pprinter = pprint.PrettyPrinter(indent=4)
+
+    def testShowInfo(self):
+        show_info = get_show_info('theflownsky')
+        self.pprinter.pprint(show_info) # pretty print the result
+        self.assertEquals('k-9b89823b4508200f', show_info['show_id'])
+
+        show_info = get_show_info('notesfromthevault')
+        self.pprinter.pprint(show_info)
+        self.assertEquals('k-27813e6f24530266', show_info['show_id'])
+
+        show_info = get_show_info('irondragons')
+        self.pprinter.pprint(show_info)
+        self.assertEquals('k-e76e81ee69d5d413', show_info['show_id'])
+
+    def testMethodHelp(self):
+        libsyn_method_help.main()
+
+    def testSystemMethods(self):
+        libsyn_system_methods.main()
