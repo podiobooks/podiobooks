f91ec34ab53f771d2ca14904024627b8eb5c5a75
Tim White <tim@cyface.com>
2012-09-23 12:37:57 -0600

    Many redirects.
    Former-commit-id: 006c04a24a6c14604eec6576222f658c15e45436
    Former-commit-id: a92d7b91486d0c5e9f66b194b35c90f69798426f
    Former-commit-id: c1dd3ec4f9ea8dd136b2b079885d672c38575a34
    Former-commit-id: 9e49cf1390b4f0591669e1eb60ecec0f1c164971
    Former-commit-id: 69474ee8ff79e5e697669f2f51d440d711d06c29
    Former-commit-id: 3dc95fd02f609d90d97fb9ae0e1aeba0912fc5de

--- podiobooks/core/tests/tests_urls.py
+++ podiobooks/core/tests/tests_urls.py
@@ -7,61 +7,61 @@ from django.test.client import Client
 
 class UrlTestCase(TestCase):
     fixtures = ['test_data.json', ]
-    
+
     def setUp(self):
         pass
-    
+
     def test_home(self):
         response = self.client.get('/')
         self.assertEquals(200, response.status_code)
-    
+
     def test_title_list(self):
         response = self.client.get('/title/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_title_search_page(self):
         response = self.client.get('/title/search/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_title_search_keywords(self):
         response = self.client.get('/title/search/science%20fiction/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_title_search_keywords_post(self):
         response = self.client.post('/title/search/', {"keywords": "science%20fiction"})
         self.assertEquals(200, response.status_code)
-        
+
         response = self.client.post('/title/search/', {"keywords": "dollie", "include_adult": 1, "completed_only": 1})
         self.assertEquals(200, response.status_code)
 
     def test_title_search_keywords_get(self):
-        response = self.client.get('/title/search/?keyword=sigler')
+        response = self.client.get('/title/search/', {'keyword': 'sigler'})
         self.assertEquals(200, response.status_code)
 
     def test_title_search_pb1(self):
-        response = self.client.get('/podiobooks/search.php?keyword=sigler')
+        response = self.client.get('/podiobooks/search.php', {'keyword': 'sigler'})
         self.assertRedirects(response, '/title/search/?keyword=sigler', status_code=301)
 
     def test_title_category_search_pb1(self):
-        response = self.client.get('/podiobooks/search.php?category=1', follow=True)
+        response = self.client.get('/podiobooks/search.php', {'category': 1}, follow=True)
         self.assertRedirects(response, '/category/science-fiction/', status_code=301)
-        
+
     def test_title_detail(self):
         response = self.client.get('/title/trader-tales-4-double-share/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_category_list(self):
         response = self.client.get('/category/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_category_detail(self):
         response = self.client.get('/category/science-fiction/')
         self.assertEquals(200, response.status_code)
-    
+
     def test_contributor_list(self):
         response = self.client.get('/contributor/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_contributor_detail(self):
         response = self.client.get('/contributor/nathan-lowell/')
         self.assertEquals(200, response.status_code)
@@ -73,7 +73,7 @@ class UrlTestCase(TestCase):
     def test_series_list(self):
         response = self.client.get('/series/')
         self.assertEquals(200, response.status_code)
-        
+
     def test_series_detail(self):
         response = self.client.get('/series/a-traders-tale-from-the-golden-age-of-the-solar-clipper/')
         self.assertEquals(200, response.status_code)
@@ -88,6 +88,14 @@ class UrlTestCase(TestCase):
 
     def test_pb1_charts_redirect(self):
         response = self.client.get('/charts.php')
+        self.assertEquals(301, response.status_code)
+
+#    def test_pb1_book_redirect(self):
+#        response = self.client.get('/book.php', {'ID': '55'})
+#        self.assertEquals(301, response.status_code)
+
+    def test_pb1_xml_redirect(self):
+        response = self.client.get('/index.xml')
         self.assertEquals(301, response.status_code)
 
     def test_pb1_feed_redirect(self):
