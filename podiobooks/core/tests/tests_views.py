bd3d78c350d07967e0ce0d3d06cb5c63b1bff325
Brant Steen <brant.steen@gmail.com>
2012-09-23 17:53:09 -0400

    Separating homepage shelf views

---
+++ podiobooks/core/tests/tests_views.py
@@ -0,0 +1,30 @@
+"""
+Test cases relating to specific views
+"""
+from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+
+class ShelfTestCase(TestCase):
+    """ Test homepage shelves """
+    def setUp(self):
+        self.client = Client()
+        
+    def test_non_existant(self):
+        """
+        When a shelf type doesn't exist, we should 404
+        """
+        resp = self.client.get(reverse("shelf", kwargs={"shelf_type": "asdf"}))
+        self.assertEquals(resp.status_code, 404)
+        
+    def test_existing(self):
+        """
+        Test existing shelves
+        """
+        shelf_types = ["featured_by_category", "top_rated_by_author", ]
+        
+        for shelf_type in shelf_types:
+            resp = self.client.get(reverse("shelf", kwargs={"shelf_type": shelf_type}))
+            self.assertEquals(resp.status_code, 200)
+            resp = self.client.get(reverse("shelf", kwargs={"shelf_type": "featured_by_category", "title_filter": "asdf"}))
+            self.assertEquals(resp.status_code, 200)
