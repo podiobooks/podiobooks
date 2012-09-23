ac5d7cc8206de7d9fa021d7ab5a170f9f0289675
Brant Steen <brant.steen@gmail.com>
2012-09-23 12:13:33 -0400

    Removing some useless markup
    Former-commit-id: 0768bda02e745e147a2eb4fcec017d270dfad253
    Former-commit-id: 0c48d0356ea5085e8e8c4d0d8efa325512200edb
    Former-commit-id: b450b11671991dbc5393a59f1f251c99534164a2
    Former-commit-id: de982f544f3edea089da846e40c46cbe098bb504
    Former-commit-id: 61aee540582aff3d9f3f05761f0cfdd3f15cd6d6
    Former-commit-id: 30397a3668122bc851a04f44ba46d5777c78b62f

--- podiobooks/core/templatetags/shelf_templatetags.py
+++ podiobooks/core/templatetags/shelf_templatetags.py
@@ -4,24 +4,24 @@ from django import template
 
 register = template.Library()
 
+
 @register.inclusion_tag('core/shelf/tags/show_shelf.html')
-def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class, shelf_title_width):
+def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class):
     """Pulls in a template to show a title shelf for a particular set of titles"""
     return {'shelf_id': shelf_id,
             'shelf_title': shelf_title,
             'title_list': title_list,
             'dropdown_form': dropdown_form,
-            'base_css_class': base_css_class,
-            'shelf_title_width': shelf_title_width
+            'base_css_class': base_css_class
     }
 
+
 @register.inclusion_tag('core/shelf/tags/show_shelf_pages.html')
-def show_shelf_pages(shelf_id, shelf_name, title_list, shelf_title_width):
+def show_shelf_pages(shelf_id, shelf_name, title_list):
     """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""
     return {'shelf_id': shelf_id,
             'shelf_name': shelf_name,
-            'title_list': title_list[:18],
-            'shelf_title_width': shelf_title_width
+            'title_list': title_list[:18]
     }
 
 
