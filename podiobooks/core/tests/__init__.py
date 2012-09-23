932c5bee40e749e870a95dbbd76f444d61b4ce41
Brant Steen <brant.steen@gmail.com>
2012-09-23 17:53:09 -0400

    Separating homepage shelf views
    Former-commit-id: 2b16fdbf3e44c861856a18479f02070b77119ce9
    Former-commit-id: a891c9a457bec264fd087b9590cddf98c25f637e
    Former-commit-id: 212b81088040f7cbb796c7c17bffb783cbc4f47a
    Former-commit-id: f028e06de281e0a70353cd0bda4939bf4f1ac564
    Former-commit-id: 79b88322fa40add711e5ebc34cbdcbfde83d403d
    Former-commit-id: 955907ae71d5a0450a0e49c8764326bb2fe76e35

--- podiobooks/core/tests/__init__.py
+++ podiobooks/core/tests/__init__.py
@@ -1,10 +1,14 @@
 """Creates A Test Suite for All the Tests"""
 import unittest
-from tests_urls import UrlTestCase
-from tests_models import TitleTestCase
+
+from podiobooks.core.tests.tests_urls import UrlTestCase
+from podiobooks.core.tests.tests_models import TitleTestCase
+from podiobooks.core.tests.tests_views import ShelfTestCase
+
 
 def suite():
     core_test_suite = unittest.TestSuite()
     core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(UrlTestCase))
     core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TitleTestCase))
+    core_test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ShelfTestCase))
     return core_test_suite
