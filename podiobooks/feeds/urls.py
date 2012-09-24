0f3196bf436819d1e881a8f31bb28dc5808a0bc2
Tim White <tim@cyface.com>
2012-09-23 18:23:44 -0600

    Temp fix for earthcore slug rename...need to add 'old slug' functionality.

--- podiobooks/feeds/urls.py
+++ podiobooks/feeds/urls.py
@@ -2,11 +2,14 @@
 
 from django.conf.urls import patterns, url
 
+from django.views.generic import RedirectView
+
 from podiobooks.feeds.feeds import EpisodeFeed, RecentTitleFeed, TitleFeed
 
 urlpatterns = patterns('',
     # feeds
     url(r'^feeds/titles/$', TitleFeed(), name="all_titles_feed"),
     url(r'^feeds/titles/recent/$', RecentTitleFeed(), name="recent_titles_feed"),
+    url(r'^feeds/episodes/earthcore-by-scott-sigler/$', RedirectView.as_view(url='/rss/feeds/episodes/earthcore/')),
     url(r'^feeds/episodes/(?P<title_slug>[^/]+)/$', EpisodeFeed(), name="title_episodes_feed"),
  )
