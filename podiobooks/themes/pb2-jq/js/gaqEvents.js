f82b1f6f724c0971e9284858a99232751285b76d
cyface <tim@cyface.com>
2012-09-26 13:23:18 -0600

    Trying to make Brant's code actually work.

--- podiobooks/themes/pb2-jq/js/gaqEvents.js
+++ podiobooks/themes/pb2-jq/js/gaqEvents.js
@@ -1,102 +1,90 @@
-$(function(){
-	
-	/*
-	 * GA events for elements in the 'ways to consume this book' list
-	 * 
-	 * The .consume-list template is actually repeated in the markup
-	 * 		So, we're going to handle each iteration of the list separately. 
-	 */
-	$(".consume-list").each(function(){
-		var list = $(this);
-		
-		var slug = list.data("title-slug");
-		
-		list.find("#consume-itunes>a").each(function(){
-			$(this).click(function(){
-				_gaq.push(['_trackEvent', 'Consume', 'DetailPage-ViewInITunes', slug]);
-			});				
-		});
-		
-		
-		list.find("#consume-amazon>a").each(function(){
-			$(this).click(function(){
-				_gaq.push(['_trackEvent', 'Consume', 'DetailPage-BuyFromAmazon', slug]);
-			});
-		});
-		
-		list.find("#consume-smashwords>a").each(function(){
-			$(this).click(function(){
-				_gaq.push(['_trackEvent', 'Consume', 'DetailPage-BuyFromSmashwords', slug]);
-			});
-		});
-		
-		list.find("#consume-rss>a").each(function(){
-			$(this).click(function(){
-				_gaq.push(['_trackEvent', 'Consume', 'DetailPage-RSSFeed', slug]);
-			});
-		});
-		
-		list.find("#consume-rss>a").each(function(){
-			$(this).bind("contextmenu", function(){
-				_gaq.push(['_trackEvent', 'Consume', 'DetailPage-RSSFeedRC', slug]);
-			});
-		});
-		
-		
-		
-	});
-	
-	/*
-	 * GA events for elements in the 'Donate to this author' form
-	 * 
-	 * The .donate-box template is actually repeated in the markup
-	 * 		So, we're going to handle each iteration of the list separately. 
-	 */
-	$(".donate-box").each(function(){
-		var wrapper = $(this);
-		var slug = wrapper.data("title-slug");
-		
-		wrapper.find("#title-donate-submit").each(function(){
-			
-			var btn = $(this);
-			var form = btn.parents("form");
-			
-			var amount = form.find('input[name="amount"]').val();
-			
-			btn.bind("click", function(){
-				_gaq.push(['_trackEvent', 'Donate', 'DetailPage-Donate', slug, amount]);
-			});
-			
-		});	
-	});
-	
-	
-	/*
-	 * GA events for homepage shelves
-	 */
-	$(".shelf-item-heading a").each(function(){
-		var link = $(this);
-		link.click(function(ev){
-			var slug = link.data("title-slug");
-			if (slug){
-				_gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfTitleClick', slug, 1]);	
-			}
-		});
-	});
-	
-	$(".shelf-cover").each(function(){
-		var img = $(this);
-		var link = img.parent();
-		link.click(function(ev){
-			var slug = img.data("title-slug");
-			if (slug){
-				_gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfTitleClick', slug, 0]);
-			}
-		});
-	});
-	
-	$(".shelf-wrapper").delegate(".shelf-select-form select", "change", function(ev){
-		_gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfDropDown', $(this).val()]);
-	});
-	
+$(function () {
+
+    /*
+     * GA events for elements in the 'ways to consume this book' list
+     *
+     * The .consume-list template is actually repeated in the markup
+     * 		So, we're going to handle each iteration of the list separately.
+     */
+    $(".consume-list").each(function () {
+        var list = $(this);
+
+        var slug = list.data("title-slug");
+
+        list.find("#consume-itunes>a").each(function () {
+            $(this).click(function () {
+                _gaq.push(['_trackEvent', 'Consume', 'DetailPage-ViewInITunes', slug]);
+            });
+        });
+
+
+        list.find("#consume-amazon>a").each(function () {
+            $(this).click(function () {
+                _gaq.push(['_trackEvent', 'Consume', 'DetailPage-BuyFromAmazon', slug]);
+            });
+        });
+
+        list.find("#consume-smashwords>a").each(function () {
+            $(this).click(function () {
+                _gaq.push(['_trackEvent', 'Consume', 'DetailPage-BuyFromSmashwords', slug]);
+            });
+        });
+
+        list.find("#consume-rss>a").each(function () {
+            $(this).click(function () {
+                _gaq.push(['_trackEvent', 'Consume', 'DetailPage-RSSFeed', slug]);
+            });
+        });
+
+        list.find("#consume-rss>a").each(function () {
+            $(this).bind("contextmenu", function () {
+                _gaq.push(['_trackEvent', 'Consume', 'DetailPage-RSSFeedRC', slug]);
+            });
+        });
+
+
+    });
+
+    /*
+     * GA events for elements in the 'Donate to this author' form
+     *
+     * The .donate-box template is actually repeated in the markup
+     * 		So, we're going to handle each iteration of the list separately.
+     */
+    $("#title-donate-submit").bind("click", function () {
+        var slug = $(".donate-box").data("title-slug");
+        var form = $(this).parents("form");
+        var amount = form.find('input[name="amount"]').val();
+        _gaq.push(['_trackEvent', 'Donate', 'DetailPage-Donate', slug, amount]);
+    });
+
+
+    /*
+     * GA events for homepage shelves
+     */
+    $(".shelf-item-heading a").each(function () {
+        var link = $(this);
+        link.click(function (ev) {
+            var slug = link.data("title-slug");
+            if (slug) {
+                _gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfTitleClick', slug, 1]);
+            }
+        });
+    });
+
+    $(".shelf-cover").each(function () {
+        var img = $(this);
+        var link = img.parent();
+        link.click(function (ev) {
+            var slug = img.data("title-slug");
+            if (slug) {
+                _gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfTitleClick', slug, 0]);
+            }
+        });
+    });
+
+    $(".shelf-wrapper").delegate(".shelf-select-form select", "change", function (ev) {
+        _gaq.push(['_trackEvent', 'Widgets', 'HomePage-ShelfDropDown', $(this).val()]);
+    });
+
 });
