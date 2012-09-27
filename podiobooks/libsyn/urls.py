7bb52f3ebd7a95d5a6d86d9958d6ff9a97649f91
cyface <tim@cyface.com>
2012-09-27 17:24:27 -0600

    Hot libsyn title importer admin function is hot.
    Former-commit-id: cffce9e73561c27bfc0cc875e2b10120cd85d000
    Former-commit-id: f032b0f6e012e9f1e7452c9fc739aa4c51e7b232
    Former-commit-id: 09444cbc8eda271d12ef13b41606aac266c29289
    Former-commit-id: 6dca445791a8c0cd0d171b35bd73b643f7fd86bd
    Former-commit-id: 31d083494189059805a0037d44984f7c2c84fea3
    Former-commit-id: 7c3aa434aed5b5e82ebe9a6b9c1a7f1379d5ab0d

--- podiobooks/libsyn/urls.py
+++ podiobooks/libsyn/urls.py
@@ -6,9 +6,11 @@ from django.conf.urls import patterns, url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.views.generic import RedirectView
-from .views import ImportFromLibsynView
+from .views import ImportFromLibsynFormView, ImportFromLibsynResultsView
 
 urlpatterns = patterns('',
     # Import Title
-    url(r'^import/', ImportFromLibsynView.as_view(), name="libsyn_import_view"),
+    url(r'^import/$', ImportFromLibsynFormView.as_view(), name="libsyn_import_view"),
+    url(r'^import/results/$', ImportFromLibsynResultsView.as_view(), name="libsyn_import_results_view"),
+    url(r'^import/slug/(?P<libsyn_slug>[\w-]+)/$', ImportFromLibsynResultsView.as_view(), name="libsyn_import_view_direct"),
 )
