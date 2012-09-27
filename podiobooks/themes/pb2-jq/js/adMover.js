993ebb2deda1c598a7d4c77303a14e5e9575d082
Brant Steen <brant.steen@gmail.com>
2012-09-27 13:59:08 -0400

    ..?
    Former-commit-id: 2c1defa746b81f774e8dea2fd8631fe58f702c2e
    Former-commit-id: 5bf64d967ae2efb63feb30f55377a1fe50a51889
    Former-commit-id: 4b6cb2098e661babca183da614b9320d91ffa18b
    Former-commit-id: 86a492dc532db20523f4c3342b6236ba4f43bc18
    Former-commit-id: 61d21dd19c73d5c09edcbc644c282071cdae61a0
    Former-commit-id: 912d02a3f8846efb42e652e550ee246ff8cfac32

--- podiobooks/themes/pb2-jq/js/adMover.js
+++ podiobooks/themes/pb2-jq/js/adMover.js
@@ -1,7 +1,8 @@
 $(function(){
+	
 	$('[data-move-ad]').each(function(){
 		var wrapper = $(this);
-		target = "#" + wrapper.data("move-ad");
+		target = $("#" + wrapper.data("move-ad"));
 		wrapper.appendTo(target);
 	});
 });
