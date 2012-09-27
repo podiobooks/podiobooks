2ac032cb05fb78e7da7de4911cebf599b4c953af
Brant Steen <brant.steen@gmail.com>
2012-09-27 13:42:50 -0400

    bylines overhaul
    Former-commit-id: af186fbd650269a83be3322e61584bc57e26f7aa
    Former-commit-id: 77820cc9a3f6ec9297a607f9b136c156cefb9189
    Former-commit-id: ea93032498bb39dfb68306d346cbe24cb1ceff99
    Former-commit-id: ffa30e5c8bb6004ca5f7455e31fcf30865b4964c
    Former-commit-id: c78ec0c4e96bd4d803e3934b930df6d62ccac76a
    Former-commit-id: bcfff5fe9918dc5d2a9a2fb4d802318b6f4d5f96

--- podiobooks/core/templatetags/shelf_templatetags.py
+++ podiobooks/core/templatetags/shelf_templatetags.py
@@ -19,6 +19,7 @@ def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class)
 @register.inclusion_tag('core/shelf/tags/show_shelf_pages.html')
 def show_shelf_pages(shelf_id, shelf_name, title_list):
     """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""
+    
     return {'shelf_id': shelf_id,
             'shelf_name': shelf_name,
             'title_list': title_list[:18]
