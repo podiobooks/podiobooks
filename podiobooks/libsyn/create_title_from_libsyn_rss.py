bfa06369f01977481c80e95f132a56ff458b0085
cyface <tim@cyface.com>
2012-10-03 11:45:24 -0600

    Updated Libsyn Import code to use low-level XML API - allows the HTML to get through. Also added sorter to try and sort episodes by creation date. Which works great on newer titles, not so great on older ones.
    Former-commit-id: 28824c54b2743687c45b8919713b2ef60c626726
    Former-commit-id: 555745b5a91ea516090717c42e3ad656dc8a5f95
    Former-commit-id: 189ae1fdaef0cd093f05e5f248dd09af548ae7c8
    Former-commit-id: 8b3b1960d5733d4351fb4cf6941b7db820bd417d
    Former-commit-id: 26b42ebd6087b4ab31ab37ce7c0c59e4e67b8051
    Former-commit-id: 61fcd66ac3c933e5d3e7ef828e5798b2e51caccf

--- podiobooks/libsyn/create_title_from_libsyn_rss.py
+++ podiobooks/libsyn/create_title_from_libsyn_rss.py
@@ -2,46 +2,59 @@
 Create a PB2 Title Object by parsing a libsyn RSS feed.
 """
 
-import feedparser
-from podiobooks.core.models import Title, Episode
+from xml.etree import ElementTree
+import urllib
+from podiobooks.core.models import Episode, License, Title
 from django.template.defaultfilters import slugify
+from email.utils import mktime_tz, parsedate_tz
 from datetime import datetime
 
 def create_title_from_libsyn_rss(rss_feed_url):
     """Parses a libsyn-generated RSS feed"""
 
-    feed = feedparser.parse(rss_feed_url)
-    feed_info = feed.get('feed', None)
+    if rss_feed_url.startswith('http'):
+        feed = urllib.urlopen(rss_feed_url)
+        feed_tree = ElementTree.parse(feed).getroot()
+    else:
+        feed_tree = ElementTree.parse(rss_feed_url).getroot()
 
-    if feed_info is None:
+    if feed_tree is None:
         return None
+
+    feed_tree = feed_tree.find('channel')
 
     title = Title()
 
-    title.name = feed_info.title
-    title.slug = slugify(feed_info.title) + "---CHANGEME--" + datetime.now().strftime('%f')
-    title.description = feed_info.summary_detail.value
-    if feed_info.itunes_explicit:
+    title.name = feed_tree.find('title').text
+    title.slug = slugify(title.name) + "---CHANGEME--" + datetime.now().strftime('%f')
+    title.description = feed_tree.find('description').text
+    if feed_tree.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}explicit').text == 'yes':
         title.is_explicit = True
     title.deleted = True
 
     title.save()
+    items = feed_tree.findall('item')
 
-    sequence = 1
-
-    for entry in feed.entries:
+    for item in items:
         episode = Episode()
+        default_license = License.objects.get(slug='by-nc-nd')
 
         episode.title = title
-        episode.name = entry.title
+        episode.license = default_license
+        episode.name = item.find('title').text
+        episode.description = item.find('description').text
+        episode.filesize = item.find('enclosure').get('length')
+        episode.url = item.find('enclosure').get('url')
+        episode.duration = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}duration').text
+        episode.media_date_created = datetime.fromtimestamp(mktime_tz(parsedate_tz(item.find('pubDate').text)))
+        episode.sequence = 0
+        episode.save()
+
+    # Re-order episodes by date created
+    episodes = title.episodes.all().order_by('media_date_created')
+    sequence = 1
+    for episode in episodes:
         episode.sequence = sequence
-        episode.description = entry.summary_detail.value
-        episode.duration = entry.itunes_duration
-
-        file = entry.links[1]
-        episode.filesize = file.length
-        episode.url = file.href
-
         sequence += 1
         episode.save()
 
