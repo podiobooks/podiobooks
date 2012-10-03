4a5741303eab6742b8bf2031ed3df66904d131a1
cyface <tim@cyface.com>
2012-10-03 11:47:53 -0600

    Re-enabled Libsyn View tests.

--- podiobooks/libsyn/tests.py
+++ podiobooks/libsyn/tests.py
@@ -1,5 +1,5 @@
 """Automated Tests of the Podiobooks LibSyn Interface"""
 
 from podiobooks.libsyn.tests_rss import LibsynRSSTestCase
-# from podiobooks.libsyn.tests_views import LibsynImportViewsTestCase
+from podiobooks.libsyn.tests_views import LibsynImportViewsTestCase
 # from podiobooks.libsyn.tests_api import LibsynAPITestCase
