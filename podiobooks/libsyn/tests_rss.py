2c6c1d2c1bb1349b7355c66079da5b660559556c
cyface <tim@cyface.com>
2012-10-05 10:27:08 -0600

    Test updates for libsyn importer.
    Former-commit-id: 52251fa0acfe784f0dbd2100222ea826e6d6ad19
    Former-commit-id: 6da403f72d3d83ba6ceeed6a7373a12722675938
    Former-commit-id: 52f3deec15b15c9ef60f084f7a4422495c53c332
    Former-commit-id: 1b2882ad5eace41ffad481aa071589b6569b2c9c
    Former-commit-id: 377a04d6c2419555554f6f3e091a3bdc12b16adc
    Former-commit-id: 9759ebfc8d004b47a3de6154b506757bac3c45f1

--- podiobooks/libsyn/tests_rss.py
+++ podiobooks/libsyn/tests_rss.py
@@ -10,12 +10,28 @@ import os
 
 class LibsynRSSTestCase(TestCase):
     # fixtures = []
-    
+
     def setUp(self):
         License.objects.create(slug='by-nc-nd', text='by-nc-nd', url='by-nc-nd')
-    
-    def testParseRss(self):
-        title = create_title_from_libsyn_rss.create_title_from_libsyn_rss(os.path.join(settings.PROJECT_ROOT,'libsyn','libsyn_example.rss'))
-#        print title
+
+    def testParseOlderRss(self):
+        title = create_title_from_libsyn_rss.create_title_from_libsyn_rss(os.path.join(settings.PROJECT_ROOT, 'libsyn', 'libsyn_example.rss'))
         self.assertEquals(title.name, 'Infected')
-        
+        self.assertEquals(title.slug, 'infected')
+        self.assertIsNotNone(title.license)
+        self.assertRegexpMatches(title.description, 'Horror')
+        self.assertEquals(title.episodes.all().count(), 20)
+
+        title2 = create_title_from_libsyn_rss.create_title_from_libsyn_rss(os.path.join(settings.PROJECT_ROOT, 'libsyn', 'libsyn_example.rss'))
+        self.assertRegexpMatches(title2.slug, 'CHANGEME')
+
+    def testParseRss(self):
+        title = create_title_from_libsyn_rss.create_title_from_libsyn_rss(os.path.join(settings.PROJECT_ROOT, 'libsyn', 'libsyn_recent_example.rss'))
+        self.assertEquals(title.name, 'The Wonderful World of Linus Bailey')
+        self.assertEquals(title.slug, 'the-wonderful-world-of-linus-bailey')
+        self.assertIsNotNone(title.license)
+        self.assertRegexpMatches(title.description, 'Linus')
+        self.assertEquals(title.episodes.all().count(), 9)
+
+        title2 = create_title_from_libsyn_rss.create_title_from_libsyn_rss(os.path.join(settings.PROJECT_ROOT, 'libsyn', 'libsyn_recent_example.rss'))
+        self.assertRegexpMatches(title2.slug, 'CHANGEME')
