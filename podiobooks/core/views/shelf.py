4fa1762701c526eb0bdf6c834761ec2cd54006b8
cyface <tim@cyface.com>
2012-10-05 12:09:42 -0600

    Query centralization and caching for shelves.

--- podiobooks/core/views/shelf.py
+++ podiobooks/core/views/shelf.py
@@ -7,6 +7,7 @@ from django.http import Http404
 from django.db.models import Max
 from django.views.generic.base import View
 from django.core.urlresolvers import reverse
+from podiobooks.core.queries import get_featured_shelf_titles, get_recently_released_shelf_titles, get_toprated_shelf_titles
 
 from podiobooks.core.models import Title
 
@@ -34,23 +35,23 @@ class FilteredShelf(View):
         """
         Top rated titles, filtered by a contributor
         """
-        toprated_title_list = Title.objects.prefetch_related("titlecontributors", "titlecontributors__contributor", "titlecontributors__contributor_type").filter(display_on_homepage=True, deleted=False, promoter_count__gte=20).order_by('-date_created')
+        toprated_title_list = get_toprated_shelf_titles()
 
         if author:
             toprated_title_list = toprated_title_list.filter(contributors__slug=author)
-    
-        toprated_title_list = toprated_title_list.order_by('-promoter_count')[:16]
+
+            toprated_title_list = toprated_title_list[:24]
     
         return render_to_response("core/shelf/tags/show_shelf_pages.html", {"title_list": toprated_title_list}, context_instance=RequestContext(self.request))    
     
     def recent_by_category(self, category=None):
         # recently released
-        recently_released_list = Title.objects.prefetch_related("titlecontributors", "titlecontributors__contributor", "titlecontributors__contributor_type").filter(is_adult=False, deleted=False).annotate(Max("episodes__date_created"))
+        recently_released_list = get_recently_released_shelf_titles()
         
         if category:
             recently_released_list = recently_released_list.filter(categories__slug=category)
-            
-        recently_released_list = recently_released_list.order_by("-episodes__date_created__max")[:16]
+
+        recently_released_list = recently_released_list[:24]
     
         return render_to_response("core/shelf/tags/show_shelf_pages.html", {"title_list": recently_released_list}, context_instance=RequestContext(self.request))
     
@@ -58,12 +59,12 @@ class FilteredShelf(View):
         """
         Featured titles, filtered by category (genre)
         """
-        featured_title_list = Title.objects.prefetch_related("titlecontributors", "titlecontributors__contributor", "titlecontributors__contributor_type").filter(display_on_homepage=True, deleted=False).order_by('-date_created').all()
+        featured_title_list = get_featured_shelf_titles()
 
         if category:
             featured_title_list = featured_title_list.filter(categories__slug=category)
-    
-        featured_title_list = featured_title_list.order_by('-date_created', 'name')[:16]
+
+        featured_title_list = featured_title_list[:24]
     
         return render_to_response("core/shelf/tags/show_shelf_pages.html", {"title_list": featured_title_list}, context_instance=RequestContext(self.request))
     
