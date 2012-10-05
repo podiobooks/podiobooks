1a63d544b2bd886181ae3dd5a811adf0f02bbefe
Tim White <tim@cyface.com>
2012-10-05 08:52:01 -0600

    Fixture Updates
    Former-commit-id: 6bbd80f4e3db8177d7389045af24fe75524a4d1d
    Former-commit-id: 21c376fda3ae3e5c8bedd0b9548d744fb8b963c8
    Former-commit-id: 68cd97c6ad55bccb6e718c0d0477cca075e5a774
    Former-commit-id: d9ebe12ef3ce5e4198ae6e17fe85e93d4894591c
    Former-commit-id: 720fd289f8ea6a7348d8b355e95d4771d63ba7b2
    Former-commit-id: fdc890e8ae217c10b3c6fb9615b3941487fa80f0

--- devscripts/gondor/gondor_update_alldata.sh
+++ devscripts/gondor/gondor_update_alldata.sh
@@ -3,3 +3,4 @@
 rm podiobooks/core/fixtures/alldata.json.zip
 gondor run primary manage.py dumpdata core > podiobooks/core/fixtures/alldata.json
 zip podiobooks/core/fixtures/alldata.json.zip -m podiobooks/core/fixtures/alldata.json
+git add podiobooks/core/fixtures/alldata.json.zip
