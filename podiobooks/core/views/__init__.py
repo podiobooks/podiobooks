d8d1ad58ec438814bd37ed04bda1932f4a06ca6b
Brant Steen <brant.steen@gmail.com>
2012-10-05 16:10:47 -0400

    That isnt going to work...

--- podiobooks/core/views/__init__.py
+++ podiobooks/core/views/__init__.py
@@ -31,28 +31,36 @@ def index(request):
     template : core/templates/index.html
     """
     # Featured Shelf
-    
+    featured_title_list = get_featured_shelf_titles()
 
     category_choice_form = CategoryChoiceForm(request, cookie="featured_by_category")
     initial_category_slug = category_choice_form.fields["category"].initial
-    
-    featured_title_list = get_featured_shelf_titles(initial_category_slug)
+
+    if initial_category_slug:
+        featured_title_list = featured_title_list.filter(categories__slug=initial_category_slug)
+
     featured_title_list = featured_title_list[:24]
 
     # Top Rated Shelf
+    toprated_title_list = get_toprated_shelf_titles()
+
     contributor_choice_form = ContributorChoiceForm(request, cookie="top_rated_by_author")
     initial_contributor_slug = contributor_choice_form.fields["contributor"].initial
-    
-    toprated_title_list = get_toprated_shelf_titles(initial_contributor_slug)
+
+    if initial_contributor_slug:
+        toprated_title_list = toprated_title_list.filter(contributors__slug=initial_contributor_slug)
+
     toprated_title_list = toprated_title_list[:24]
 
     # Recently Released Shelf
-    
+    recently_released_list = get_recently_released_shelf_titles()
 
     category_choice_form_recent = CategoryChoiceForm(request, cookie="recent_by_category")
     initial_category_slug_recent = category_choice_form_recent.fields["category"].initial
 
-    recently_released_list = get_recently_released_shelf_titles(initial_category_slug_recent)
+    if initial_category_slug_recent:
+        recently_released_list = recently_released_list.filter(categories__slug=initial_category_slug_recent)
+
     recently_released_list = recently_released_list[:24]
 
     # Render template    
