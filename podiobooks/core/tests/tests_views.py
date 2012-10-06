a37a56ae2e54e48ab20ecad02f81e7b2ed6dece2
Tim White <tim@cyface.com>
2012-10-06 14:20:53 -0600

    Updated name of url.
    Former-commit-id: 1b2b48061543f0ce45565460efa7a24bf0f5878f
    Former-commit-id: 48df9e88ae5e02c864b1458cccae0a04d43557b9
    Former-commit-id: e7e451979521eb3f3be4246dd0c244643fa03425
    Former-commit-id: 7b0282924dd797001666c4c6338f5cfbcd03057b
    Former-commit-id: ee8397738a128c1116cdbe1946b8a67fa55cb4b4
    Former-commit-id: 56cf75b787d0458cd0544c78f4a071d25c86998d

--- podiobooks/core/tests/tests_views.py
+++ podiobooks/core/tests/tests_views.py
@@ -29,5 +29,5 @@ class ShelfTestCase(TestCase):
         for shelf_type in shelf_types:
             resp = self.client.get(reverse("shelf", kwargs={"shelf_type": shelf_type}))
             self.assertEquals(resp.status_code, 200)
-            resp = self.client.get(reverse("shelf", kwargs={"shelf_type": shelf_type, "title_filter": "all"}))
+            resp = self.client.get(reverse("shelf_filtered", kwargs={"shelf_type": shelf_type, "title_filter": "all"}))
             self.assertEquals(resp.status_code, 200)
