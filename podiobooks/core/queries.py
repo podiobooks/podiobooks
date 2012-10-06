7901e44d52b2ccddeba88958ccc675e433e35721
Tim White <tim@cyface.com>
2012-10-05 20:33:42 -0600

    Now seriously, was that so hard?
    Former-commit-id: 8bb255b3d748c93e028a0d8d69153fa91d633aa7
    Former-commit-id: 03265762b9e9925b8c360d0452920fb103d08a71
    Former-commit-id: 29dc30375d4f5eddd7c1a46def2477b9e5ac5b41
    Former-commit-id: 81914bbb24b77cb169b43dc46ed52a326d80ee31
    Former-commit-id: faee0eaf6a027602207dff01ec96687267b6682d
    Former-commit-id: 84a0ba77d708efb1a4667219232f69f04128b2d8

--- podiobooks/core/queries.py
+++ podiobooks/core/queries.py
@@ -1,12 +1,17 @@
 """Common Queries Used By Shelves and Such"""
 
 from django.core.cache import cache
-from podiobooks.core.models import Title, Contributor, Category
-from django.db.models import Q, Count, Max
+from podiobooks.core.models import Title
+from django.db.models import Max, Q
 
-def get_featured_shelf_titles():
+def get_featured_shelf_titles(category='all'):
     """Returns a randomized list of non-deleted titles with display_on_homepage == True"""
-    titles = cache.get('featured_shelf_titles')
+    titles = cache.get('featured_shelf_titles_' + category)
+
+    if category != 'all':
+        category_filter = Q(categories__slug=category)
+    else:
+        category_filter = Q()
 
     if not titles:
         titles = Title.objects.prefetch_related(
@@ -14,16 +19,23 @@ def get_featured_shelf_titles():
             "titlecontributors__contributor",
             "titlecontributors__contributor_type"
         ).filter(
-            display_on_homepage=True,
-            deleted=False
-        ).order_by('?')
-        cache.set('featured_shelf_titles', titles, 604800)
+            Q(display_on_homepage=True) &
+            Q(deleted=False) &
+            category_filter
+        ).order_by('?')[:16]
+        cache.set('featured_shelf_titles_' + category, titles, 90)
 
     return titles
 
-def get_toprated_shelf_titles():
+
+def get_toprated_shelf_titles(contributor='all'):
     """Returns a list of non-deleted titles in descending rating order"""
-    titles = cache.get('toprated_shelf_titles')
+    titles = cache.get('toprated_shelf_titles_' + contributor)
+
+    if contributor != 'all':
+        contributor_filter = Q(contributors__slug=contributor)
+    else:
+        contributor_filter = Q()
 
     if not titles:
         titles = Title.objects.prefetch_related(
@@ -31,17 +43,24 @@ def get_toprated_shelf_titles():
             "titlecontributors__contributor",
             "titlecontributors__contributor_type"
         ).filter(
-            display_on_homepage=True,
-            deleted=False,
-            promoter_count__gte=20
-        ).order_by('-promoter_count')
-        cache.set('toprated_shelf_titles', titles, 240)
+            Q(display_on_homepage=True) &
+            Q(deleted=False) &
+            Q(promoter_count__gte=20) &
+            contributor_filter
+        ).order_by('-promoter_count')[:16]
+        cache.set('toprated_shelf_titles_' + contributor, titles, 90)
 
     return titles
 
-def get_recently_released_shelf_titles():
+
+def get_recently_released_shelf_titles(category='all'):
     """Returns a list of non-deleted titles in descending rating order"""
-    titles = cache.get('recently_released_shelf_titles')
+    titles = cache.get('recently_released_shelf_titles_' + category)
+
+    if category != 'all':
+        category_filter = Q(categories__slug=category)
+    else:
+        category_filter = Q()
 
     if not titles:
         titles = Title.objects.prefetch_related(
@@ -49,9 +68,11 @@ def get_recently_released_shelf_titles():
             "titlecontributors__contributor",
             "titlecontributors__contributor_type"
         ).filter(
-            is_adult=False,
-            deleted=False
-        ).annotate(Max("episodes__date_created")).order_by('-episodes__date_created__max')
-        cache.set('recently_released_shelf_titles', titles, 240)
+            Q(is_adult=False) &
+            Q(deleted=False) &
+            category_filter
+        ).annotate(Max("episodes__date_created")
+        ).order_by('-episodes__date_created__max')[:16]
+        cache.set('recently_released_shelf_titles_' + category, titles, 90)
 
     return titles
