ed0b3c885f945ecdca5081489fccfaa3b7f182b5
Brant Steen <brant.steen@gmail.com>
2012-10-09 15:08:17 -0400

    Adding "How many titles deep" GA events

--- podiobooks/themes/pb2-jq/js/home.js
+++ podiobooks/themes/pb2-jq/js/home.js
@@ -1,26 +1,43 @@
 $(function(){
 	
+	var gaqPush = function(shelf, titlesDeep, perSlide){
+		
+		var shelfPages = shelf.find(".shelf-pages");
+		var threshold = parseInt(shelfPages.data("swipe-threshold"));
+		
+		if(!threshold){
+			threshold = perSlide;
+			shelfPages.data("swipe-threshold", threshold);
+		}
+		
+		if (titlesDeep > threshold){
+			threshold = titlesDeep;
+			shelfPages.data("swipe-threshold", threshold);
+		}
+	};
+	
 	if ($("body").hasClass("home")){
 		
 		$("#featured_shelfPageContainer").pbShelf({
-			"cookie": "featured_by_category", 
-			"checkCookie": true,
-			"clearShelfFirst": false
+			"cookie": "featured_by_category",
+			"afterMoveEnd": function(titlesDeep, perSlide){
+				gaqPush($("#featured_shelfPageContainer"), titlesDeep, perSlide);
+			}
 		});
 		
 		$("#top_titles_shelfPageContainer").pbShelf({
-			"cookie": "top_rated_by_author", 
-			"checkCookie": true,
-			"clearShelfFirst": false
+			"cookie": "top_rated_by_author",
+			"afterMoveEnd": function(titlesDeep, perSlide){
+				gaqPush($("#top_titles_shelfPageContainer"), titlesDeep, perSlide);
+			}
 		});
 		
 		$("#recent_titles_shelfPageContainer").pbShelf({
-			"cookie": "recent_by_category", 
-			"checkCookie": true,
-			"clearShelfFirst": false
+			"cookie": "recent_by_category",
+			"afterMoveEnd": function(titlesDeep, perSlide){
+				gaqPush($("#recent_titles_shelfPageContainer"), titlesDeep, perSlide);
+			}
 		});
-		
-		//$(".half-shelf>.shelf, .quarter-shelf>.shelf").pbShelf();
 		
 	}
 });
