6ed0ff7a297f4c03b31bcd3ba30044b53c0c5cb8
Brant Steen <brant.steen@gmail.com>
2012-10-10 16:04:31 -0400

    Removing little slicer that was limiting shelves to 18
    Former-commit-id: 6c27b56280b80219ae4a5e5ea039010c9bb25519
    Former-commit-id: 80a1fa4d295cc21f775fdb2b57336a9c49abf858
    Former-commit-id: 126acb93b08c3255b6d688d4aa1ff12cab54553c
    Former-commit-id: 393e94ea7f6595b7c5530182d295cdcf4040411d
    Former-commit-id: 008c436f773c446781dd552fbe224c9503ebc763
    Former-commit-id: 822ce4eaf96e35593679793bdaddf17d27052f66

--- podiobooks/core/templatetags/shelf_templatetags.py
+++ podiobooks/core/templatetags/shelf_templatetags.py
@@ -8,6 +8,7 @@ register = template.Library()
 @register.inclusion_tag('core/shelf/tags/show_shelf.html')
 def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class):
     """Pulls in a template to show a title shelf for a particular set of titles"""
+        
     return {'shelf_id': shelf_id,
             'shelf_title': shelf_title,
             'title_list': title_list,
@@ -22,13 +23,14 @@ def show_shelf_pages(shelf_id, shelf_name, title_list):
     
     return {'shelf_id': shelf_id,
             'shelf_name': shelf_name,
-            'title_list': title_list[:18]
+            'title_list': title_list
     }
 
 
 @register.inclusion_tag('core/shelf/tags/show_shelf_item.html')
 def show_shelf_item(shelf_id, title):
     """ Displays a detail snippet for a single title """
+    
     return { 'shelf_id': shelf_id, 'title' : title }
 
 
