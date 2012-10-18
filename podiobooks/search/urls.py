4cad6ae82b35c3f34498c02a51a8b3974670ae9e
cyface <tim@cyface.com>
2012-10-18 12:46:30 -0600

    Added Google Custom Search Styling. Moved whoosh search into branch.

--- podiobooks/search/urls.py
+++ podiobooks/search/urls.py
@@ -1,12 +1,9 @@
 """Django URLs for Site Search"""
 
 from django.conf.urls import patterns, url, include
-from .views import GoogleSearchView, TitleSearchView
+from .views import GoogleSearchView
 
 urlpatterns = patterns('',
-    # Search
-    url(r'^$', TitleSearchView(), name='title_haystack_search'),
-
     # Google Custom Search
-    url(r'^google/$', GoogleSearchView.as_view(), name='google_site_search'),
+    url(r'^$', GoogleSearchView.as_view(), name='google_site_search'),
 )
