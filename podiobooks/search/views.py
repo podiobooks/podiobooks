c7253cee8f9901414be1b8cd53a9ede4315f5d50
cyface <tim@cyface.com>
2012-10-18 12:46:30 -0600

    Added Google Custom Search Styling. Moved whoosh search into branch.
    Former-commit-id: 6748774787dce486e6bdb875c9ac5dd29bc8cc04
    Former-commit-id: 96058e2b3c4a57c6d8276a340fe44ff67b0600e4
    Former-commit-id: 547b18572671d7b5d39cfaaec71bfd496ea0242e
    Former-commit-id: 5e859d7cabb05b5c12f3eea06dc5786f3ff78021
    Former-commit-id: 0296122b16ab8d23247e7297ae879549e3381217
    Former-commit-id: 39d6970191401f9584533c575b032c7ac7a36004

--- podiobooks/search/views.py
+++ podiobooks/search/views.py
@@ -1,20 +1,6 @@
 """ Django Views for Search"""
 
-from haystack.views import SearchView
-from django.template import RequestContext
-from .forms import TitleSearchForm
 from django.views.generic import TemplateView
-
-class TitleSearchView(SearchView):
-    """Haystack View for Searching Titles"""
-
-    def __init__(self, *args, **kwargs):
-        self.load_all = True
-        self.form_class = TitleSearchForm
-        self.context_class = RequestContext
-        self.searchqueryset = None
-        self.results_per_page = 30
-        self.template = 'search/search.html'
 
 class GoogleSearchView(TemplateView):
     """Google Custom Search"""
