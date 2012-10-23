d64d8fa94bef0296d6234dff24baed140f631f1f
cyface <tim@cyface.com>
2012-10-23 10:50:24 -0600

    Added google custom snip version so I can add params locally.

--- podiobooks/search/views.py
+++ podiobooks/search/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 class GoogleSearchView(TemplateView):
     """Google Custom Search"""
     template_name = 'search/google_custom.html'
+
+class GoogleSearchSnipView(TemplateView):
+    """Google Custom Search With Snippets"""
+    template_name = 'search/google_custom_snip.html'
