8259d8aff03b5ed2414915bed60b4e11509404ac
Tim White <tim@cyface.com>
2012-11-07 10:38:40 -0700

    Shut off URL calculation/caching if DEBUG is true.
    Former-commit-id: 39a867e2fe5097b99275136ec00147a9eb839f94
    Former-commit-id: 374c14ceb172e245406a38d51258e137047714c0
    Former-commit-id: 3e91bbbafae6517aabb3eb4fb7d38a7010a205ea
    Former-commit-id: 0ce0829007ba370719432be7cee31ba62c26ba12
    Former-commit-id: 6343eace8e5cc5b077a28128f9485082e2d5dd0d
    Former-commit-id: 211cb6ecdb34ed19b1968944ec5bb5e0cacf14ee

--- podiobooks/core/templatetags/title_templatetags.py
+++ podiobooks/core/templatetags/title_templatetags.py
@@ -23,14 +23,17 @@ def show_contributors(title):
 
 def get_libsyn_cover_url(title, height, width):
     scale_url = "http://asset-server.libsyn.com/show/{0}/height/{1}/width/{2}".format(title.libsyn_show_id, height, width)
-    redirected_url = cache.get(scale_url)
-    if not redirected_url:
-        try:
-            redirected_url = urllib2.urlopen(scale_url, None, 20).url
-            cache.set(scale_url, redirected_url, 1000)
-        except IOError:
-            redirected_url = scale_url
-    return redirected_url
+    if not settings.DEBUG:
+        redirected_url = cache.get(scale_url)
+        if not redirected_url:
+            try:
+                redirected_url = urllib2.urlopen(scale_url, None, 20).url
+                cache.set(scale_url, redirected_url, 1000)
+            except IOError:
+                redirected_url = scale_url
+        return redirected_url
+    else:
+        return scale_url
 
 
 @register.inclusion_tag('core/title/tags/show_titlecover.html')
