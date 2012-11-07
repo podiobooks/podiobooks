fc4d46d60cbd7eb5d7ed4f16fa61300668bc8db3
Brant Steen <brant.steen@gmail.com>
2012-11-07 14:55:15 -0500

    fiddling with JS call for site search

--- podiobooks/themes/pb2-jq/js/googleSiteSearch.js
+++ podiobooks/themes/pb2-jq/js/googleSiteSearch.js
@@ -1,25 +1,24 @@
-/* 
- * Use V2 of searach API 
+/*
+ * Use V2 of searach API
  * https://developers.google.com/custom-search/docs/element#cse-element
  * https://developers.google.com/custom-search/docs/element#supported_attributes
  */
-$(function(){
+
+    
+google.setOnLoadCallback(function () {
     if ($("body").hasClass("search-results")){
-        google.setOnLoadCallback(function () {     
-            google.search.Csedr.addOverride("pb_");
-            google.search.cse.element.render({
-                div: "cse",
-                tag: 'search',
-                attributes: {
-                    resultsUrl: '/search/snip/',
-                    webSearchResultSetSize: 20,
-                    webSearchSafesearch: "active",
-                    enableHistory: true,
-                    newWindow: false,
-                    linkTarget: null
-                }
-                
-            });
+        google.search.Csedr.addOverride("pb_");
+        google.search.cse.element.render({
+            div: "cse",
+            tag: 'search',
+            attributes: {
+                resultsUrl: '/search/snip/',
+                webSearchResultSetSize: 20,
+                webSearchSafesearch: "active",
+                enableHistory: true,
+                newWindow: false,
+                linkTarget: null
+            }
         });
     }
 });
