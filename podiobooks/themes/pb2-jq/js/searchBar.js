5a261d9098535fd860ae15f43da42e3ccf29705e
Brant Steen <brant.steen@gmail.com>
2012-11-07 13:49:10 -0500

    taking a run at search bar in nav bar
    Former-commit-id: 33b56e511feaf4f7e98c61db5d81d1f430a22c55
    Former-commit-id: 919324523dbd4bb4053c238ef75006a457b58f89
    Former-commit-id: 11fce975a6e25fa1297dd8c953b4bd0cf1f38d59
    Former-commit-id: 438df13b7945c323af3fe1557a75ed343e4f4f0a
    Former-commit-id: 9db78994582184debd0279276163d54c104aa6d0
    Former-commit-id: 760acb49adf92d82bb7087b83855af4e1819fe9d

--- podiobooks/themes/pb2-jq/js/searchBar.js
+++ podiobooks/themes/pb2-jq/js/searchBar.js
@@ -1,26 +1,7 @@
 $(function(){
-	
-	var navBar = $(".nav-bar");
-	var keywords = navBar.find(".search-keywords");
-	
-	keywords.bind("focus", function(ev){
-		navBar.addClass("nav-bar-searching");
-	}).bind("blur", function(ev){
-		navBar.removeClass("nav-bar-searching");
-	});
-	
-	navBar.find(".search-submit").click(function(ev){
-		if (keywords.val() == "" || (!(navBar.hasClass("nav-bar-searching")) && $(window).width() < 501)){
-			ev.preventDefault();
-			keywords.trigger("focus");
-		}
-	});
-	
-	$(".search-additional-fields input").change(function(){
-		var form = $(this).parents("form");
-		var container = $(this).parents(".search-additional-fields");		
-		form.trigger("submit");		
-		container.addClass("search-additional-fields-thinking");		
-	});
-	
+	var searchTrigger = $("<a class='search-bar-trigger' data-icon='L' href='#'>Search</a>").appendTo(".nav-bar");
+	searchTrigger.click(function(ev){
+		ev.preventDefault();
+		$(".search").show().find('input[type="text"]').focus();
+	});	
 });
