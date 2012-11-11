b54e45cb75cc331306d237526cfbd80545cb9026
Tim White <tim@cyface.com>
2012-11-11 08:01:49 -0700

    Google Search view/template cleanup. Added Genre and number of ratings to search results for titles, fixed bug with non-title thumbnails.
    Former-commit-id: 96b6ea406d0655688033a5fb8ec6c36c4b636174
    Former-commit-id: 844c6d841660762ae699a139bff7597f5ad4e630
    Former-commit-id: e472121d6e16d8de34ac9e55a11f5ac75a78a3c8
    Former-commit-id: 864e3d7e6a89caffc5677c6696a2b5cfa653c24e
    Former-commit-id: cf46939f333770bc6bfdd5934a598c167c73497a
    Former-commit-id: 0f3cdecac26f5236ead7f5de018ae6842c1504da

--- podiobooks/search/views.py
+++ podiobooks/search/views.py
@@ -4,8 +4,4 @@ from django.views.generic import TemplateView
 
 class GoogleSearchView(TemplateView):
     """Google Custom Search"""
-    template_name = 'search/google_custom.html'
-
-class GoogleSearchSnipView(TemplateView):
-    """Google Custom Search With Snippets"""
-    template_name = 'search/google_custom_snip.html'
+    template_name = 'search/google_custom_search.html'
