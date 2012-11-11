d2507047e57bc26a9aadb248b9477a3fad723397
Tim White <tim@cyface.com>
2012-11-11 08:01:49 -0700

    Google Search view/template cleanup. Added Genre and number of ratings to search results for titles, fixed bug with non-title thumbnails.

--- podiobooks/search/urls.py
+++ podiobooks/search/urls.py
@@ -1,10 +1,9 @@
 """Django URLs for Site Search"""
 
-from django.conf.urls import patterns, url, include
-from .views import GoogleSearchView, GoogleSearchSnipView
+from django.conf.urls import patterns, url
+from .views import GoogleSearchView
 
 urlpatterns = patterns('',
     # Google Custom Search
     url(r'^$', GoogleSearchView.as_view(), name='google_site_search'),
-    url(r'^snip/$', GoogleSearchSnipView.as_view(), name='google_site_search_snip'),
 )
