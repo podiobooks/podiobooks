a9b38296f516a9ae56ca83c240c80baa72ccd410
Tim White <tim@cyface.com>
2012-11-13 13:33:01 -0700

    Ratings views with IP/cache limiting.
    Former-commit-id: 632ef02b278a9059611d9f3156ff99991807a7bf
    Former-commit-id: 783218a9e8ddfafa5f7bfb460cdae578e70b7f20
    Former-commit-id: bfe0ed4fdd73ec0565255102e7577a00259d07f6
    Former-commit-id: f3229591c30e13eff81671eebe896eb9e28ff19a
    Former-commit-id: a9bc7d54f0d770c5ce054cf1421972c7b050c3de
    Former-commit-id: 4243704d0075a5da90b830d54b1deda1b701b85b

--- podiobooks/ratings/views.py
+++ podiobooks/ratings/views.py
@@ -1,6 +1,7 @@
 """ Django Views for Ratings"""
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.views.generic import View
 from podiobooks.core.views import Title
@@ -14,7 +15,17 @@ class RateTitleView(View):
     def get(self, request, pk=None, up=True):
         """Add an upvote/downvote for a specific title"""
 
+        # Add Request.is_ajax() check
+
         # Add IP check based on Request, Cookie Check
+        ip_title_list = cache.get(request.META['REMOTE_ADDR'])
+        if not ip_title_list:
+            cache.set(request.META['REMOTE_ADDR'], [pk,], 10000000)
+        elif pk in ip_title_list:
+            return HttpResponse("['error':'You have already voted for this title.']", mimetype="application/json")
+        else:
+            ip_title_list.add(pk)
+            cache.set(request.META['REMOTE_ADDR'], ip_title_list, 10000000)
 
         try:
             title = Title.objects.get(pk=pk)
@@ -24,12 +35,12 @@ class RateTitleView(View):
             else:
                 title.detractor_count = F('detractor_count') + 1
 
-            #title.save() # Commented out until security can be put in place.
+            title.save() # Commented out until security can be put in place.
 
             title = Title.objects.get(pk=pk)
 
-            return HttpResponse("[{0}, {1}]".format(title.promoter_count, title.detractor_count), mimetype='text/plain')
+            return HttpResponse("[{0}, {1}]".format(title.promoter_count, title.detractor_count), mimetype='application/json')
 
         except ObjectDoesNotExist or MultipleObjectsReturned:
-            return HttpResponse("[]", mimetype='text/plain')
+            return HttpResponse("['error':'Title Not Found.']", mimetype='application/json')
 
