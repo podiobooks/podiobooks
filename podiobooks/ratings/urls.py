1857a87f354e33f6683d1ec345d63742a642e80d
Tim White <tim@cyface.com>
2012-11-13 13:33:01 -0700

    Ratings views with IP/cache limiting.

--- podiobooks/ratings/urls.py
+++ podiobooks/ratings/urls.py
@@ -2,11 +2,12 @@
 
 from django.conf.urls import patterns, url
 from .views import RateTitleView
+from django.views.decorators.cache import never_cache
 
 urlpatterns = patterns('',
     # Promote Title
-    url(r'^promote/(?P<pk>[^/]+)/$', RateTitleView.as_view(), name='promote_title'),
+    url(r'^promote/(?P<pk>[^/]+)/$', never_cache(RateTitleView.as_view()), name='promote_title'),
 
     # Detract Title
-    url(r'^detract/(?P<pk>[^/]+)/$', RateTitleView.as_view(), kwargs={"up":False}, name='detract_title'),
+    url(r'^detract/(?P<pk>[^/]+)/$', never_cache(RateTitleView.as_view()), kwargs={"up":False}, name='detract_title'),
 )
