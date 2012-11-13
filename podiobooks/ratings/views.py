79d43c4d65eba4bb391d1479a7070732442ee6bb
Tim White <tim@cyface.com>
2012-11-13 14:39:45 -0700

    Ratings views with cookie limiting.
    Former-commit-id: 331a20b6c158d1b3750549e90d6c54bac056bb10
    Former-commit-id: 6f5dce01ab7205f0623e09270d69cf9d9d958d93
    Former-commit-id: 739f6d6e29e2401c9420bf9d9d930f6e2d618683
    Former-commit-id: 03b993aa15695cdfed7237736555b3783aeb9cb1
    Former-commit-id: 4365baac64a0ea45798eda3eac168353bb61363b
    Former-commit-id: f94f88a89cc0fe62418bdacbfa92131dac0597d4

--- podiobooks/ratings/views.py
+++ podiobooks/ratings/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from podiobooks.core.views import Title
 from django.db.models import F
+import pickle
 
 class RateTitleView(View):
     """Rate Title"""
@@ -17,7 +18,7 @@ class RateTitleView(View):
 
         # Add Request.is_ajax() check
 
-        # Add IP check based on Request, Cookie Check
+        # Use cache to hold IP
         ip_title_list = cache.get(request.META['REMOTE_ADDR'])
         if not ip_title_list:
             cache.set(request.META['REMOTE_ADDR'], [pk,], 10000000)
@@ -26,6 +27,20 @@ class RateTitleView(View):
         else:
             ip_title_list.add(pk)
             cache.set(request.META['REMOTE_ADDR'], ip_title_list, 10000000)
+
+        # Keep a comma-separated, signed cookie of all the titles ever voted for
+        # It's possible that someone could hit max cookie length limit here, what then?
+        # Also, since the cookies are sent back and forth on each request, this means the more you vote, the slower the site
+        vote_cookie = request.get_signed_cookie('v', False)
+        if vote_cookie:
+            vote_cookie_list = vote_cookie.split(',')
+            if pk in vote_cookie_list:
+                return HttpResponse("['error':'You have already voted for this title.']", mimetype="application/json")
+            else:
+                vote_cookie_list.append(pk)
+                vote_cookie = ','.join(vote_cookie_list)
+        else:
+            vote_cookie = str(pk)
 
         try:
             title = Title.objects.get(pk=pk)
@@ -39,7 +54,10 @@ class RateTitleView(View):
 
             title = Title.objects.get(pk=pk)
 
-            return HttpResponse("[{0}, {1}]".format(title.promoter_count, title.detractor_count), mimetype='application/json')
+            response = HttpResponse("[{0}, {1}]".format(title.promoter_count, title.detractor_count), mimetype='application/json')
+            response.set_signed_cookie('v', vote_cookie)
+
+            return response
 
         except ObjectDoesNotExist or MultipleObjectsReturned:
             return HttpResponse("['error':'Title Not Found.']", mimetype='application/json')
