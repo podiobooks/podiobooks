a842a8c3703441465934fa5c284e290df683b879
Tim White <tim@cyface.com>
2012-11-26 09:25:14 -0700

    Removed pre-fetch of final cover url.

--- podiobooks/core/templatetags/title_templatetags.py
+++ podiobooks/core/templatetags/title_templatetags.py
@@ -25,17 +25,9 @@ def show_contributors(title):
 def get_libsyn_cover_url(title, height, width):
     """Pulls the final libsyn URL for a title from libsyn"""
     scale_url = "http://asset-server.libsyn.com/show/{0}/height/{1}/width/{2}".format(title.libsyn_show_id, height, width)
-    if not settings.DEBUG:
-        redirected_url = cache.get(scale_url)
-        if not redirected_url:
-            try:
-                redirected_url = urllib2.urlopen(scale_url, None, 20).url
-                cache.set(scale_url, redirected_url, 1000)
-            except IOError:
-                redirected_url = scale_url
-        return redirected_url
-    else:
-        return scale_url
+    # Removed Lookup logic with caching and such - was not improving overall site performance.
+    return scale_url
+
 
 
 @register.inclusion_tag('core/title/tags/show_titlecover.html')
