433fe772e82a420353ef9b2f6359c9d92bf3fb01
Brant Steen <brant.steen@gmail.com>
2012-11-29 13:35:33 -0500

    sqashing an annoying bug where hidden search bar keeps reloading an image
    Former-commit-id: c039312a23e59130732c66523c62ddda809efa6d
    Former-commit-id: 69a71e63dcb0ff7a5cab1ec8c3297f9c1af2757e
    Former-commit-id: d1a075885cff7766db944d4d02b7c6bd11cbdeae
    Former-commit-id: c963540562909221f529e21afc7d1355d03b358d
    Former-commit-id: ed785003f04396ef8f489993b91620526db66c93
    Former-commit-id: 505b08b7ba5036852cdf8e35cb96991e769b54ea

--- podiobooks/themes/pb2-jq/js/searchBar.js
+++ podiobooks/themes/pb2-jq/js/searchBar.js
@@ -2,6 +2,6 @@ $(function(){
 	var searchTrigger = $("<a class='search-bar-trigger' data-icon='L' href='#'>Search</a>").appendTo(".nav-bar");
 	searchTrigger.click(function(ev){
 		ev.preventDefault();
-		$(".search").show().find('input[type="text"]').focus();
+		$(".search").addClass("search-active").find('input[type="text"]').focus();
 	});	
 });
