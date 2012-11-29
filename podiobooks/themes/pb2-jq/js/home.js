97eb7d3e7e00acdec761087bcf6f1605c45cd4fd
Brant Steen <brant.steen@gmail.com>
2012-11-29 13:35:11 -0500

    re-adding analytic triggers for shelf movement
    Former-commit-id: 4eef64de79db5df7d1bf9536b3387475728d9e8f
    Former-commit-id: d61ae6aaf7e8cd448121b442120d95bcb4f58c56
    Former-commit-id: bb25599b785e4da9e715b4caf7b93764f48504ca
    Former-commit-id: 1acb66376070fd1dbdfa1ebe35f41b0b0f839838
    Former-commit-id: 8e4abc9b3f187b4f9bd1ad6beb098aadedc73326
    Former-commit-id: ac67e08a52215d92324b977fe11932396d8acd44

--- podiobooks/themes/pb2-jq/js/home.js
+++ podiobooks/themes/pb2-jq/js/home.js
@@ -22,21 +22,21 @@ $(function(){
 		$("#featured_shelfPageContainer").pbShelf({
 			"cookie": "featured_by_category",
 			"afterMoveEnd": function(titlesDeep, perSlide){
-				//gaqPush($("#featured_shelfPageContainer"), titlesDeep, perSlide);
+				gaqPush($("#featured_shelfPageContainer"), titlesDeep, perSlide);
 			}
 		});
 		
 		$("#top_titles_shelfPageContainer").pbShelf({
 			"cookie": "top_rated_by_author",
 			"afterMoveEnd": function(titlesDeep, perSlide){
-				//gaqPush($("#top_titles_shelfPageContainer"), titlesDeep, perSlide);
+				gaqPush($("#top_titles_shelfPageContainer"), titlesDeep, perSlide);
 			}
 		});
 		
 		$("#recent_titles_shelfPageContainer").pbShelf({
 			"cookie": "recent_by_category",
 			"afterMoveEnd": function(titlesDeep, perSlide){
-				//gaqPush($("#recent_titles_shelfPageContainer"), titlesDeep, perSlide);
+				gaqPush($("#recent_titles_shelfPageContainer"), titlesDeep, perSlide);
 			}
 		});
 		
