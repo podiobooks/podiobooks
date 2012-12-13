ac844cd6f2624edc203da6d2e88f8e17610fc779
Tim White <tim@cyface.com>
2012-12-13 13:03:30 -0700

    Adding redirect interstitial for entry to the search url from offsite. Hoping to reduce Google requests.
    Former-commit-id: cfd24e3769a0536ca3ae460c6958ba3547118cb2
    Former-commit-id: f13bb51c642d850ce152fefa0af9c77bf5310266
    Former-commit-id: 9e80288bff9805b2bcfdacb429dfda1a44cfe57e
    Former-commit-id: 217a1cb3ad5c2ca67ee2057b16a8637bf1807930
    Former-commit-id: aaf3c67d2e39dd31632573150a375a6b6186357b
    Former-commit-id: f2a8d77896a18300403c78cc151d2a9f327dcbf9

--- podiobooks/search/views.py
+++ podiobooks/search/views.py
@@ -1,7 +1,32 @@
 """ Django Views for Search"""
 
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+class GoogleSearchInterstitialView(TemplateView):
+    """Google Custom Search"""
+    template_name = 'search/google_search_interstitial.html'
 
 class GoogleSearchView(TemplateView):
     """Google Custom Search"""
     template_name = 'search/google_custom_search.html'
+
+    def get(self, request, *args, **kwargs):
+        referrer = request.META.get('HTTP_REFERER')
+        if referrer:
+            referrer_ok = False
+            if 'podiobooks' in referrer:
+                referrer_ok = True
+            if 'localhost' in referrer:
+                referrer_ok = True
+            if'127.0.0.1' in referrer:
+                referrer_ok = True
+
+            if not referrer_ok:
+                return HttpResponseRedirect(reverse('site_search_interstitial'))
+        else:
+            return HttpResponseRedirect(reverse('site_search_interstitial'))
+
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
