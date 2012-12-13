09c0bc376d984de9c3e1f52ddb9843dd1d7f7283
Tim White <tim@cyface.com>
2012-12-13 13:03:30 -0700

    Adding redirect interstitial for entry to the search url from offsite. Hoping to reduce Google requests.
    Former-commit-id: cb55470f7db72421924a1da6742654714d73505a
    Former-commit-id: be28293410f2cb1ff6196d6c0afc4446ce96a074
    Former-commit-id: bc46110b0c0e7bee504f226cf903ccff854e5595
    Former-commit-id: d584e6da192d5fbc6ebfa8001f5fbfde40fdbbe9
    Former-commit-id: 78ec3e61a642e52ea864e254e2ce23b64b6a69c2
    Former-commit-id: 561cbba3340a7906c4a89f5024fad6448db38951

--- podiobooks/search/urls.py
+++ podiobooks/search/urls.py
@@ -1,9 +1,11 @@
 """Django URLs for Site Search"""
 
 from django.conf.urls import patterns, url
-from .views import GoogleSearchView
+from .views import GoogleSearchInterstitialView, GoogleSearchView
 
 urlpatterns = patterns('',
     # Google Custom Search
     url(r'^$', GoogleSearchView.as_view(), name='site_search'),
+    # Google Custom Search Interstitial
+    url(r'^interstitial/$', GoogleSearchInterstitialView.as_view(), name='site_search_interstitial'),
 )
