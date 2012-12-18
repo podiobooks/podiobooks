39d6bd22a36f8615e06b197b105ae5fb2a459d87
Brant Steen <brant.steen@gmail.com>
2012-12-18 13:23:52 -0500

    back to 3
    Former-commit-id: 276cea49a6d5dec33cf4dc3075b5bb5d07d2c73c
    Former-commit-id: b75f5f2697dff4fed02ff9394845fc35bfc41d8f
    Former-commit-id: a7cb3439fe79fa808beb82951039e2f432d9b5b5
    Former-commit-id: 64a13432c41a9d13883b154dbf2d13f71fb03ffd
    Former-commit-id: 54a3e41409bf0a5a4bd8bfbf7e6195c376662c8f
    Former-commit-id: 617d70686eb5d270b3f63d6e713d12db185c5d2a

--- podiobooks/themes/pb2-jq/js/jquery.pbShelf.js
+++ podiobooks/themes/pb2-jq/js/jquery.pbShelf.js
@@ -40,7 +40,7 @@
 			"positionShelfSteps"	:		true,
 			"ajaxLoaderImage"		:		"img/ajax-loading.gif",
 			"easeFunction"			:		"easeOutCirc",
-			"rubberBandStrength"	:		2
+			"rubberBandStrength"	:		3
 		};
 		
 		return this.each(function(){
