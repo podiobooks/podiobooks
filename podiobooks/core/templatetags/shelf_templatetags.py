0ad599c6ada3ded2807363e32d74d019acd4e805
Brant Steen <brant.steen@gmail.com>
2013-01-02 16:17:57 -0500

    Trying out a few things...
    ads in shelves, adjusting a few things on the details page before total
    overhaul

--- podiobooks/core/templatetags/shelf_templatetags.py
+++ podiobooks/core/templatetags/shelf_templatetags.py
@@ -7,31 +7,33 @@ register = template.Library()
 
 @register.inclusion_tag('core/shelf/tags/show_shelf.html')
 def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class):
-    """Pulls in a template to show a title shelf for a particular set of titles"""
-        
-    return {'shelf_id': shelf_id,
-            'shelf_title': shelf_title,
-            'title_list': title_list,
-            'dropdown_form': dropdown_form,
-            'base_css_class': base_css_class
+    """Pulls in a template to show a title shelf for a particular set of titles"""    
+    return {
+        'shelf_id': shelf_id,
+        'shelf_title': shelf_title,
+        'title_list': title_list,
+        'dropdown_form': dropdown_form,
+        'base_css_class': base_css_class,
     }
 
 
 @register.inclusion_tag('core/shelf/tags/show_shelf_pages.html')
 def show_shelf_pages(shelf_id, shelf_name, title_list):
-    """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""
-    
-    return {'shelf_id': shelf_id,
-            'shelf_name': shelf_name,
-            'title_list': title_list
+    """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""    
+    return {
+        'shelf_id': shelf_id,
+        'shelf_name': shelf_name,
+        'title_list': title_list,
     }
 
 
 @register.inclusion_tag('core/shelf/tags/show_shelf_item.html')
 def show_shelf_item(shelf_id, title):
-    """ Displays a detail snippet for a single title """
-    
+    """ Displays a detail snippet for a single title """    
     return { 'shelf_id': shelf_id, 'title' : title }
 
 
-
+@register.filter
+def possible_ad_placements(title_list):
+    """ Based on a list of titles, decide possible places to put an ad """
+    return range(1, len(title_list) / 2)
