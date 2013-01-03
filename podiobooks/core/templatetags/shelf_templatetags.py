168062f44ccc3dc121cf76506c950a504645175a
Brant Steen <brant.steen@gmail.com>
2013-01-03 16:38:10 -0500

    separating ad code into templates. Separate ads per shelf
    Former-commit-id: f72866a5a1f0e3e48a3513973ec5722617e7e50f
    Former-commit-id: 557b70640c49e9d8f5c945ebe6c8b007d2c8d91c
    Former-commit-id: 9c40cda06f1f54589299e34785225e3017e9551a
    Former-commit-id: 3ee3af0edad26eaae5f2d96b26d1739218aad413
    Former-commit-id: d21d59e88a6a14974f236099a3e4d10742c45304
    Former-commit-id: b3aa3197b9cece97c7ee294956a81eff73b38832

--- podiobooks/core/templatetags/shelf_templatetags.py
+++ podiobooks/core/templatetags/shelf_templatetags.py
@@ -6,24 +6,26 @@ register = template.Library()
 
 
 @register.inclusion_tag('core/shelf/tags/show_shelf.html')
-def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class):
-    """Pulls in a template to show a title shelf for a particular set of titles"""    
+def show_shelf(shelf_id, shelf_title, title_list, dropdown_form, base_css_class, ad_template=None):
+    """Pulls in a template to show a title shelf for a particular set of titles"""
     return {
         'shelf_id': shelf_id,
         'shelf_title': shelf_title,
         'title_list': title_list,
         'dropdown_form': dropdown_form,
         'base_css_class': base_css_class,
+        'ad_template': ad_template,
     }
 
 
 @register.inclusion_tag('core/shelf/tags/show_shelf_pages.html')
-def show_shelf_pages(shelf_id, shelf_name, title_list):
-    """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""    
+def show_shelf_pages(shelf_id, shelf_name, title_list, ad_template=None):
+    """Shows the guts of the shelf, the pages of items...used mainly to reload the guts of the shelf on the fly"""
     return {
         'shelf_id': shelf_id,
         'shelf_name': shelf_name,
         'title_list': title_list,
+        'ad_template': ad_template,
     }
 
 
