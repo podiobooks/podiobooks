6440475703f9007bfe507ba3baef11bc22ddf546
Tim White <tim@cyface.com>
2013-02-20 11:09:09 -0700

    Removed search interstitial.
    Former-commit-id: 32e157feeea808705cbb0f1b82e7e07e9319e279
    Former-commit-id: e52f27a2cee7d21d1ff49c3e681be6dca59efd96
    Former-commit-id: fff4dff6c2239a954329d34087b212c634360fc6
    Former-commit-id: 7857fe930e37482d3a525b69993c1f1691804702
    Former-commit-id: e34acb23b4ff5800c7fc0dd86a73729e6d5d409a
    Former-commit-id: 85d37c1b630144f4c9e9163d6e4c573954e851f1

--- podiobooks/search/views.py
+++ podiobooks/search/views.py
@@ -1,12 +1,6 @@
 """ Django Views for Search"""
 
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-
-class GoogleSearchInterstitialView(TemplateView):
-    """Google Custom Search"""
-    template_name = 'search/google_search_interstitial.html'
 
 class GoogleSearchView(TemplateView):
     """Google Custom Search"""
