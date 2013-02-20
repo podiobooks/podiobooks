21795fc274cc6a23d8e6df1b4a4749729dc262c6
Tim White <tim@cyface.com>
2013-02-20 11:09:09 -0700

    Removed search interstitial.

--- podiobooks/search/urls.py
+++ podiobooks/search/urls.py
@@ -1,11 +1,9 @@
 """Django URLs for Site Search"""
 
 from django.conf.urls import patterns, url
-from .views import GoogleSearchInterstitialView, GoogleSearchView
+from .views import GoogleSearchView
 
 urlpatterns = patterns('',
     # Google Custom Search
     url(r'^$', GoogleSearchView.as_view(), name='site_search'),
-    # Google Custom Search Interstitial
-    url(r'^interstitial/$', GoogleSearchInterstitialView.as_view(), name='site_search_interstitial'),
 )
