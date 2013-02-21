76809635115e30504a4fd7cb734db17c80acd0cf
Tim White <tim@cyface.com>
2013-02-21 09:52:22 -0700

    Adding gondor primary host names to allowed hosts for Django 1.4.4, and adding redirect middleware to send them back to primary domain.

---
+++ podiobooks/core/middleware.py
@@ -0,0 +1,18 @@
+"""Custom Django Middleware"""
+
+from django.conf import settings
+from django.http import HttpRequest, HttpResponsePermanentRedirect
+from django.contrib.sites.models import Site
+
+
+class PermanentRedirectMiddleware(object):
+    """Checks hostname to see if it should be redirected based on REDIRECT_DOMAINS list in settings"""
+    def process_request(self, request):
+        host = HttpRequest.get_host(request)
+        redirect_domains = settings.REDIRECT_DOMAINS
+        if host in redirect_domains:
+            return HttpResponsePermanentRedirect("http%s://%s%s" % (
+                request.is_secure() and 's' or '',
+                Site.objects.get_current().domain,
+                request.get_full_path(),
+            ))
