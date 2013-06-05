cad70fb2dd02f502fea8f928531511967ea27ffc
Tim White <tim@cyface.com>
2013-06-05 14:34:25 -0600

    Pylint cleanup.
    Former-commit-id: 4d3fc3c1ea1784c7b0473aaf222f1f8f4e809c66
    Former-commit-id: c603af05c8a295721e3b42da3a841949779561fb
    Former-commit-id: 908cb424bf8119066c33462907d0f0f4f11d20b9
    Former-commit-id: 0d33b378b1646629eba5eda98fcea873ae24bcda
    Former-commit-id: 8e50a80919f3095f8e965547acdbd404f3b20f06
    Former-commit-id: b5710762fec1937280809e312342a706d4d43784

--- podiobooks/views.py
+++ podiobooks/views.py
@@ -3,12 +3,14 @@
 from django.views.generic import RedirectView, TemplateView
 from django.contrib.sites.models import Site
 
+
 class TextTemplateView(TemplateView):
     """Utility View to Render text/plain MIME Type"""
     def render_to_response(self, context, **response_kwargs):
         """Returns a Template as text/plain"""
         response_kwargs['mimetype'] = 'text/plain'
         return super(TemplateView, self).render_to_response(context, **response_kwargs)
+
 
 class RobotsView(TextTemplateView):
     """
@@ -21,6 +23,7 @@ class RobotsView(TextTemplateView):
             return ['robots_prod.txt']
         else:
             return ['robots.txt']
+
 
 class BlogRedirectView(RedirectView):
     permanent = True
