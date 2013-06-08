ab3660104e4be9a9e4f70c41ec1064bcdaa1c711
Brant Steen <brant.steen@gmail.com>
2013-06-08 11:47:12 -0400

    Clearing shelf cookies, making them non-functional
    fixes #266

--- podiobooks/themes/pb2-jq/static/js/home.js
+++ podiobooks/themes/pb2-jq/static/js/home.js
@@ -1,5 +1,9 @@
 $(function(){
 	
+	var clearShelfCookies = function(cookie){
+		$.cookie(cookie, "None", {expires: -1});
+	};
+		
 	var gaqPush = function(shelf, titlesDeep, perSlide){
 		
 		var shelfPages = shelf.find(".shelf-pages");
@@ -17,20 +21,26 @@ $(function(){
 		}
 	};
 	
+	
+	// 6/8/2013 - added this to clear out old cookies
+	// Can be removed later (anytime after july 2013)
+	clearShelfCookies("featured_by_category");
+	clearShelfCookies("top_rated_by_author");
+	clearShelfCookies("recent_by_category");
+	
+	
 	if ($("body").hasClass("home")){
-		
 		$("#featured_shelfPageContainer").pbShelf({
-			"cookie": "featured_by_category",
+			//"cookie": "featured_by_category",
 			"afterMoveEnd": function(titlesDeep, perSlide){
 				gaqPush($("#featured_shelfPageContainer"), titlesDeep, perSlide);
 			},
 			"ajaxLoaderImage": siteVars("img") + "ajax-loader-bar.gif",
 			"alreadyWrapped": true
-			
 		});
 		
 		$("#top_titles_shelfPageContainer").pbShelf({
-			"cookie": "top_rated_by_author",
+			//"cookie": "top_rated_by_author",
 			"afterMoveEnd": function(titlesDeep, perSlide){
 				gaqPush($("#top_titles_shelfPageContainer"), titlesDeep, perSlide);
 			},
@@ -39,7 +49,7 @@ $(function(){
 		});
 		
 		$("#recent_titles_shelfPageContainer").pbShelf({
-			"cookie": "recent_by_category",
+			//"cookie": "recent_by_category",
 			"afterMoveEnd": function(titlesDeep, perSlide){
 				gaqPush($("#recent_titles_shelfPageContainer"), titlesDeep, perSlide);
 			},
