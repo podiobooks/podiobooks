cb3f21f50ede93ec24ae112b337f8fa7e50a69ab
Tim White <tim@cyface.com>
2013-06-15 15:34:35 -0600

    First cut at working data model for ad inserts.
    Former-commit-id: 17df091d90b068484d0fcad4948f2d31e5cf20e9
    Former-commit-id: 9bfc6012d6eecaf20d8d43230392b2f275fb687b
    Former-commit-id: 3e3074ced83fb0534448e941cc3069d645c7ebde
    Former-commit-id: ff3369c6929aed6f862c4b97230579f33d54df80
    Former-commit-id: 8578e31ba95dcc5ec4315a6d638a9d7602618ac8
    Former-commit-id: 7fb251e22831f9b012e46fdb9adffb0a75f618a5

--- podiobooks/ads/models.py
+++ podiobooks/ads/models.py
@@ -6,9 +6,11 @@ from podiobooks.core.models import Episode, Title
 
 # pylint: disable=C0111,R0201,W0232
 
+
 class AdSchedule(models.Model):
     """Ad Schedules Control What Ads Get Inserted Where For A Title."""
     name = models.CharField(blank=True, max_length=255)
+    description = models.TextField(blank=True)
 
     deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -21,10 +23,11 @@ class AdSchedule(models.Model):
     def __unicode__(self):
         return self.name
 
+
 class AdSchedulePosition(models.Model):
     """Many-To-Many Intersection Entity Between An Ad Episode and An Ad Schedule."""
-    ad_schedule = models.ForeignKey('AdSchedule', related_name='titleadschedules')
-    episode = models.ForeignKey(Episode, related_name='adscheduleepisodes')
+    ad_schedule = models.ForeignKey('AdSchedule', related_name='ad_schedule_positions')
+    episode = models.ForeignKey(Episode, related_name='ad_schedule_episodes')
     sequence = models.IntegerField()
 
     class Meta:
@@ -43,7 +46,7 @@ class AdSchedulePosition(models.Model):
 class AdScheduleTitle(models.Model):
     """Ad Schedules Many To Many With Titles."""
     ad_schedule = models.ForeignKey('AdSchedule', related_name='titles')
-    title = models.ForeignKey(Title, related_name='adschedules')
+    title = models.ForeignKey(Title, related_name='ad_schedules')
 
     # Note - titles are available as titles.all()
     date_created = models.DateTimeField(auto_now_add=True)
