65fbb5eb6be9517e3168405a9985fa28aeb88231
Tim White <tim@cyface.com>
2013-06-15 15:34:35 -0600

    First cut at working data model for ad inserts.
    Former-commit-id: 2d0bfb27831de224b1f7e910489f95d9cb24d8d0
    Former-commit-id: 1261d574989584949dc2055d87edcb69b6fc811e
    Former-commit-id: 5614e689bd2101f49cf66ac9051483a5a127afbe
    Former-commit-id: 77a2f77514068edaa783ff9d48ade168c5c2e351
    Former-commit-id: 3a66a59b5f4df2dc6aa8112769c35f86275ab64e
    Former-commit-id: f2198be1c319bcab1657356d659e96b65eed96c7

---
+++ podiobooks/ads/tests.py
@@ -0,0 +1,66 @@
+"""Automated unitests for the Podiobooks ad classes"""
+
+# pylint: disable=C0103,C0111,R0902,R0904,W0401,W0614
+
+from django.test import TestCase
+from .models import AdSchedule, AdSchedulePosition, AdScheduleTitle
+from podiobooks.core.models import Episode, Title
+from django.template.defaultfilters import slugify
+
+
+class TitleTestCase(TestCase):
+    fixtures = ['test_data.json', ]
+
+    def setUp(self):
+        # This method sets up a whole series of model objects, and then tries to connect them together.
+
+        # Create an AdSchedule
+        self.schedule1 = AdSchedule.objects.create(
+            name="Podiobooks Ad Schedule #1",
+            description="Test Ad Schedule"
+        )
+
+        # Grab Episodes to Attach
+        all_episodes = Episode.objects.filter(title__pk=92)
+        self.episode1 = all_episodes[1]
+        self.episode2 = all_episodes[2]
+
+        # Add Ad Episodes to AdSchedule
+        self.ad_schedule_ep1 = AdSchedulePosition.objects.create(
+            ad_schedule=self.schedule1,
+            episode=self.episode1,
+            sequence=5
+        )
+
+        self.ad_schedule_ep2 = AdSchedulePosition.objects.create(
+            ad_schedule=self.schedule1,
+            episode=self.episode2,
+            sequence=10
+        )
+
+        self.ad_schedule_ep2 = AdSchedulePosition.objects.create(
+            ad_schedule=self.schedule1,
+            episode=self.episode1,
+            sequence=15
+        )
+
+        self.title1 = Title.objects.all()[1]
+
+        self.ad_schedule_title = AdScheduleTitle.objects.create(
+            title=self.title1,
+            ad_schedule=self.schedule1
+        )
+
+    def test_ad_schedules(self):
+        self.assertEquals(3, self.schedule1.ad_schedule_positions.count())
+
+        self.assertEquals(1, self.title1.ad_schedules.count())
+
+        episode_list = list(self.title1.episodes.all())
+        first_ad_schedule = self.title1.ad_schedules.get()
+        ad_schedule_positions = first_ad_schedule.ad_schedule.ad_schedule_positions.all()
+        for position in ad_schedule_positions:
+            episode_list.insert(position.sequence - 1, position.episode)  # sequence - 1 since .insert inserts after
+
+        print episode_list
+        self.assertEquals(self.episode1, episode_list[4])
