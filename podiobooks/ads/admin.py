e732b23f3540cf96755ccc9fe26346c30e241ddd
Brant Steen <brant.steen@gmail.com>
2013-07-15 14:26:58 -0400

    simple m2m between ad schedules and titles

--- podiobooks/ads/admin.py
+++ podiobooks/ads/admin.py
@@ -4,7 +4,7 @@
 
 from django.contrib import admin
 
-from podiobooks.ads.models import AdSchedule, AdSchedulePosition, AdScheduleTitle
+from podiobooks.ads.models import AdSchedule, AdSchedulePosition
 from podiobooks.core.models import Episode
 
 ### INLINES
@@ -20,16 +20,16 @@ class AdScheduledPositionInline(admin.TabularInline):
             kwargs["queryset"] = Episode.objects.filter(title__slug='pb-ads')
         return super(AdScheduledPositionInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-
-class AdTitleInline(admin.TabularInline):
-    model = AdScheduleTitle
+# 
+# class AdTitleInline(admin.TabularInline):
+#     model = AdScheduleTitle
 
 ### MAIN ADMIN CLASSES
 
 
 class AdScheduleAdmin(admin.ModelAdmin):
     model = AdSchedule
-    inlines = [AdScheduledPositionInline, AdTitleInline]
-
+    inlines = [AdScheduledPositionInline, ]
+    filter_horizontal = ["titles", ]
 
 admin.site.register(AdSchedule, AdScheduleAdmin)
