1609fde51bef09a9e1ab0a8ffabc0ecc023e1902
Tim White <tim@cyface.com>
2013-07-17 09:30:32 -0600

    Made ad migration dependent on core migration.

--- podiobooks/ads/migrations/0001_initial.py
+++ podiobooks/ads/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("core", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'AdSchedule'
         db.create_table(u'ads_adschedule', (
