cd94d2d799ecdce590910aca4b365bbd3ac10e3f
Brant Steen <brant.steen@gmail.com>
2013-11-17 12:40:05 -0500

    Adding a solution for devices moving between IPs
    Need to comb through this a bit..

--- podiobooks/ratings/util.py
+++ podiobooks/ratings/util.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from podiobooks.core.views import Title
 
 
-def get_ratings_widget_dict(request, title):
+def get_ratings_widget_dict(request, title, in_storage=False):
 
     resp = {"status": "ok"}
 
@@ -18,13 +18,18 @@ def get_ratings_widget_dict(request, title):
     if ip_title_list and ip_title_list.has_key(title.pk):
         rating = ip_title_list[title.pk]
 
+    if rating == 0 and in_storage:
+        rating = in_storage
+
     resp = {
         "promotors": title.promoter_count,
         "detrators": title.detractor_count,
         "widget": render_to_string(
             "ratings/widget.html",
             {"rating": rating, "title": title}
-        )
+        ),
+        "userRating": rating,
+        "titleSlug": title.slug
     }
 
     return resp
