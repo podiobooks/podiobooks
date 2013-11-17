b30e7c5ae9334f1bd91431130b8e7551b8136cfa
Brant Steen <brant.steen@gmail.com>
2013-11-17 12:40:05 -0500

    Adding a solution for devices moving between IPs
    Need to comb through this a bit..
    
    
    Former-commit-id: 15922c3788eeb921186f991cbf6cd0ed9865abcf
    Former-commit-id: fea5a25a0a76849d87820ec5c1090dfa094906e2
    Former-commit-id: 4e9842f378cbacbaf7a6e116938a59d855af2162
    Former-commit-id: 4d0c8e128eb978615a1744254f064e3c7407e2b3
    Former-commit-id: 9eff45efc95336d0368262fdea100a691d5accaa
    Former-commit-id: 53304ecf60bc1074a00e4f68ae4fbdf249e0868a

--- podiobooks/themes/pb2-jq/static/js/ratings.js
+++ podiobooks/themes/pb2-jq/static/js/ratings.js
@@ -1,25 +1,33 @@
 $(function(){
 
+	var hasLocalStorage = $("html").hasClass("localstorage");
+
 	var article = $("#titleArticle");
+	var slug = article.data("title-slug");
+
 	var cover = article.find(".title-details-header-cover");
 
 	cover.wrap("<div class='title-details-header-cover-wrap' />");
-
-
 
 	var ratingPlacement = $("<div id='titleRating' class='rating-widget-wrap' />").insertAfter(cover);
 	$("<p class='rating-instructions'>Rate this Podiobook</p>").insertBefore(ratingPlacement);
 
 	var waitingBar = $("<img src='" + siteVars("img") + "ajax-loader-bar.gif' />").hide().appendTo("body");
 
-	article.each(function(){
-		var slug = article.data("title-slug");
-		$.ajax('/rate/' + slug + '/').success(function(data){
-			if (data.widget){
-				ratingPlacement.html($(data.widget));
-			}
-		});
+	var postData = {};
+
+	if (hasLocalStorage && localStorage.getItem(slug)){
+		postData = {"in_storage": localStorage.getItem(slug)};
+	}
+
+	$.ajax('/rate/' + slug + '/', {
+		data: postData
+	}).success(function(data){
+		if (data.widget){
+			ratingPlacement.html($(data.widget));
+		}
 	});
+
 
 	$("#titleArticle").on("click", ".rate-title", function(ev){
 		ev.preventDefault();
@@ -28,19 +36,29 @@ $(function(){
 		ratingPlacement.empty();
 
 		waitingBar.show().appendTo(ratingPlacement);
+
 		var lnk = $(this);
 		var href = lnk.attr("href");
-		$.ajax(
-			href,
-			{type: "POST"}
-		).success(function(data){
+		var postData = {};
+
+		if (hasLocalStorage && localStorage.getItem(slug)){
+			postData = {"in_storage": localStorage.getItem(slug)};
+		}
+
+		$.ajax(href, {
+			type: "POST",
+			data: postData
+		}).success(function(data){
 			if (data.widget){
 				ratingPlacement.height('');
 				waitingBar.hide().appendTo("body");
 				ratingPlacement.html($(data.widget));
 			}
 
+			if (data.titleSlug && hasLocalStorage){
+				localStorage.setItem(data.titleSlug, data.userRating);
+			}
+
 		});
 	});
-
 });
