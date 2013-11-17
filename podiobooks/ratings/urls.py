05a70dbdf5a3a249f8e0fa17cb0a7a0710c71d77
Brant Steen <brant.steen@gmail.com>
2013-11-17 13:49:52 -0500

    CSRF protection for ratings

--- podiobooks/ratings/urls.py
+++ podiobooks/ratings/urls.py
@@ -3,13 +3,14 @@
 from django.conf.urls import patterns, url
 from .views import RateTitleView
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 
 urlpatterns = patterns('',
     url(r'^(?P<slug>[^/]+)/$', 'podiobooks.ratings.views.get_ratings', name='get_ratings'),
 
     # Promote Title
-    url(r'^(?P<slug>[^/]+)/promote/$', never_cache(RateTitleView.as_view()), name='promote_title'),
+    url(r'^(?P<slug>[^/]+)/promote/$', never_cache(csrf_protect(RateTitleView.as_view())), name='promote_title'),
 
     # Detract Title
-    url(r'^(?P<slug>[^/]+)/detract/$', never_cache(RateTitleView.as_view()), kwargs={"up":False}, name='detract_title'),
+    url(r'^(?P<slug>[^/]+)/detract/$', never_cache(csrf_protect(RateTitleView.as_view())), kwargs={"up":False}, name='detract_title'),
 )
