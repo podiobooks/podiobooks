677738f6f487bca32dee115c535f3b84e81ebe6e
Brant Steen <brant.steen@gmail.com>
2013-11-17 11:56:29 -0500

    new modernizr, few updates
    Former-commit-id: 9197dd45ac92e73ea97d9a5551028807b6ecf02d
    Former-commit-id: 3c3585c24789d3e651d87f08728e4cfc9f66504c
    Former-commit-id: bb9411e07bc83f76421673cfac2b4ddc3f6d42e0
    Former-commit-id: a510da91c9b0ba9ab77f4238db09f284d90e5a8d
    Former-commit-id: 70d8a05dcb3c5c2911ae62dacfe36f2e5bd5a2e9
    Former-commit-id: 253d6bc5fa8f68354e8a1f32b7a5eff79af6a697

--- podiobooks/themes/pb2-jq/static/js/ratings.js
+++ podiobooks/themes/pb2-jq/static/js/ratings.js
@@ -43,23 +43,4 @@ $(function(){
 		});
 	});
 
-
-
-	/*
-	var csrftoken = $.cookie("csrftoken");
-	function csrfSafeMethod(method) {
-		// these HTTP methods do not require CSRF protection
-		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-	}
-
-	$.ajaxSetup({
-		crossDomain: false, // obviates need for sameOrigin test
-		beforeSend: function(xhr, settings) {
-			if (!csrfSafeMethod(settings.type)) {
-				xhr.setRequestHeader("X-CSRFToken", csrftoken);
-			}
-		}
-	});
-	*/
-
 });
