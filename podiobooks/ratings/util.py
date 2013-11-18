2a6147d347b3b764ad76f05861ec9463622bbc28
Brant Steen <brant.steen@gmail.com>
2013-11-18 10:14:44 -0500

    Revamp on logic for allowing real votes vs. perceived votes

--- podiobooks/ratings/util.py
+++ podiobooks/ratings/util.py
@@ -18,8 +18,26 @@ def get_ratings_widget_dict(request, title, in_storage=False):
     if ip_title_list and ip_title_list.has_key(title.pk):
         rating = ip_title_list[title.pk]
 
-    if rating == 0 and in_storage:
-        rating = in_storage
+
+    # The following setups become honeypots for voting:
+    #   to prevent abuse
+    #   while letting the user perceive their actions as votes
+
+    # if there is no rating based on IP
+    #   But we received one from storage
+    #   spit back a widget based on the rating from storage
+    if rating == 0:
+        if in_storage:
+            rating = in_storage
+
+    # if there is an IP-based rating
+    #   but there was nothing in storage
+    #   spit back a fresh widget
+    else:
+        if not in_storage:
+            rating = 0
+
+    # End honeypots
 
     resp = {
         "promotors": title.promoter_count,
@@ -33,3 +51,22 @@ def get_ratings_widget_dict(request, title, in_storage=False):
     }
 
     return resp
+
+
+def get_rating_from_storage(request):
+    """
+    return the current rating
+    from local storage in user's browser
+
+    returns False if unavailable or invalid
+    """
+
+    # in_storage is the old vote (if there was one)
+    try:
+        in_storage = int(request.REQUEST.get("in_storage", False))
+        if in_storage != -1 and in_storage != 1:
+            in_storage = False
+    except ValueError:
+        in_storage = False
+
+    return in_storage
