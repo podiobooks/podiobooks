b09348ca1d10640115dea031f64f3c3d4ebf88fd
Brant Steen <brant.steen@gmail.com>
2013-11-18 10:14:44 -0500

    Revamp on logic for allowing real votes vs. perceived votes
    Former-commit-id: 04fc1424dc104cc09820196419e02ab7db98a0d1
    Former-commit-id: 6397160411b70864368dc52fc9783bdbd04fc298
    Former-commit-id: 7ad7acceb65533e3ecd3cdb29bf1518acf845a6b
    Former-commit-id: 107ac7aadbfaf20e036e4b5b71c35b100f9249dd
    Former-commit-id: 4344ca04803a27e7e6f8b9d6a2207f94e60e4f40
    Former-commit-id: 17321f3d223b16dde280509bc94ad391daa57a9a

--- podiobooks/themes/pb2-jq/static/js/ratings.js
+++ podiobooks/themes/pb2-jq/static/js/ratings.js
@@ -5,27 +5,48 @@ $(function(){
 		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 	};
 
+	// CSRF protection
+	var csrftoken = $.cookie('csrftoken');
+	$.ajaxSetup({
+		crossDomain: false, // obviates need for sameOrigin test
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type)) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		}
+	});
 
-	if ($("#titleArticle").length > 0){
+	var hasLocalStorage = $("html").hasClass("localstorage");
 
-		var hasLocalStorage = $("html").hasClass("localstorage");
+	var getCurrentStorage = function(slug){
 
+		var currentlyStored = localStorage.getItem(slug);
+
+		// corrects tampering
+		if (currentlyStored !== "-1" && currentlyStored !== "1"){
+			localStorage.removeItem(slug);
+			currentlyStored = localStorage.getItem(slug);
+		}
+
+		return currentlyStored;
+	};
+
+
+	if ($("#titleArticle").length > 0 && hasLocalStorage){
+
+		var waitingBar = $("<img src='" + siteVars("img") + "ajax-loader-bar.gif' />").hide().appendTo("body");
 		var article = $("#titleArticle");
 		var slug = article.data("title-slug");
-
 		var cover = article.find(".title-details-header-cover");
-
 		cover.wrap("<div class='title-details-header-cover-wrap' />");
-
 		var ratingPlacement = $("<div id='titleRating' class='rating-widget-wrap' />").insertAfter(cover);
 		$("<p class='rating-instructions'>Rate this Podiobook</p>").insertBefore(ratingPlacement);
 
-		var waitingBar = $("<img src='" + siteVars("img") + "ajax-loader-bar.gif' />").hide().appendTo("body");
-
 		var postData = {};
 
-		if (hasLocalStorage && localStorage.getItem(slug)){
-			postData = {"in_storage": localStorage.getItem(slug)};
+		var currentStorage = getCurrentStorage(slug);
+		if (currentStorage){
+			postData = {"in_storage": currentStorage};
 		}
 
 		$.ajax('/rate/' + slug + '/', {
@@ -34,38 +55,28 @@ $(function(){
 			if (data.widget){
 				ratingPlacement.html($(data.widget));
 			}
-
-			if (data.titleSlug && hasLocalStorage){
+			if (data.titleSlug){
 				localStorage.setItem(data.titleSlug, data.userRating);
 			}
 		});
 
 
 		$("#titleArticle").on("click", ".rate-title", function(ev){
+
 			ev.preventDefault();
 
-			var csrftoken = $.cookie('csrftoken');
-
-			$.ajaxSetup({
-				crossDomain: false, // obviates need for sameOrigin test
-				beforeSend: function(xhr, settings) {
-					if (!csrfSafeMethod(settings.type)) {
-						xhr.setRequestHeader("X-CSRFToken", csrftoken);
-					}
-				}
-			});
-
+			// Correct layout bouncing, show waiting gif
 			ratingPlacement.height(ratingPlacement.height());
 			ratingPlacement.empty();
-
 			waitingBar.show().appendTo(ratingPlacement);
 
 			var lnk = $(this);
 			var href = lnk.attr("href");
 			var postData = {};
 
-			if (hasLocalStorage && localStorage.getItem(slug)){
-				postData = {"in_storage": localStorage.getItem(slug)};
+			var currentStorage = getCurrentStorage(slug);
+			if (currentStorage){
+				postData = {"in_storage": currentStorage};
 			}
 
 			$.ajax(href, {
@@ -78,10 +89,9 @@ $(function(){
 					ratingPlacement.html($(data.widget));
 				}
 
-				if (data.titleSlug && hasLocalStorage){
+				if (data.titleSlug){
 					localStorage.setItem(data.titleSlug, data.userRating);
 				}
-
 			});
 		});
 	}
