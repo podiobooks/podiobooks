92f40234dd505245b602c03a6502420a5eb6d7cc
Brant Steen <brant.steen@gmail.com>
2013-12-12 13:04:52 -0500

    some more api changes
    Former-commit-id: 0c6c754cfabd80f85b808bcd68ead372879fe7aa
    Former-commit-id: e21880009c4883dd773bb054641215d49c87581f
    Former-commit-id: f78cf9b3384b7d3c4e46efad4aa218e87233c276
    Former-commit-id: 41fffd1711780f5051cfe2f014096ceb5dd04a31
    Former-commit-id: c2ee37a9961523def4c4a3d21cc3e6dc73663df3
    Former-commit-id: 22e294a4d57b6e5f1960a0aad0bf07330dd7844e

--- podiobooks/core/api.py
+++ podiobooks/core/api.py
@@ -3,39 +3,53 @@
 # pylint: disable=R0904
 
 from tastypie.resources import ModelResource
+from tastypie import fields
 
-from podiobooks.core.models import Title
+from podiobooks.core.models import Title, Episode
 from podiobooks.core.util import get_libsyn_cover_url
 from podiobooks.core.queries import get_featured_shelf_titles, get_recently_released_shelf_titles, get_popular_shelf_titles
 
+
+
+
+
 class TitlesResource(ModelResource):
     """Resource for Exposing Title Model via API"""
+
 
     def dehydrate(self, bundle):
         bundle.data["cover_url"] = get_libsyn_cover_url(bundle.obj, 334, 200)
         return bundle
 
-    class Meta(object):
-        """Defines queryset exposed via API"""
+    class Meta:
         queryset = Title.objects.all()
+        include_resource_uri = True
+
+
+class TitleDetailResource(TitlesResource):
+    episodes = fields.ToManyField('podiobooks.core.api.EpisodesResource', 'episodes', related_name="episodes", full=True)
+
+
+class EpisodesResource(ModelResource):
+    class Meta:
+        queryset = Episode.objects.all()
+
 
 class AllTitlesResource(TitlesResource):
-    """
-    """
-    class Meta(object):
-        """Defines queryset exposed via API"""
+    class Meta:
         queryset = Title.objects.all()
 
+
 class FeaturedTitlesResource(TitlesResource):
-    class Meta(object):
-        """Defines queryset exposed via API"""
+    class Meta:
         queryset = get_featured_shelf_titles()
 
+
 class RecentTitlesResource(TitlesResource):
-    class Meta(object):
+    class Meta:
         queryset = get_recently_released_shelf_titles()
 
 
 class TopRatedTitlesResource(TitlesResource):
-    class Meta(object):
+    class Meta:
         queryset = get_popular_shelf_titles()
--- podiobooks/core/urls/urls_api.py
+++ podiobooks/core/urls/urls_api.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include
 from tastypie.api import Api
-from podiobooks.core.api import AllTitlesResource, FeaturedTitlesResource, RecentTitlesResource, TopRatedTitlesResource
+from podiobooks.core.api import (
+    AllTitlesResource, FeaturedTitlesResource,
+    RecentTitlesResource, TopRatedTitlesResource,
+    EpisodesResource, TitleDetailResource
+)
 
 v1_api = Api(api_name='v1')
 v1_api.register(AllTitlesResource())
 v1_api.register(FeaturedTitlesResource())
 v1_api.register(RecentTitlesResource())
 v1_api.register(TopRatedTitlesResource())
+v1_api.register(TitleDetailResource())
+v1_api.register(EpisodesResource())
 
 urlpatterns = patterns('',
     (r'', include(v1_api.urls)), #http://podiobooks.com/api/v1/title/?format=json
