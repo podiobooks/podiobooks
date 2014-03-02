f5c4f61a966d84fcdf843450361774a5a9034322
Tim White <tim@cyface.com>
2014-03-02 08:54:32 -0700

    Updated package versions of most core stuff. Trying to move django-debug-toolbar to requirements_dev.txt again.
    Former-commit-id: ea9780e2ebebf25e47a90ae149b54be3a2546261
    Former-commit-id: 33e41ff3dbef3c2e3f1f07a22b81e69e457cf42f
    Former-commit-id: c9a8183679c5179561bb6926c7d901a309c6d2a8
    Former-commit-id: 41aa063506a3d7518654214afb0fb2d8ed1fd9bd
    Former-commit-id: 0d9fb9600acee8246f28ad6aeff0ccf00d34d445
    Former-commit-id: 3abaa2697c7b0a6edaea7f290478bbe9565262be

--- podiobooks/settings_gondor.py
+++ podiobooks/settings_gondor.py
@@ -102,16 +102,6 @@ if DEBUG:
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
 
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
-
     DEBUG_TOOLBAR_CONFIG = {
         'INTERCEPT_REDIRECTS': False
     }
