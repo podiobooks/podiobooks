2ccca4477621d20a1fa6314c3a4878690290826b
Tim White <tim@cyface.com>
2014-04-08 08:30:19 -0600

    Updated alldata scripts to include ads.

--- devscripts/gondor/gondor_update_alldata.sh
+++ devscripts/gondor/gondor_update_alldata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Extracts prod data to update alldata fixture.
 rm podiobooks/core/fixtures/alldata.json.zip
-gondor run primary manage.py dumpdata core > podiobooks/core/fixtures/alldata.json
+gondor run primary manage.py dumpdata core ads > podiobooks/core/fixtures/alldata.json
 zip podiobooks/core/fixtures/alldata.json.zip -m podiobooks/core/fixtures/alldata.json
 git add podiobooks/core/fixtures/alldata.json.zip
