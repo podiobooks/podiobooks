4298bf5218381470031c466611dc4e34eaff1c2b
Tim White <tim@cyface.com>
2014-04-08 10:04:41 -0600

    Removed data-related scripts.
    Former-commit-id: d94081ea0cc545c051b829cd692a5582294b3041
    Former-commit-id: 464c046aca63a7d7950bab569cc2941993920c41
    Former-commit-id: b06be499efaa31ee60422ef330e041e1ff6ff338
    Former-commit-id: a595e2c109bdc09f3f4ccd54570294e81a610a4d
    Former-commit-id: 9bb3ec65fe2464424b770ec5fb70a97cec6d0b80
    Former-commit-id: 8e5c6988d148efd774f0816d268d366a2c313cc0

--- devscripts/gondor/gondor_load_alldata.bat
+++
@@ -1,4 +0,0 @@
-REM This needs to be run from the directory where the .gondor directory lives (socialprofile)
-call ..\podiobooks-env\Scripts\activate.bat
-REM Creates Superuser in primary Gondor Instance
-gondor run primary loaddata alldata.json
--- devscripts/gondor/gondor_primary_create_superuser.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Creates Superuser in primary Gondor Instance
-. ./podiobooks-env/bin/activate
-gondor run primary createsuperuser
