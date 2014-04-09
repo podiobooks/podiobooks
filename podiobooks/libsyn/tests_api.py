0b278f17fb76474669deb6b330cc6b9fd7a67871
Tim White <tim@cyface.com>
2014-04-09 10:29:02 -0600

    Fixed handling of skipped libsyn tests.

--- podiobooks/libsyn/tests_api.py
+++ podiobooks/libsyn/tests_api.py
@@ -3,6 +3,7 @@
 # pylint: disable=C0103,C0111,R0904
 
 from django.test import TestCase
+from unittest import skipIf
 from django.conf import settings
 from podiobooks.libsyn.libsyn_utils import get_show_info
 from podiobooks.libsyn import libsyn_method_help
@@ -16,30 +17,24 @@ class LibsynAPITestCase(TestCase):
     def setUp(self):
         self.pprinter = pprint.PrettyPrinter(indent=4)
 
+    @skipIf(not hasattr(settings, 'LIBSYN_NETWORK_SLUG'), 'Libsyn Key Not Provided')
     def test_show_info(self):
-        if hasattr(settings, 'LIBSYN_NETWORK_SLUG'):
-            show_info = get_show_info('theflownsky')
-            self.pprinter.pprint(show_info)  # pretty print the result
-            self.assertEquals('k-9b89823b4508200f', show_info['show_id'])
+        show_info = get_show_info('theflownsky')
+        self.pprinter.pprint(show_info)  # pretty print the result
+        self.assertEquals('k-9b89823b4508200f', show_info['show_id'])
 
-            show_info = get_show_info('notesfromthevault')
-            self.pprinter.pprint(show_info)
-            self.assertEquals('k-27813e6f24530266', show_info['show_id'])
+        show_info = get_show_info('notesfromthevault')
+        self.pprinter.pprint(show_info)
+        self.assertEquals('k-27813e6f24530266', show_info['show_id'])
 
-            show_info = get_show_info('irondragons')
-            self.pprinter.pprint(show_info)
-            self.assertEquals('k-e76e81ee69d5d413', show_info['show_id'])
-        else:
-            self.assertTrue(True)
+        show_info = get_show_info('irondragons')
+        self.pprinter.pprint(show_info)
+        self.assertEquals('k-e76e81ee69d5d413', show_info['show_id'])
 
+    @skipIf(not hasattr(settings, 'LIBSYN_NETWORK_SLUG'), 'Libsyn Key Not Provided')
     def test_method_help(self):
-        if hasattr(settings, 'LIBSYN_NETWORK_SLUG'):
-            libsyn_method_help.main()
-        else:
-            self.assertTrue(True)
+        libsyn_method_help.main()
 
+    @skipIf(not hasattr(settings, 'LIBSYN_NETWORK_SLUG'), 'Libsyn Key Not Provided')
     def test_system_methods(self):
-        if hasattr(settings, 'LIBSYN_NETWORK_SLUG'):
-            libsyn_system_methods.main()
-        else:
-            self.assertTrue(True)
+        libsyn_system_methods.main()
