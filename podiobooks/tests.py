0413082f0a6f86c0952ae329a01bd458655f2ec6
Tim White <tim@cyface.com>
2014-04-09 13:18:08 -0600

    Added basic tests for management commands.
    Former-commit-id: 24585d7a69c4239e8c4e129bb940ca16a860c762
    Former-commit-id: cd9897736c22c226d1a2181d8233713fb0d504f7
    Former-commit-id: 5fe15c891104161ced82e6a15af1ec755e819cf5
    Former-commit-id: 8d81400e9350f1d3ef8e0657c139808d1e7edd34
    Former-commit-id: 3ce35a758f8f6c6209ae51e375e1fe447ccaf552
    Former-commit-id: ecbab83ab5a1f0ee1ca7cd34a195b42290ec4324

---
+++ podiobooks/tests.py
@@ -0,0 +1,22 @@
+"""Tests for top-level items like management commands"""
+
+# pylint: disable=C0103,C0111,R0902,R0904,W0401,W0614
+
+from django.test import TestCase
+from django.core.management import call_command
+
+
+class ManagementCommandsTestCase(TestCase):
+    """Test the Podiobooks Models from a Title-Centric POV"""
+
+    def setUp(self):
+        pass
+
+    def test_clear_cache(self):
+        call_command('clear_cache')
+
+    def test_resave_title_cats(self):
+        call_command("resave_title_categories")
+
+    def test_collectmedia(self):
+        call_command("collectmedia")
