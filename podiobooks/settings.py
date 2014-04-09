385161fb152025488f2f9cefa3427453bcea48ac
Tim White <tim@cyface.com>
2014-04-09 09:05:43 -0600

    Re-enabling gzip middleware, since we are not running SSL.
    Former-commit-id: 0cc9bb1a9557f4e1603b9b5f4f6b18283bdb0f8c
    Former-commit-id: f6120981b275ac7f167172f6fd90889bc8e4e14a
    Former-commit-id: fea5e3fc93688a317cd3a3f518f384384534183f
    Former-commit-id: 689c2de4f4eccceb935f440ec5c887bb63757128
    Former-commit-id: 2c27b20d06f727311dd2485b21dc656459eb72aa
    Former-commit-id: 63924d7dcb600e9aeace41834fa69a1f784db99d

--- podiobooks/settings.py
+++ podiobooks/settings.py
@@ -61,7 +61,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES = (
-#    'django.middleware.gzip.GZipMiddleware',  # https://www.djangoproject.com/weblog/2013/aug/06/breach-and-django/
+    'django.middleware.gzip.GZipMiddleware',  # Not running SSL, so gzip doesn't matter
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
