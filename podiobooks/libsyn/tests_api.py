dc4fa982f0c08998f9db3e293b2e56507852776a
Tim White <tim@cyface.com>
2014-04-09 09:35:28 -0600

    Renaming test names to python standard.

--- podiobooks/libsyn/tests_api.py
+++ podiobooks/libsyn/tests_api.py
@@ -3,6 +3,7 @@
 # pylint: disable=C0103,C0111,R0904
 
 from django.test import TestCase
+from django.conf import settings
 from podiobooks.libsyn.libsyn_utils import get_show_info
 from podiobooks.libsyn import libsyn_method_help
 from podiobooks.libsyn import libsyn_system_methods
@@ -15,21 +16,33 @@ class LibsynAPITestCase(TestCase):
     def setUp(self):
         self.pprinter = pprint.PrettyPrinter(indent=4)
 
-    def testShowInfo(self):
-        show_info = get_show_info('theflownsky')
-        self.pprinter.pprint(show_info)  # pretty print the result
-        self.assertEquals('k-9b89823b4508200f', show_info['show_id'])
+    def test_show_info(self):
+        if settings.LIBSYN_NETWORK_SLUG:
+            show_info = get_show_info('theflownsky')
+            self.pprinter.pprint(show_info)  # pretty print the result
+            self.assertEquals('k-9b89823b4508200f', show_info['show_id'])
 
-        show_info = get_show_info('notesfromthevault')
-        self.pprinter.pprint(show_info)
-        self.assertEquals('k-27813e6f24530266', show_info['show_id'])
+            show_info = get_show_info('notesfromthevault')
+            self.pprinter.pprint(show_info)
+            self.assertEquals('k-27813e6f24530266', show_info['show_id'])
 
-        show_info = get_show_info('irondragons')
-        self.pprinter.pprint(show_info)
-        self.assertEquals('k-e76e81ee69d5d413', show_info['show_id'])
+            show_info = get_show_info('irondragons')
+            self.pprinter.pprint(show_info)
+            self.assertEquals('k-e76e81ee69d5d413', show_info['show_id'])
+        else:
+            print("Skipping Libsyn Show Info Tests")
+            self.assertTrue(True)
 
-    def testMethodHelp(self):
-        libsyn_method_help.main()
+    def test_method_help(self):
+        if settings.LIBSYN_NETWORK_SLUG:
+            libsyn_method_help.main()
+        else:
+            print("Skipping Libsyn Help API Test")
+            self.assertTrue(True)
 
-    def testSystemMethods(self):
-        libsyn_system_methods.main()
+    def test_system_methods(self):
+        if settings.LIBSYN_NETWORK_SLUG:
+            libsyn_system_methods.main()
+        else:
+            print("Skipping Libsyn Method API Test")
+            self.assertTrue(True)
