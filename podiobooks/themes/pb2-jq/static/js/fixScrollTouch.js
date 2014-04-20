8ec35523b54518f30c1a61c0b4d1c56083797d21
Tim White <tim@cyface.com>
2014-04-19 18:55:58 -0600

    jslint cleanup.
    Former-commit-id: adfc801565acf0ce9e7e211465991e419ddbc97b
    Former-commit-id: f9a45ffe05eb07b5b2d3fa7c307d04e96f38f269
    Former-commit-id: b3c23ccb63af9db0d13efc2c08df1aa7403b20e8
    Former-commit-id: 34deaa16e1091e2efff158437f6c0bb3f6c9ff63
    Former-commit-id: 7de7393e16e454711bb5629dab883a5cecf48b6f
    Former-commit-id: 8ccfc93f0c3ef4d8867463112f6e57ce76349a76

--- podiobooks/themes/pb2-jq/static/js/fixScrollTouch.js
+++ podiobooks/themes/pb2-jq/static/js/fixScrollTouch.js
@@ -1,12 +1,12 @@
 (function (window) {
-	if ($("html").hasClass("touch")){
-		
-		
+    if ($("html").hasClass("touch")) {
+
+
         // This library re-implements setTimeout, setInterval, clearTimeout, clearInterval for iOS6.
         // iOS6 suffers from a bug that kills timers that are created while a page is scrolling.
         // This library fixes that problem by recreating timers after scrolling finishes (with interval correction).
-		// This code is free to use by anyone (MIT, blabla).
-		// Author: rkorving@wizcorp.jp
+        // This code is free to use by anyone (MIT, blabla).
+        // Author: rkorving@wizcorp.jp
 
         var timeouts = {};
         var intervals = {};
@@ -17,119 +17,119 @@
 
 
         function createTimer(set, map, args) {
-                var id, cb = args[0], repeat = (set === orgSetInterval);
+            var id, cb = args[0], repeat = (set === orgSetInterval);
 
-                function callback() {
-                        if (cb && typeof(cb) == "function") {
-                        	
-                                cb.apply(window, arguments);
+            function callback() {
+                if (cb && typeof(cb) == "function") {
 
-                                if (!repeat) {
-                                        delete map[id];
-                                        cb = null;
-                                }
-                        }
+                    cb.apply(window, arguments);
+
+                    if (!repeat) {
+                        delete map[id];
+                        cb = null;
+                    }
                 }
+            }
 
-                args[0] = callback;
+            args[0] = callback;
 
-                id = set.apply(window, args);
+            id = set.apply(window, args);
 
-                map[id] = { args: args, created: Date.now(), cb: cb, id: id };
+            map[id] = { args: args, created: Date.now(), cb: cb, id: id };
 
-                return id;
+            return id;
         }
 
 
         function resetTimer(set, clear, map, virtualId, correctInterval) {
-                var timer = map[virtualId];
+            var timer = map[virtualId];
 
-                if (!timer) {
-                        return;
+            if (!timer) {
+                return;
+            }
+
+            var repeat = (set === orgSetInterval);
+
+            // cleanup
+
+            clear(timer.id);
+
+            // reduce the interval (arg 1 in the args array)
+
+            if (!repeat) {
+                var interval = timer.args[1];
+
+                var reduction = Date.now() - timer.created;
+                if (reduction < 0) {
+                    reduction = 0;
                 }
 
-                var repeat = (set === orgSetInterval);
-
-                // cleanup
-
-                clear(timer.id);
-
-                // reduce the interval (arg 1 in the args array)
-
-                if (!repeat) {
-                        var interval = timer.args[1];
-
-                        var reduction = Date.now() - timer.created;
-                        if (reduction < 0) {
-                                reduction = 0;
-                        }
-
-                        interval -= reduction;
-                        if (interval < 0) {
-                                interval = 0;
-                        }
-
-                        timer.args[1] = interval;
+                interval -= reduction;
+                if (interval < 0) {
+                    interval = 0;
                 }
 
-                // recreate
+                timer.args[1] = interval;
+            }
 
-                function callback() {
-                        if (timer.cb) {
-                                timer.cb.apply(window, arguments);
-                                if (!repeat) {
-                                        delete map[virtualId];
-                                        timer.cb = null;
-                                }
-                        }
+            // recreate
+
+            function callback() {
+                if (timer.cb) {
+                    timer.cb.apply(window, arguments);
+                    if (!repeat) {
+                        delete map[virtualId];
+                        timer.cb = null;
+                    }
                 }
+            }
 
-                timer.args[0] = callback;
-                timer.created = Date.now();
-                timer.id = set.apply(window, timer.args);
+            timer.args[0] = callback;
+            timer.created = Date.now();
+            timer.id = set.apply(window, timer.args);
         }
 
 
         window.setTimeout = function () {
-                return createTimer(orgSetTimeout, timeouts, arguments);
+            return createTimer(orgSetTimeout, timeouts, arguments);
         };
 
 
         window.setInterval = function () {
-                return createTimer(orgSetInterval, intervals, arguments);
+            return createTimer(orgSetInterval, intervals, arguments);
         };
 
         window.clearTimeout = function (id) {
-                var timer = timeouts[id];
+            var timer = timeouts[id];
 
-                if (timer) {
-                        delete timeouts[id];
-                        orgClearTimeout(timer.id);
-                }
+            if (timer) {
+                delete timeouts[id];
+                orgClearTimeout(timer.id);
+            }
         };
 
         window.clearInterval = function (id) {
-                var timer = intervals[id];
+            var timer = intervals[id];
 
-                if (timer) {
-                        delete intervals[id];
-                        orgClearInterval(timer.id);
-                }
+            if (timer) {
+                delete intervals[id];
+                orgClearInterval(timer.id);
+            }
         };
 
         window.addEventListener('scroll', function () {
-                // recreate the timers using adjusted intervals
-                // we cannot know how long the scroll-freeze lasted, so we cannot take that into account
+            // recreate the timers using adjusted intervals
+            // we cannot know how long the scroll-freeze lasted, so we cannot take that into account
 
-                var virtualId;
+            var virtualId;
 
-                for (virtualId in timeouts) {
-                        resetTimer(orgSetTimeout, orgClearTimeout, timeouts, virtualId);
-                }
+            for (virtualId in timeouts) {
+                resetTimer(orgSetTimeout, orgClearTimeout, timeouts, virtualId);
+            }
 
-                for (virtualId in intervals) {
-                        resetTimer(orgSetInterval, orgClearInterval, intervals, virtualId);
-                }
+            for (virtualId in intervals) {
+                resetTimer(orgSetInterval, orgClearInterval, intervals, virtualId);
+            }
         });
-	}
+    }
 }(window));
