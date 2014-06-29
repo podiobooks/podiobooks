1a05651e6bda7d36da331d7f31a71600a85b1cd4
Brant Steen <brant.steen@gmail.com>
2014-06-28 21:06:45 -0400

    Dynamic downloading of covers
    lazy downloading

--- podiobooks/core/util.py
+++ podiobooks/core/util.py
@@ -10,6 +10,9 @@ from django.conf import settings
 def download_cover(title, upload_path=''):
     raw_cover_url = "http://asset-server.libsyn.com/show/%s/" % title.libsyn_show_id
 
+    if not upload_path:
+        upload_path = title.cover.field.upload_to
+
     destination_dir = os.path.join(settings.MEDIA_ROOT, upload_path)
 
     if not os.path.isdir(destination_dir):
@@ -31,3 +34,29 @@ def download_cover(title, upload_path=''):
         title.save()
 
     return title.cover
+
+
+def get_cover_url_at_width(title, width):
+    """
+    Returns the URL for a title.cover at a particular width
+
+    Will download the cover lazily if needed.
+
+    returns None if it fails all attempts
+    """
+    attr = "cover_%s" % width
+    try:
+        return getattr(title, attr).url
+    except AttributeError:
+        download_cover(title)
+        if title.cover:
+            try:
+                return getattr(title, attr).url
+            except AttributeError:
+                return title.cover
+    return None
+
+
+def get_libsyn_cover_url(title, height, width):
+    """Pulls the final libsyn URL for a title from libsyn"""
+    return "http://asset-server.libsyn.com/show/{0}/height/{1}/width/{2}".format(title.libsyn_show_id, height, width)
