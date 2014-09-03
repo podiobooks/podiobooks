45d3476aec07fe42a324b6d2be8da4eca58a28de
Brant Steen <brant.steen@gmail.com>
2014-09-03 12:10:49 -0700

    Management command for feed caching
    fixes #315
    
    
    Former-commit-id: c3233fd34f40eed82ce3bbfd50eba9a77e45e02e
    Former-commit-id: 38d3eb8f4303b7a38f71a93d857ccf28f4680658
    Former-commit-id: 89c747c62a9a7c93c9c19f050432d7ca2d0b2f59
    Former-commit-id: 38f2bb81adb3a8947c78a8a9a53447b80c484916
    Former-commit-id: 524fa582ae3a4f2e015a35d2c3ed4b0499fdd412
    Former-commit-id: 7fdb75aadb176181fd5c0a84fc4a170864bf4426

---
+++ podiobooks/feeds/util.py
@@ -0,0 +1,18 @@
+import urllib2
+import json
+
+from django.conf import settings
+
+
+def cache_title_feed(title):
+    """ responsible for calling the endpoint url caching a title's feed """
+    # contents = urllib2.urlopen(settings.FEED_CACHE_ENDPOINT).read()
+
+    contents = urllib2.urlopen(
+        settings.FEED_CACHE_ENDPOINT,
+        "url=%s&token=%s" % (title.get_rss_feed_url(), settings.FEED_CACHE_TOKEN),
+        timeout=120).read()
+    return json.loads(contents)["success"]
+
+
+
