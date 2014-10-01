a4ff505ec51d7c0a445f0e9032cef4c9bbc47c92
Brant Steen <brant.steen@gmail.com>
2014-10-01 10:23:10 -0400

    Removing print lines
    going to move to staging for testing
    
    
    Former-commit-id: 2caf676e8e87660e808e52aa673b1f59d653bc39
    Former-commit-id: 12063a89f1ad92991414473b1224250b27744b1c
    Former-commit-id: d2ef5bf7123b445d5e95af4010410f9f341f5cde
    Former-commit-id: c008270127bda57c4bdc4c1028db576abf079a91
    Former-commit-id: 57e75b67429426315032e235379272eb972e4fe2
    Former-commit-id: a2a9afa9575087d1b6e8dac2dad3b39c148a4493

--- podiobooks/core/util.py
+++ podiobooks/core/util.py
@@ -51,35 +51,28 @@ def download_cover_from_libsyn(title, upload_path=''):
     destination = os.path.join(destination_dir, image_file)
     upload_path = "%s/%s" % (upload_path, image_file)
 
-    print "Image File: %s" % image_file
-    print "Destination: %s" % destination
-    print "Upload path: %s" % upload_path
+    try:
+        if not os.path.isfile(destination):
+            if title.libsyn_cover_image_url:
+                raw_cover_url = title.libsyn_cover_image_url
+            else:
+                raw_cover_url = "http://asset-server.libsyn.com/show/%s/" % title.libsyn_show_id
 
-    # try:
-    if not os.path.isfile(destination):
-        if title.libsyn_cover_image_url:
-            raw_cover_url = title.libsyn_cover_image_url
-        else:
-            raw_cover_url = "http://asset-server.libsyn.com/show/%s/" % title.libsyn_show_id
+            print raw_cover_url
 
-        print raw_cover_url
+            filename, httpresponse = urllib.urlretrieve(raw_cover_url)  # pylint: disable=W0612
 
-        filename, httpresponse = urllib.urlretrieve(raw_cover_url)  # pylint: disable=W0612
+            img = Image.open(filename)
+            if img.mode != "RGB":
+                img = img.convert("RGB")
+            img.save(destination, "JPEG", quality=100)
 
-        img = Image.open(filename)
-        if img.mode != "RGB":
-            img = img.convert("RGB")
-        img.save(destination, "JPEG", quality=100)
-    else:
-        print "Found file %s" % destination
+        if not title.cover or title.cover != upload_path:
+            title.cover = upload_path
+            title.save()
 
-    if not title.cover or title.cover != upload_path:
-        print "saving cover to field"
-        title.cover = upload_path
-        title.save()
-
-    # except IOError:
-    #     pass
+    except IOError:
+        pass
 
     return title.cover
 
