3efa6d9d5e20bc2bb3eac411129febc706495b61
Brant Steen <brant.steen@gmail.com>
2014-10-07 16:29:21 -0400

    lets try it like this
    Former-commit-id: 23ffab39ed025526d91298f5d7ed6c37106a7034
    Former-commit-id: 34d1a35bf4f92b4512bff5d6139e74a02734f815
    Former-commit-id: ed33edd11b860795c66884e4e0d00546639387c9
    Former-commit-id: fe322e363439ce0c54773081b0d919342cb698b8
    Former-commit-id: 4cbcccfb67b0eb0f379333e05e8dc781c199bd11
    Former-commit-id: 87bcc1eba42aa0e3631a1c3db4b32192f24c11e1

--- podiobooks/celery.py
+++ podiobooks/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'podiobooks.settings')
 
-app = Celery('podiobooks')
+app = Celery()
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
