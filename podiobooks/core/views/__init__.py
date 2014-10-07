60fc753d0c234d27f29a2586ae45a73864b0e9ae
Brant Steen <brant.steen@gmail.com>
2014-10-07 16:36:55 -0400

    syntax
    Former-commit-id: 298e84dd1ccb25babc5326e173189afaaf3aebc4
    Former-commit-id: 9150f1cbbfa06a8dd63057ce95acd7a86a554809
    Former-commit-id: ab364c2049d69d7d383d833cd731e8f468868a26
    Former-commit-id: 6634e77b6b569946a54302f96a3a54f6ae2bdf2c
    Former-commit-id: b4f2cceaeb93118746662ddb724a44a15d2395ec
    Former-commit-id: e436cc2bf49dc0a98cdcaec28a0296b3153e70fd

--- podiobooks/core/views/__init__.py
+++ podiobooks/core/views/__init__.py
@@ -66,7 +66,7 @@ class IndexView(TemplateView):
         }
 
         logger.error("JUST TESTING From VIEW")
-        logger.error("TIME!!! %s" % datetime.now() + timedelta(seconds=4))
+        logger.error("TIME!!! %s" % (datetime.now() + timedelta(seconds=4)))
 
         hello_world.apply_async(eta=datetime.now() + timedelta(seconds=4))
 
