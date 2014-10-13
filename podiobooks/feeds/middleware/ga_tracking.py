5206a4e397761d0b47fd71410eff1ff368ef037b
Brant Steen <brant.steen@gmail.com>
2014-10-13 14:29:59 -0400

    ..
    Former-commit-id: 58233b19e18882077288473c5c11844b2808027a
    Former-commit-id: fb7394833b39a86ffebc0f0d836c3ae3e5023566
    Former-commit-id: b6bb230514a097325a7b6f248f4eedfa57955d18
    Former-commit-id: 102d527c854e05725631a039e682e559268dd1f2
    Former-commit-id: 6dd372f94c6b7c4ccbb2cecb7f1d8bd5639df347
    Former-commit-id: eb4a5482ba0e38abcf55369bd27975ea891fcd6c

--- podiobooks/feeds/middleware/ga_tracking.py
+++ podiobooks/feeds/middleware/ga_tracking.py
@@ -3,17 +3,9 @@ MIddleware to send GA events when feeds are accessed
 """
 import logging
 
-from urllib2 import URLError
-from socket import timeout
-
-from pyga.requests import Event, Session, Tracker, Visitor
-from django.conf import settings
-from django.contrib.sites.models import Site
-from django.core.urlresolvers import reverse
-
 from podiobooks.tasks import ping_analytics_for_feeds
 
-LOGGER = logging.getLogger(name='root')
+
 GA_TRACKING = 'GA_TRACK'
 
 
@@ -21,26 +13,12 @@ class GATracker(object):
     """
     Middleware class to handle sending GA tracking event
     """
-
-    # def process_view(self, request, view_func, view_args, view_kwargs):
-    #     """
-    #     Process at the view level
-    #     """
-    #     if GA_TRACKING in view_kwargs and GA_TRACKING:
-    #         del view_kwargs[GA_TRACKING]
-    #         LOGGER.info("INSIDE MIDDLEWARE")
-    #         print "YOYOYOOY"
-    #         ping_analytics_for_feeds(request, view_func, view_args, view_kwargs)
-
     def process_request(self, request):
         path = request.META.get("PATH_INFO", None)
         if path and path.startswith("/rss/feeds/episodes/") and path.endswith("/"):
-            LOGGER.info("INSIDE MIDDLEWARE")
+
+            logger = logging.getLogger(name='root')
+            logger.info("INSIDE MIDDLEWARE")
+
             slug = path.split("/")[-2]
             ping_analytics_for_feeds(request, slug)
-
-        # if "PATH_INFO" in request.META and request.META["PATH_INFO"]:
-
-
-    # def process_request(self, request):
-    #     print request
