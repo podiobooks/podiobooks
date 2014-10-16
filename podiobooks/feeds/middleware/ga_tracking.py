15c5bf5616ca7a0ba18be460542065355417f44d
Brant Steen <brant.steen@gmail.com>
2014-10-16 16:45:44 -0400

    handles not pinging on old slug feeds
    Former-commit-id: c9c6002a8dfe409ebb11f437d4b426de89b49627
    Former-commit-id: 5efd27ad5facc885bfa7a23a2e13e65ca67db336
    Former-commit-id: dbaf8c47815b07eb0abacd851bf825ef07396e80
    Former-commit-id: 34bd1b7b9557d90cf92e4fb82308c4947f647db7
    Former-commit-id: 3126d0f51bd447671c5685793ca7e8768b60fe9b
    Former-commit-id: a3172f131566b98c3cdae5a4f8c7b68bcf809d4a

--- podiobooks/feeds/middleware/ga_tracking.py
+++ podiobooks/feeds/middleware/ga_tracking.py
@@ -2,11 +2,13 @@
 MIddleware to send GA events when feeds are accessed
 """
 import logging
+from django.db.models import Q
 
 from django.core.urlresolvers import reverse
+from django.core.cache import cache
 
 from podiobooks.tasks import ping_analytics_for_feeds
-
+from podiobooks.core.models import Title
 
 GA_TRACKING = 'GA_TRACK'
 
@@ -16,6 +18,13 @@ class GATracker(object):
     Middleware class to handle sending GA tracking event
     """
     def process_request(self, request):
+        slugs_from_cache = cache.get("old_title_slugs")
+
+        if not slugs_from_cache:
+            with_old_slugs = Title.objects.filter(~Q(old_slug=""), old_slug__isnull=False)
+            slugs_from_cache = [item.old_slug for item in with_old_slugs if item.slug != item.old_slug]
+            cache.set('old_title_slugs', slugs_from_cache, 60*60*2)  # cache for 2 hours
+
         path = request.META.get("PATH_INFO", None)
         if path and path.endswith("/"):
 
@@ -29,7 +38,10 @@ class GATracker(object):
 
                 slug = path.split("/")[-1]
 
-                ping_analytics_for_feeds(request, slug)
+                if slug not in slugs_from_cache:
+                    ping_analytics_for_feeds(request, slug)
+                else:
+                    logger.info("OLD SLUG URL, NOT PINGING GA")
 
             elif path == reverse('all_titles_feed'):
                 ping_analytics_for_feeds(request, "ALL TITLES FEED")
