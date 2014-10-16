11d6a3fed318997a739c6487a673dd155de889f2
Brant Steen <brant.steen@gmail.com>
2014-10-16 16:12:15 -0400

    Tracking for recent titles feed and all titles feed
    Former-commit-id: 6d0de88d44e91cf53a15ae556f46333affd0d03f
    Former-commit-id: 4bf78f1e373d8f66017f2c6fb58e2eec8662ca62
    Former-commit-id: 40d9766b31a0f534639c0be4776c412ea269350e
    Former-commit-id: d4f278f72f184657e37349a7f7ca661cfc41b7de
    Former-commit-id: c7155eaee43ffe9633e2d55ba2e2b54e9c5a014d
    Former-commit-id: f9feb8a05f8311b5bcf2621d132fb36377bbfb1c

--- podiobooks/feeds/middleware/ga_tracking.py
+++ podiobooks/feeds/middleware/ga_tracking.py
@@ -3,6 +3,8 @@ MIddleware to send GA events when feeds are accessed
 """
 import logging
 
+from django.core.urlresolvers import reverse
+
 from podiobooks.tasks import ping_analytics_for_feeds
 
 
@@ -15,14 +17,22 @@ class GATracker(object):
     """
     def process_request(self, request):
         path = request.META.get("PATH_INFO", None)
-        if path and path.startswith("/rss/feeds/episodes/") and path.endswith("/"):
+        if path and path.endswith("/"):
 
             logger = logging.getLogger(name='root')
             logger.info("INSIDE MIDDLEWARE")
 
-            while path.endswith("/"):
-                path = "/".join(path.split("/")[:-1])
+            if path.startswith("/rss/feeds/episodes/"):
 
-            slug = path.split("/")[-1]
+                while path.endswith("/"):
+                    path = "/".join(path.split("/")[:-1])
 
-            ping_analytics_for_feeds(request, slug)
+                slug = path.split("/")[-1]
+
+                ping_analytics_for_feeds(request, slug)
+
+            elif path == reverse('all_titles_feed'):
+                ping_analytics_for_feeds(request, "ALL TITLES FEED")
+
+            elif path == reverse('recent_titles_feed'):
+                ping_analytics_for_feeds(request, "RECENT TITLES FEEDS")
--- podiobooks/tasks.py
+++ podiobooks/tasks.py
@@ -17,20 +17,20 @@ def hello_world():
     print "hi"
 
 
-def ping_analytics_for_feeds(request, slug):
+def ping_analytics_for_feeds(request, action):
     tracker = Tracker(settings.GOOGLE_ANALYTICS_ID, Site.objects.get_current().domain)
     visitor = Visitor()
     visitor.ip_address = request.META.get('REMOTE_ADDR', '')
     visitor.user_agent = request.META.get('HTTP_USER_AGENT', '')
 
-    event = Event(category='RSS', action=slug, label=request.path, value=None, noninteraction=False)
+    event = Event(category='RSS', action=action, label=request.path, value=None, noninteraction=False)
 
     try:
         logger = logging.getLogger(name='root')
 
         logger.info("Pushing feed ping to GA...")
         logger.info("Category: RSS")
-        logger.info("Action: %s" % slug)
+        logger.info("Action: %s" % action)
         logger.info("Label: %s" % request.path)
 
         tracker.track_event(event, Session(), visitor)
