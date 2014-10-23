c8e98974f49a9876d6d18fb75956770e2e59edb8
cyface <tim@cyface.com>
2014-10-23 15:21:12 -0600

    Removed unused task import.

--- podiobooks/core/views/__init__.py
+++ podiobooks/core/views/__init__.py
@@ -52,8 +52,6 @@ class IndexView(TemplateView):
         initial_category_slug_recent = category_choice_form_recent.fields["category"].initial
         recently_released_list = get_recently_released_shelf_titles(initial_category_slug_recent)
 
-        # hello_world.apply_async(countdown=5)
-
         # Render Template
         response_data = {
             'featured_title_list': featured_title_list,
