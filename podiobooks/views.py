a57d9f7da6aff75ab17ba7af75dc16ff0c41cd3f
cyface <tim@cyface.com>
2014-10-30 10:18:28 -0600

    Added HttpResponseRedirect import.

--- podiobooks/views.py
+++ podiobooks/views.py
@@ -2,7 +2,7 @@
 
 from django.views.generic import RedirectView, TemplateView, View
 from django.views.decorators.cache import never_cache
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 import os
 
