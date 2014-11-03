c7975e57bb259d4d260de5b86d139b849159dca2
cyface <tim@cyface.com>
2014-11-03 08:37:24 -0700

    Installed Django REST Framework, added basic config.

--- podiobooks/core/api.py
+++ podiobooks/core/api.py
@@ -3,11 +3,41 @@
 # pylint: disable=R0904
 
 from tastypie.resources import ModelResource
-from podiobooks.core.models import Title
+from podiobooks.core.models import Category, Contributor, Title, TitleContributor
+from rest_framework import routers, serializers, viewsets
 
 
 class TitleResource(ModelResource):
     """Resource for Exposing Title Model via API"""
+
     class Meta(object):
         """Defines queryset exposed via API"""
         queryset = Title.objects.all()
+        fields = (
+            'name', 'contributors', 'series', 'series_sequence', 'description', 'slug', 'cover', 'license', 'is_adult', 'is_explicit',
+            'is_family_friendly', 'is_for_kids', 'promoter_count', 'detractor_count',
+            'category_list', 'date_updated'
+        )
+
+class TitleSerializer(serializers.ModelSerializer):
+    """API Definitions for Titles"""
+    categories = serializers.RelatedField(many=True)
+    contributors = serializers.RelatedField(many=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'name', 'contributors', 'series', 'series_sequence', 'description', 'slug', 'cover', 'license', 'is_adult', 'is_explicit',
+            'is_family_friendly', 'is_for_kids', 'promoter_count', 'detractor_count',
+            'category_list', 'date_updated'
+        )
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """View Behavior for TitleViews"""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'titles', TitleViewSet)
--- podiobooks/core/urls/urls_api.py
+++ podiobooks/core/urls/urls_api.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include
 from tastypie.api import Api
-from podiobooks.core.api import TitleResource
+from podiobooks.core.api import router, TitleResource, TitleSerializer, TitleViewSet
 
 v1_api = Api(api_name='v1')
 v1_api.register(TitleResource())
 
 urlpatterns = patterns('',
-    (r'', include(v1_api.urls)), #http://podiobooks.com/api/v1/title/?format=json
+    (r'', include(v1_api.urls)), # http://podiobooks.com/api/v1/title/?format=json
+    (r'', include(router.urls)),  # http://podiobooks.com/api/title/?format=json
 )
