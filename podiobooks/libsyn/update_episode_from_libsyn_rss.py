80bb18536fb2907d364bbe7b488f28b98d335cd8
Tim White <tim@cyface.com>
2015-04-10 12:05:40 -0600

    Added management command to update episode durations.

--- podiobooks/libsyn/update_episode_from_libsyn_rss.py
+++ podiobooks/libsyn/update_episode_from_libsyn_rss.py
@@ -6,13 +6,8 @@ Update duration data on an episode by parsing a libsyn RSS feed.
 
 from xml.etree import ElementTree
 import urllib
-from podiobooks.core.models import Episode, License, Title
-from django.template.defaultfilters import slugify
-from email.utils import mktime_tz, parsedate_tz
-import datetime
-import time
-from HTMLParser import HTMLParser
-from django.utils import timezone
+from podiobooks.core.models import Title
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 import re
 
 
@@ -21,8 +16,11 @@ def update_episode_from_libsyn_rss(rss_feed_url):
 
     if rss_feed_url.startswith('http'):
         feed = urllib.urlopen(rss_feed_url)
-        feed_tree = ElementTree.parse(feed).getroot()
         libsyn_slug = re.search('//(.*).podiobooks', rss_feed_url).group(1)
+        try:
+            feed_tree = ElementTree.parse(feed).getroot()
+        except ElementTree.ParseError:
+            feed_tree = None
     else:  # Only unit tests hit this side
         feed_tree = ElementTree.parse(rss_feed_url).getroot()
         libsyn_slug = 'linus'
@@ -37,10 +35,13 @@ def update_episode_from_libsyn_rss(rss_feed_url):
     title = Title.objects.get(libsyn_slug=libsyn_slug)
 
     for item in items:
-        episode_url = item.find('enclosure').get('url').replace('traffic.libsyn.com', 'media.podiobooks.com')
-        sequence = int(episode_url[episode_url.rfind('.') - 2:episode_url.rfind('.')])  # Use URL File Name to Calc Seq
-        episode = title.episodes.get(sequence=sequence)
-        episode.duration = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}duration').text
-        episode.save()
+        try:
+            episode_url = item.find('enclosure').get('url').replace('traffic.libsyn.com', 'media.podiobooks.com')
+            sequence = int(episode_url[episode_url.rfind('.') - 2:episode_url.rfind('.')])  # Use URL File Name to Calc Seq
+            episode = title.episodes.get(sequence=sequence)
+            episode.duration = item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}duration').text
+            episode.save()
+        except (AttributeError, ValueError, ElementTree.ParseError, ObjectDoesNotExist, MultipleObjectsReturned) as e:
+            print "{0} Was Not Updated: {1}".format(title.slug, e)
 
     return title
---
+++ podiobooks/management/commands/update_episode_durations.py
@@ -0,0 +1,27 @@
+"""Clears the Cache"""
+
+from django.core.management.base import NoArgsCommand
+from podiobooks.core.models import Title
+from podiobooks.libsyn.update_episode_from_libsyn_rss import update_episode_from_libsyn_rss
+
+
+class Command(NoArgsCommand):
+    """
+        Updates Episode Durations
+    """
+    help = "Updates Episode Durations"
+
+    def handle_noargs(self, **options):
+        """Updates Episode Durations"""
+        titles_to_update = Title.objects.filter(episodes__duration="45:00").exclude(libsyn_slug__isnull=True, libsyn_slug="").distinct()
+        for title in titles_to_update:
+            if title.libsyn_slug:
+                updated_title = update_episode_from_libsyn_rss("http://{0}.podiobooks.libsynpro.com/rss".format(title.libsyn_slug))
+                if updated_title:
+                    self.stdout.write("{0} Updated".format(updated_title.slug))
+                else:
+                    self.stdout.write("{0} ERROR".format(title.slug))
+            else:
+                self.stdout.write("{0} No Libsyn Slug".format(title.slug))
+
+        self.stdout.write('Complete\n')
