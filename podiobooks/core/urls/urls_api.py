814abc2f4a52e0cda2e5c4399b3d6c3bef4b5385
Tim White <tim@cyface.com>
2015-04-23 09:34:46 -0600

    Delete urls_api.py

--- podiobooks/core/urls/urls_api.py
+++
@@ -1,6 +0,0 @@
-# from django.conf.urls import patterns, include
-# from podiobooks.core.api import router
-
-# urlpatterns = patterns('',
-#                       (r'', include(router.urls)),  # http://podiobooks.com/api/titles/
-#                       )
