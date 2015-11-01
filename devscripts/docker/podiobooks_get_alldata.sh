a537c2706ca55a11fc5f9f9d5c54e396b2c3c917
Tim White <tim@cyface.com>
2015-11-01 13:26:36 -0700

    Updating scripts.

--- devscripts/docker/podiobooks_get_alldata.sh
+++ devscripts/docker/podiobooks_get_alldata.sh
@@ -4,8 +4,9 @@
 mkdir /opt/podiobooks/.ssh
 chmod 700 /opt/podiobooks/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /opt/podiobooks/.ssh/config
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 chmod 600 /opt/podiobooks/.ssh/config
 ssh-agent bash -c 'ssh-add /opt/podiobooks/data/.ssh/id_rsa; git clone --depth=1 git@github.com:podiobooks/podiobooks_data.git /opt/podiobooks/data/podiobooks_data'
 cd /opt/podiobooks/data/podiobooks
 . .env/bin/activate
-python manage.py loaddata /opt/podiobooks/data/podiobooks_data/alldata.json.zip
+python manage.py loaddata /opt/podiobooks/data/podiobooks_data/alldata.json.zip --settings=podiobooks.settings_docker
--- devscripts/docker/podiobooks_get_dep.sh
+++ devscripts/docker/podiobooks_get_dep.sh
@@ -4,6 +4,7 @@
 mkdir /opt/podiobooks/.ssh
 chmod 700 /opt/podiobooks/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /opt/podiobooks/.ssh/config
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 chmod 600 /opt/podiobooks/.ssh/config
 ssh-agent bash -c 'ssh-add /opt/podiobooks/data/.ssh/id_rsa; git clone --depth=1 git@github.com:podiobooks/podiobooks_dep.git /opt/podiobooks/data/podiobooks_dep'
 ln -s /opt/podiobooks/data/podiobooks_dep/docker/podiobooks-uwsgi-dev.xml /opt/podiobooks/data/podiobooks-uwsgi-local.xml
