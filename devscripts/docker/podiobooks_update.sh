b22d96843bef0072dc369837ccf9e75cbab0eaec
Tim White <tim@cyface.com>
2015-11-01 17:32:38 -0700

    Adding management commands to update script.

--- devscripts/docker/podiobooks_update.sh
+++ devscripts/docker/podiobooks_update.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 cd /opt/podiobooks/data/podiobooks
 git pull
+. .env/bin/activate
+python manage.py migrate --fake-initial --noinput --settings=podiobooks.settings_docker
+python manage.py collectstatic --noinput --settings=podiobooks.settings_docker
+python manage.py collectmedia --settings=podiobooks.settings_docker
+python manage.py mub_minify --settings=podiobooks.settings_docker
+python manage.py localize_title_covers --settings=podiobooks.settings_docker
