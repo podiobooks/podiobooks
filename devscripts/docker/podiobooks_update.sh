e6066f807c29fd1eee522e20d9fc19cb546b7d74
Tim White <tim@cyface.com>
2015-11-01 21:02:45 -0700

    Moved collectstatic after mub_minify.

--- devscripts/docker/podiobooks_update.sh
+++ devscripts/docker/podiobooks_update.sh
@@ -7,3 +7,4 @@ python manage.py mub_minify --settings=podiobooks.settings_docker
 python manage.py localize_title_covers --settings=podiobooks.settings_docker
 python manage.py collectstatic --noinput --settings=podiobooks.settings_docker
 python manage.py collectmedia --settings=podiobooks.settings_docker
+chown -R podiobooks.podiobooks /opt/podiobooks/data/podiobooks
