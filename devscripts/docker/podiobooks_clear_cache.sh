18e49fc43dc20fc8bff2cc53797945f453bdee2c
Tim White <tim@cyface.com>
2015-11-01 17:10:50 -0700

    Updating scripts.

--- devscripts/docker/podiobooks_clear_cache.sh
+++ devscripts/docker/podiobooks_clear_cache.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Note this will only work if you have set up an ssh key in /opt/podiobooks/data/.ssh and added it to the podiobooks_data deploy keys
 # The podiobooks-initial-setup.sh should create the key for you, but you have to add it in the github interface as the podiobooks user in github.
+cd /opt/podiobooks/data/podiobooks
 . .env/bin/activate
 python manage.py clear_cache
