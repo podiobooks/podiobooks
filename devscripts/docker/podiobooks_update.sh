07cf9df97d6b8edf82b4c1399d6b45dbb004c74c
Tim White <tim@cyface.com>
2015-11-10 12:34:13 -0700

    Enabling touch-reload.

--- devscripts/docker/podiobooks_update.sh
+++ devscripts/docker/podiobooks_update.sh
@@ -10,4 +10,4 @@ python manage.py collectstatic --noinput --settings=podiobooks.settings_docker
 python manage.py collectmedia --settings=podiobooks.settings_docker
 chown -R podiobooks.podiobooks /opt/podiobooks/data/podiobooks
 echo "Touching uWSGI"
-touch /opt/podiobooks/data/podiobooks-uwsgi.xml
+touch /opt/podiobooks/data/podiobooks/podiobooks/wsgi_docker.py
