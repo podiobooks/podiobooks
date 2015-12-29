4436a9a6cbb0811918b637685c8202d68e2f26c2
Tim White <tim@cyface.com>
2015-12-29 14:48:29 -0700

    Trying out uwsgi-cache.

--- devscripts/docker-compose/init.sh
+++ devscripts/docker-compose/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker-compose run uwsgi python manage.py migrate --noinput
-docker-compose run uwsgi python manage.py loaddata initial
+docker-compose run uwsgi python manage.py loaddata ../podiobooks_data/alldata.json.zip
 docker-compose run uwsgi python manage.py collectstatic --noinput
 docker-compose run uwsgi python manage.py createsuperuser
