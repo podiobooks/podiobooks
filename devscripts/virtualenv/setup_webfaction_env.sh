53f755ae151fcf824b18f3f2a129e27861f5c3c6
cyface <tim@cyface.om>
2016-04-21 19:28:33 +0000

    adding virtualenv webfaction script

---
+++ devscripts/virtualenv/setup_webfaction_env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# You must have virtualenv installed, and the virtualenv command in your path for this to work.
+# Assuming you have python installed, you can install virtualenv using the command below.
+# curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
+# This should be run from the project directory
+
+virtualenv --no-site-packages .env
+. ./.env/bin/activate
+pip install -r podiobooks/requirements_webfaction.txt
