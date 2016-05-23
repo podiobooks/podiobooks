eb80af28a490141e6c2f402f6f6a9465f87e5af1
Tim White <tim@cyface.com>
2016-05-23 14:40:51 -0600

    Delete update_dev_env.bat

--- devscripts/virtualenv/update_dev_env.bat
+++
@@ -1,4 +0,0 @@
-REM This updates an already in-place virtualenv to match the current requirements_dev.txt file.
-REM It will not upgrade package versions.
-call .env\Scripts\activate.bat
-pip install -r podiobooks\requirements_dev.txt
