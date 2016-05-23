3e895d46090e419091d7ddbdecaf66681968d202
Tim White <tim@cyface.com>
2016-05-23 14:44:19 -0600

    Update setup_dev_env.bat

--- devscripts/virtualenv/setup_dev_env.bat
+++ devscripts/virtualenv/setup_dev_env.bat
@@ -8,4 +8,4 @@ pypm --non-interactive install virtualenv
 pypm --non-interactive install pil
 virtualenv --system-site-packages --distribute .env
 call .env\Scripts\activate.bat
-pip install -r podiobooks\requirements_dev.txt
+pip install --upgrade -r podiobooks\requirements_dev.txt
